522591a90e01ce09719804782ff8f879fbb12d22
Petr Vaněk <petr@yarpen.cz>
2004-12-20 19:24:26 +0000

    guides instead strange marks
    git-svn-id: svn://scribus.net/trunk/Scribus@937 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/samples/golden-mean.py
+++ scribus/plugins/scriptplugin/samples/golden-mean.py
@@ -3,13 +3,11 @@
 
 """Golden Mean for Scribus.
 
-This script creates supplementary marks on the page to
+This script creates supplementary guides on the page to
 help design the "right" layout in golden mean (golden
 ratio).
-The marks are located in new non-printable layer.
 
-See www.yarpen.cz, py.cz and scribus.net for fresh
-versions to come...
+See scribus.net and CVS for fresh versions to come...
 
 REQUIREMENTS:
 Scribus - CVS version later 02/24/2004 or later release 1.5
@@ -51,53 +49,24 @@ except ImportError:
 
 from math import sqrt
 
-GMLAYER = "Golden Mean Layer"
 
 def goldenMean(aSize=0):
     """x = (?5-1)/2"""
     return aSize * ((sqrt(5) - 1)/2)
 
 
-def createMark(X=0, Y=0, aName=""):
-    """Create something like cross on page."""
-    D = 10
-    if objectExists(aName) == 1:
-        return
-    createPolyLine(
-        [X-D, Y, X+D, Y, X, Y-D, X, Y+D,
-         X-D, Y, X, Y-D, X, Y+D, X+D, Y], aName)
-
-
 def main():
     # remember user settings
     unit = getUnit()
-    layer = getActiveLayer()
-
     # set my environment - points needed
     setUnit(0)
     # Paper format
     paper = pageDimension()
-
-    layerExists = 0
-    for i in getLayers():
-        if i == GMLAYER:
-           layerExists = 1
-    if layerExists == 0:
-        createLayer(GMLAYER)
-    setActiveLayer(GMLAYER)
-    setLayerPrintable(GMLAYER, 0)
-
-    createMark(goldenMean(paper[0]), goldenMean(paper[1]), "GoldenMeanA4-1")
-    createMark(paper[0] - goldenMean(paper[0]), goldenMean(paper[1]),
-                "GoldenMeanA4-2")
-    createMark(goldenMean(paper[0]), paper[1] - goldenMean(paper[1]),
-                "GoldenMeanA4-3")
-    createMark(paper[0] - goldenMean(paper[0]), paper[1] - goldenMean(paper[1]),
-                "GoldenMeanA4-4")
-
+    # set the guides. The get* functions are for "remembering" the old ones...
+    setVGuides(getVGuides() + [goldenMean(paper[0]), paper[0] - goldenMean(paper[0])])
+    setHGuides(getHGuides() + [goldenMean(paper[1]), paper[1] - goldenMean(paper[1])])
     # restore user settings
     setUnit(unit)
-    setActiveLayer(layer)
 
 if __name__ == '__main__':
     if haveDoc():
