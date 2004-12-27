93eedc713ae42a9e9d20b1d6020d482bf10115f1
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2004-12-27 21:43:31 +0000

    Fixed Bug #1148
    git-svn-id: svn://scribus.net/trunk/Scribus@1007 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/hruler.cpp
+++ scribus/hruler.cpp
@@ -49,6 +49,8 @@ Hruler::Hruler(QScrollView *pa, ScribusDoc *doc) : QWidget(pa)
 void Hruler::mousePressEvent(QMouseEvent *m)
 {
 	Mpressed = true;
+	if (doku->loading)
+		return;
 	if (ItemPosValid)
 	{
 		RulerCode = 0;
@@ -131,6 +133,8 @@ void Hruler::mousePressEvent(QMouseEvent *m)
 void Hruler::mouseReleaseEvent(QMouseEvent *m)
 {
 	Mpressed = false;
+	if (doku->loading)
+		return;
 	if (ItemPosValid)
 	{
 		if ((m->y() < height()) && (m->y() > 0))
@@ -212,6 +216,8 @@ void Hruler::mouseReleaseEvent(QMouseEvent *m)
 
 void Hruler::mouseMoveEvent(QMouseEvent *m)
 {
+	if (doku->loading)
+		return;
 	if (ItemPosValid)
 	{
 		double ColWidth = (ItemEndPos - ItemPos - (ColGap * (Cols - 1)) - Extra - RExtra - 2*lineCorr) / Cols;
@@ -366,6 +372,8 @@ void Hruler::mouseMoveEvent(QMouseEvent *m)
 
 void Hruler::paintEvent(QPaintEvent *)
 {
+	if (doku->loading)
+		return;
 	int pc, xx;
 	double of, xl, iter, iter2;
 	double sc = doku->Scale;
