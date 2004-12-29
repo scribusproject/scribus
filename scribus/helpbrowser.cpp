48d03018f224e0b421d1471679e35d2ed710134a
Craig Bradney <mrb@scribus.info>
2004-12-29 20:05:29 +0000

    Clean up helpbrowser and enable tutorials to be installed separately
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@1033 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/helpbrowser.cpp
+++ scribus/helpbrowser.cpp
@@ -38,14 +38,15 @@
 #include <qfileinfo.h>
 #include <qtextcodec.h>
 #include <qdom.h>
+#include <qdir.h>
 #include <qsplitter.h>
 
 #ifdef _MSC_VER
- #if (_MSC_VER >= 1200)
-  #include "win-config.h"
- #endif
+#if (_MSC_VER >= 1200)
+#include "win-config.h"
+#endif
 #else
- #include "config.h"
+#include "config.h"
 #endif
 
 extern QPixmap loadIcon(QString nam);
@@ -185,12 +186,12 @@ void HelpBrowser::histChosen(int i)
 void HelpBrowser::jumpToHelpSection(const QString newGuiLanguage, QString jumpToSection, QString jumpToFile)
 {
 	QString toLoad;
-	QString guiLanguage = newGuiLanguage=="" ? "en" : newGuiLanguage;
+	QString guiLanguage = newGuiLanguage=="" ? "en" : newGuiLanguage.left(2);
 
 	if (jumpToFile=="") 
 	{
 		QString pfad = DOCDIR;
-		toLoad = pfad + + guiLanguage.left(2) + "/"; //clean this later to handle 5 char locales
+		toLoad = pfad + guiLanguage + "/"; //clean this later to handle 5 char locales
 		if (jumpToSection=="") 
 		{
 			toLoad+="index.html";
@@ -251,9 +252,9 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 {
 	QString pfad = DOCDIR;
 	QString toLoad;
-	QString guiLanguage = newGuiLanguage=="" ? "en" : newGuiLanguage;
+	QString guiLanguage = newGuiLanguage=="" ? "en" : newGuiLanguage.left(2);
 
-	QString pfad2 = pfad + guiLanguage.left(2) + "/menu.xml";
+	QString pfad2 = pfad + guiLanguage + "/menu.xml";
 
 	QFileInfo fi = QFileInfo(pfad2);
 
@@ -262,6 +263,7 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 	else
 	{
 		toLoad = pfad + "en/menu.xml";
+		guiLanguage="en";
 		fi = QFileInfo(toLoad);
 		qDebug("Scribus help in your selected language does not exist, trying English. Otherwise, please visit http://docs.scribus.net.");
 	}
@@ -282,6 +284,8 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 		QDomElement docElem = doc.documentElement();
 		QDomNode n = docElem.firstChild();
 		QListViewItem *qlvi=NULL, *qlvi2=NULL, *qlvi3=NULL, *qlvi4=NULL, *qlvi5=NULL, *qlvi6=NULL;
+		QListViewItem *tutorialsMenuItem;
+		bool haveTutorials=false;
 
 		while( !n.isNull() ) 
 		{
@@ -295,14 +299,13 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 					if (qlvi2==NULL)
 						qlvi=new QListViewItem(listView, textAttr.value(), fileAttr.value());
 					else 
+						qlvi=new QListViewItem(listView, qlvi2, textAttr.value(), fileAttr.value());
+					if (qlvi!=NULL && textAttr.value()=="Tutorials")
 					{
-						qlvi=new QListViewItem(listView, qlvi2);
-						if (qlvi!=NULL) 
-						{
-							qlvi->setText(0, textAttr.value());
-							qlvi->setText(1, fileAttr.value());
-						}
+						haveTutorials=true;
+						tutorialsMenuItem=qlvi;
 					}
+
 					if (qlvi!=NULL)
 						qlvi2=qlvi;
 				}
@@ -323,13 +326,11 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 								if (qlvi4==NULL)
 									qlvi3=new QListViewItem(qlvi, textAttr.value(), fileAttr.value());
 								else 
+									qlvi3=new QListViewItem(qlvi, qlvi4, textAttr.value(), fileAttr.value());
+								if (qlvi3!=NULL && textAttr.value()=="Tutorials")
 								{
-									qlvi3=new QListViewItem(qlvi, qlvi4);
-									if (qlvi3!=NULL) 
-									{
-										qlvi3->setText(0, textAttr.value());
-										qlvi3->setText(1, fileAttr.value());
-									}
+									haveTutorials=true;
+									tutorialsMenuItem=qlvi3;
 								}
 								if (qlvi3!=NULL)
 									qlvi4=qlvi3;
@@ -351,13 +352,11 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 											if (qlvi6==NULL)
 												qlvi5=new QListViewItem(qlvi3, textAttr.value(), fileAttr.value());
 											else 
+												qlvi5=new QListViewItem(qlvi3, qlvi6, textAttr.value(), fileAttr.value());
+											if (qlvi5!=NULL && textAttr.value()=="Tutorials")
 											{
-												qlvi5=new QListViewItem(qlvi3, qlvi6);
-												if (qlvi5!=NULL) 
-												{
-													qlvi5->setText(0, textAttr.value());
-													qlvi5->setText(1, fileAttr.value());
-												}
+												haveTutorials=true;
+												tutorialsMenuItem=qlvi5;
 											}
 										}
 										if (qlvi5!=NULL)
@@ -371,14 +370,91 @@ void HelpBrowser::loadMenu(const QString newGuiLanguage)
 			}
 			n = n.nextSibling();
 		}
+		//scan for installed tutorials
+		if (haveTutorials) 
+		{
+			QString path = DOCDIR;
+			path += guiLanguage + "/tutorials/";
+			QDir dir(path, "*", QDir::Name, QDir::Dirs | QDir::NoSymLinks);
+
+			if (dir.exists() && (dir.count() != 0)) 
+			{
+				for (uint i = 0; i < dir.count(); ++i) 
+				{
+					if (dir[i]!="." && dir[i]!="..")
+					{
+						QFileInfo file(path + dir[i] + "/menu.xml");
+						if (file.exists())  // menu.xml exists for tutorial
+						{
+							QDomDocument docTutorial( "tutorialmenuentries" );
+							QFile fileTutorialMenu( file.filePath() );
+							if ( !fileTutorialMenu.open( IO_ReadOnly ) )
+								break;
+							if ( !docTutorial.setContent( &fileTutorialMenu ) ) 
+							{
+								fileTutorialMenu.close();
+								break;
+							}
+							fileTutorialMenu.close();
+
+							QDomElement docElemTutorial = docTutorial.documentElement();
+							QDomNode nTutorial = docElemTutorial.firstChild();
+							QListViewItem *tutorialQLVI=NULL;
+
+							while( !nTutorial.isNull() ) 
+							{
+								QDomElement eTutorial = nTutorial.toElement(); // try to convert the node to an element.
+								if( !eTutorial.isNull() ) 
+								{
+									if (tutorialsMenuItem!=NULL && eTutorial.hasAttribute( "text" ) && eTutorial.hasAttribute( "file" )) 
+									{
+										QDomAttr textAttr = eTutorial.attributeNode( "text" );
+										QDomAttr fileAttr = eTutorial.attributeNode( "file" );
+										tutorialQLVI=new QListViewItem(tutorialsMenuItem, textAttr.value(), fileAttr.value());
+									}
+									QDomNodeList nl=nTutorial.childNodes();
+									QListViewItem *tutorialSubMenuItem, *tutorialSubMenuItemLast=NULL;
+									for(uint i=0 ; i<= nl.count() ; i++)
+									{
+										QDomNode child=nl.item(i);
+										if (child.isElement())
+										{
+											QDomElement ec = child.toElement();
+											if (!ec.isNull())
+											{
+												if (ec.hasAttribute( "text" ) && ec.hasAttribute( "file" ))
+												{
+													QDomAttr textAttr = ec.attributeNode( "text" );
+													QDomAttr fileAttr = ec.attributeNode( "file" );
+													if (tutorialSubMenuItemLast==NULL)
+														tutorialSubMenuItem=new QListViewItem(tutorialQLVI, textAttr.value(), fileAttr.value());
+													else 
+														tutorialSubMenuItem=new QListViewItem(tutorialQLVI, tutorialSubMenuItemLast, textAttr.value(), fileAttr.value());
+													if (tutorialSubMenuItem!=NULL)
+														tutorialSubMenuItemLast=tutorialSubMenuItem;
+												}
+											}
+										}
+									}
+								}
+								nTutorial = nTutorial.nextSibling();
+							}
+						}
+					}
+				}
+			}
+
+		}
 	}
 }
 
 void HelpBrowser::itemSelected(QListViewItem *item)
 {
-	QString pfad = DOCDIR;
 	if ( !item )
 		return;
-	else if ((item->text(1)!=QString::null) && (item->text(1)!=QString("")))
+	if (item->text(1)!=QString::null)
+	{
+		QString pfad = DOCDIR;
 		loadHelp(pfad + "en/"+item->text(1));
+	}
 }
