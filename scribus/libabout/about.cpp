824db5eb6768289cb613e52e4afdd75b6df8bb6e
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2005-01-03 23:14:13 +0000

    Adjusted the Date in the "About" Dialog
    git-svn-id: svn://scribus.net/trunk/Scribus@1069 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/libabout/about.cpp
+++ scribus/libabout/about.cpp
@@ -58,7 +58,7 @@ About::About( QWidget* parent )
 	tabLayout1->addWidget( PixmapLabel1 );
 	BuildID = new QLabel( tab, "BB" );
 	BuildID->setAlignment(Qt::AlignCenter);
-	QString bu = tr("%1. %2 %3 ").arg("28").arg("December").arg("2004");
+	QString bu = tr("%1. %2 %3 ").arg("03").arg("January").arg("2005");
 #ifdef HAVE_CMS
 	bu += "C";
 #else
