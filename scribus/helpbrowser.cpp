57a30bf9cb82786a4b77b907aacaf455df27e9a7
Craig Bradney <mrb@scribus.info>
2005-01-06 16:33:00 +0000

    Allow translation of Tutorials menu and avoid duplication of it
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@1103 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/helpbrowser.cpp
+++ scribus/helpbrowser.cpp
@@ -304,10 +304,14 @@ void HelpBrowser::loadMenu()
 						qlvi=new QListViewItem(listView, textAttr.value(), fileAttr.value());
 					else 
 						qlvi=new QListViewItem(listView, qlvi2, textAttr.value(), fileAttr.value());
-					if (qlvi!=NULL && textAttr.value()=="Tutorials")
+					if (qlvi!=NULL && e.hasAttribute( "section" ))
 					{
-						haveTutorials=true;
-						tutorialsMenuItem=qlvi;
+						QDomAttr sectionAttr = e.attributeNode( "section" );
+						if (sectionAttr.value()=="tutorials") 
+						{
+							haveTutorials=true;
+							tutorialsMenuItem=qlvi;
+						}
 					}
 
 					if (qlvi!=NULL)
@@ -331,10 +335,14 @@ void HelpBrowser::loadMenu()
 									qlvi3=new QListViewItem(qlvi, textAttr.value(), fileAttr.value());
 								else 
 									qlvi3=new QListViewItem(qlvi, qlvi4, textAttr.value(), fileAttr.value());
-								if (qlvi3!=NULL && textAttr.value()=="Tutorials")
+								if (qlvi3!=NULL && ec.hasAttribute( "section" ))
 								{
-									haveTutorials=true;
-									tutorialsMenuItem=qlvi3;
+									QDomAttr sectionAttr = e.attributeNode( "section" );
+									if (sectionAttr.value()=="tutorials")
+									{
+										haveTutorials=true;
+										tutorialsMenuItem=qlvi3;
+									}
 								}
 								if (qlvi3!=NULL)
 									qlvi4=qlvi3;
@@ -357,10 +365,14 @@ void HelpBrowser::loadMenu()
 												qlvi5=new QListViewItem(qlvi3, textAttr.value(), fileAttr.value());
 											else 
 												qlvi5=new QListViewItem(qlvi3, qlvi6, textAttr.value(), fileAttr.value());
-											if (qlvi5!=NULL && textAttr.value()=="Tutorials")
+											if (qlvi5!=NULL && ecc.hasAttribute( "section" ))
 											{
-												haveTutorials=true;
-												tutorialsMenuItem=qlvi5;
+												QDomAttr sectionAttr = e.attributeNode( "section" );
+												if (sectionAttr.value()=="tutorials")
+												{
+													haveTutorials=true;
+													tutorialsMenuItem=qlvi5;
+												}
 											}
 										}
 										if (qlvi5!=NULL)
