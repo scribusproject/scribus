60acf3258296a7e22fd807fee4e649749609907a
Craig Bradney <mrb@scribus.info>
2005-01-12 17:36:50 +0000

    Fix for 1516, include Apple Roman codec in list
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@1165 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/customfdialog.cpp
+++ scribus/customfdialog.cpp
@@ -237,7 +237,7 @@ CustomFDialog::CustomFDialog(QWidget *pa, QString wDir, QString cap, QString fil
 			QString tmp_txc[] = {"ISO 8859-1", "ISO 8859-2", "ISO 8859-3", "ISO 8859-4", "ISO 8859-5", "ISO 8859-6",
 								"ISO 8859-7", "ISO 8859-8", "ISO 8859-9", "ISO 8859-10", "ISO 8859-13", "ISO 8859-14",
 								"ISO 8859-15", "utf8", "KOI8-R", "KOI8-U", "CP1250", "CP1251", "CP1252", "CP1253",
-								"CP1254", "CP1255", "CP1256", "CP1257"};
+								"CP1254", "CP1255", "CP1256", "CP1257", "Apple Roman"};
 			size_t array = sizeof(tmp_txc) / sizeof(*tmp_txc);
 			for (uint a = 0; a < array; ++a)
 				TxCodeM->insertItem(tmp_txc[a]);
