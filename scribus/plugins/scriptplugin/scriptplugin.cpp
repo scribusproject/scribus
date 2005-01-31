6fc03a7feaecbc7883e72a008ab768906dca894b
Petr Vaněk <petr@yarpen.cz>
2005-01-31 19:26:34 +0000

    fixed annoying main docstring bug. The 'Scribus Python' large paragraphs won't get lost after lupdate
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@1345 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/scriptplugin.cpp
+++ scribus/plugins/scriptplugin/scriptplugin.cpp
@@ -128,13 +128,13 @@ void InitPlug(QWidget *d, ScribusApp *plug)
 	Tes->scrScripterActions.insert("scripterExecuteScript", new ScrAction(QObject::tr("&Execute Script..."), QKeySequence(), Tes, "scripterExecuteScript"));
 	Tes->scrScripterActions.insert("scripterShowConsole", new ScrAction(QObject::tr("Show &Console"), QKeySequence(), Tes, "scripterShowConsole"));
 	Tes->scrScripterActions.insert("scripterAboutScript", new ScrAction(QObject::tr("&About Script..."), QKeySequence(), Tes, "scripterAboutScript"));
-	
+
 	Tes->scrScripterActions["scripterShowConsole"]->setToggleAction(true);
-	
+
 	QObject::connect( Tes->scrScripterActions["scripterExecuteScript"], SIGNAL(activated()) , Tes, SLOT(slotTest()) );
 	QObject::connect( Tes->scrScripterActions["scripterShowConsole"], SIGNAL(toggled(bool)) , Tes, SLOT(slotInteractiveScript(bool)) );
 	QObject::connect( Tes->scrScripterActions["scripterAboutScript"], SIGNAL(activated()) , Tes, SLOT(aboutScript()) );
-	
+
 	Tes->menuMgr->createMenu("Scripter", QObject::tr("&Script"));
 	Tes->menuMgr->addMenuToMenuBarAfter("Scripter","Windows");
 	Tes->menuMgr->createMenu("ScribusScripts", QObject::tr("&Scribus Scripts"), "Scripter");
@@ -143,13 +143,13 @@ void InitPlug(QWidget *d, ScribusApp *plug)
 	Tes->menuMgr->addMenuSeparator("Scripter");
 	Tes->menuMgr->addMenuItem(Tes->scrScripterActions["scripterShowConsole"], "Scripter");
 	Tes->menuMgr->addMenuItem(Tes->scrScripterActions["scripterAboutScript"], "Scripter");
-	
+
 	Tes->SavedRecentScripts.clear();
 	Tes->ReadPlugPrefs();
 	Tes->buildScribusScriptsMenu();
 	Tes->buildRecentScriptsMenu();
 	Tes->pcon = new PConsole(d);
-	
+
 	QObject::connect(Tes->pcon->OutWin, SIGNAL(returnPressed()), Tes, SLOT(slotExecute()));
 	QObject::connect(Tes->pcon, SIGNAL(paletteShown(bool)), Tes, SLOT(slotInteractiveScript(bool)));
 }
@@ -1179,39 +1179,34 @@ void initscribus(ScribusApp *pl)
 	// the others, because we can just create a Unicode object and insert it in our
 	// module dictionary.
 	QString docstring = QObject::tr("Scribus Python interface module\n\
-		\n\
-		This module is the Python interface for Scribus. It provides functions\n\
-		to control scribus and to manipulate objects on the canvas. Each\n\
-		function is documented individually below.\n\
-		\n\
-		A few things are common across most of the interface.\n\
-		\n\
-		Most functions operate on frames. Frames are identified by their name,\n\
-		a string - they are not real Python objects. Many functions take an\n\
-		optional (non-keyword) parameter, a frame name.\n\
-		Many exceptions are also common across most functions. These are\n\
-		not currently documented in the docstring for each function.\n\
-		\n\
-		    - Many functions will raise a NoDocOpenError if you try to use them\n\
-		      without a document to operate on.\n\
-		\n\
-		    - If you do not pass a frame name to a function that requires one,\n\
-		      the function will use the currently selected frame, if any, or\n\
-		      raise a NoValidObjectError if it can't find anything to operate\n\
-		      on.\n\
-		\n\
-		    - Many functions will raise WrongFrameTypeError if you try to use them\n\
-		      on a frame type that they do not make sense with. For example, setting\n\
-		      the text colour on a graphics frame doesn't make sense, and will result\n\
-		      in this exception being raised.\n\
-		\n\
-		    - Errors resulting from calls to the underlying Python API will be\n\
-		      passed through unaltered. As such, the list of exceptions thrown by\n\
-		      any function as provided here and in its docstring is incomplete.\n\
-		\n\
-		Details of what exceptions each function may throw are provided on the\n\
-		function's documentation.\n\
-		");
+\n\
+This module is the Python interface for Scribus. It provides functions\n\
+to control scribus and to manipulate objects on the canvas. Each\n\
+function is documented individually below.\n\
+\n\
+A few things are common across most of the interface.\n\
+\n\
+Most functions operate on frames. Frames are identified by their name,\n\
+a string - they are not real Python objects. Many functions take an\n\
+optional (non-keyword) parameter, a frame name.\n\
+Many exceptions are also common across most functions. These are\n\
+not currently documented in the docstring for each function.\n\
+- Many functions will raise a NoDocOpenError if you try to use them\
+without a document to operate on.\n\
+- If you do not pass a frame name to a function that requires one,\
+the function will use the currently selected frame, if any, or\
+raise a NoValidObjectError if it can't find anything to operate\
+on.\n\
+- Many functions will raise WrongFrameTypeError if you try to use them\n\
+on a frame type that they do not make sense with. For example, setting\n\
+the text colour on a graphics frame doesn't make sense, and will result\n\
+in this exception being raised.\n\
+- Errors resulting from calls to the underlying Python API will be\n\
+passed through unaltered. As such, the list of exceptions thrown by\n\
+any function as provided here and in its docstring is incomplete.\n\
+\n\
+Details of what exceptions each function may throw are provided on the\n\
+function's documentation.");
 
 	// Py_UNICODE is a typedef for unsigned short
 	PyObject* uniDocStr = Py_BuildValue(const_cast<char*>("u"), (Py_UNICODE*)(docstring.ucs2()));
