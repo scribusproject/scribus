d900e9cc25b3cf24c1d3c49517e0a3d8da1c3c5e
Craig Bradney <mrb@scribus.info>
2005-04-08 08:01:02 +0000

    #1677: Rename kerning to tracking
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@1845 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/mpalette.h
+++ scribus/mpalette.h
@@ -201,7 +201,7 @@ public:
 	QLabel* bottomLabel;
 	QLabel* leftLabel;
 	QLabel* rightLabel;
-	QLabel* kerningLabel;
+	QLabel* trackingLabel;
 	QLabel* linespacingLabel;
 	QLabel* fontsizeLabel;
 	QLabel* yscaleLabel;
