469564cd6a3472d8b94db6570e44e04187cef4e6
Petr Vaněk <petr@yarpen.cz>
2005-07-31 20:58:22 +0000

    #2301: new doc dialog: dialog gets bigger when selecting facing pages
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@2909 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/marginWidget.h
+++ scribus/marginWidget.h
@@ -7,35 +7,61 @@
 #include "scribusstructs.h"
 class MSpinBox;
 
+/*! Widget for Margins setting.
+Used e.g. in "New Doc Dialog" or "Preferences".
+*/
 class MarginWidget : public QGroupBox
 {
 	Q_OBJECT
 
 public:
+	/*! It creates an user interface for base margin settings for the document.
+	\param parent Parent widget (used as standard in Qt)
+	\param title Title of the Group box
+	\param MarginStruct* margs: A reference to the margins in the preferences.
+	\param decimals How much decimals in spinboxes: e.g. 0.00 for 2 decimals.
+	\param unit mainly doc->unitRatio
+	\param einh unit text to display in the spin boxes (mm, pt etc)
+	*/
 	MarginWidget( QWidget* parent, QString title, MarginStruct* margs, int decimals, double unitRatio, QString einh);
 	~MarginWidget() {};
+	/*! Setup the labels by facing pages option */
 	void setFacingPages(bool facing);
+	/*! Setup the spinboxes properties (min/max value etc.) by width */
 	void setPageWidth(double width);
+	/*! Setup the spinboxes properties (min/max value etc.) by height */
 	void setPageHeight(double heigth);
+	/*! Setup the widgets by new options */
 	void unitChange(double newUnit, int newDecimals, QString newSuffix);
+	/*! Spinboxes */
 	MSpinBox* topR;
 	MSpinBox* bottomR;
 	MSpinBox* rightR;
 	MSpinBox* leftR;
+	/*! Labels */
 	QLabel* GRText1;
 	QLabel* GRText2;
 	QLabel* GRText3;
 	QLabel* GRText4;
+	/*! Top margin value converted by unitRatio */
 	double RandT;
+	/*! Bottom margin value converted by unitRatio */
 	double RandB;
+	/*! Left margin value converted by unitRatio */
 	double RandL;
+	/*! Right margin value converted by unitRatio */
 	double RandR;
+	/*! Self contained ratio taken in constructor */
 	double unitRatio;
+	/*! Internally used page width */
 	double pageWidth;
+	/*! Internally used page height */
 	double pageHeight;
+	/*! unused */
 	int docUnitIndex;
 
 public slots:
+	/*! Recompute the values after spinbox change */
 	void setTop();
 	void setBottom();
 	void setLeft();
