0e3bd43e8b3fdf232dec3cae8083fa641290898c
Craig Bradney <mrb@scribus.info>
2005-08-07 11:34:48 +0000

    Add in those missing strings files.. sorry :)
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@2953 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/commonstrings.cpp
@@ -0,0 +1,46 @@
+/***************************************************************************
+	begin                : Aug 2005
+	copyright            : (C) 2005 by Craig Bradney	
+	email                : cbradney@zip.com.au
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   ScApp program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
+#include "commonstrings.h"
+
+QString CommonStrings::_Apply      = "";
+QString CommonStrings::tr_Apply    = "";
+QString CommonStrings::_Cancel     = "";
+QString CommonStrings::tr_Cancel   = "";
+QString CommonStrings::None        = "";
+QString CommonStrings::trNone      = "";
+QString CommonStrings::_OK         = "";
+QString CommonStrings::tr_OK       = "";
+QString CommonStrings::_Save       = "";
+QString CommonStrings::tr_Save     = "";
+
+void CommonStrings::languageChange()
+{
+	CommonStrings::_Apply    = "&Apply";
+	CommonStrings::tr_Apply  = tr( "&Apply" );
+	
+	CommonStrings::_Cancel    = "&Cancel";
+	CommonStrings::tr_Cancel  = tr( "&Cancel" );
+	
+	CommonStrings::None       = "None";
+	CommonStrings::trNone     = QT_TR_NOOP( "None" );
+	
+	CommonStrings::_OK        = "&OK";
+	CommonStrings::tr_OK      = tr( "&OK" );
+	
+	CommonStrings::_Save      = "&Save";
+	CommonStrings::tr_Save    = QT_TR_NOOP( "&Save" );
+}
+
