6fd86ec8a64691daaae71fbd3233e44b64d62735
Peter Linnell <mrdocs@scribus.info>
2005-09-05 22:59:20 +0000

    convert to new styles <qt> tr string
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@3188 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/fontreplacedialog.cpp
+++ scribus/fontreplacedialog.cpp
@@ -25,7 +25,7 @@ FontReplaceDialog::FontReplaceDialog( QWidget* parent, QMap<QString, QString> *R
 
 	textLabel1 = new QLabel( this, "textLabel1" );
 	textLabel1->setAlignment( int( QLabel::AlignVCenter ) );
-	textLabel1->setText( tr( "This document contains some fonts that are not installed on your system,\nplease choose a suitable replacement for them.\nCancel will stop the document from loading." ) );
+	textLabel1->setText( "<qt>" + tr("This document contains some fonts that are not installed on your system, please choose a suitable replacement for them. Cancel will stop the document from loading.") + "</qt>" );
 	FontReplaceDialogLayout->addWidget( textLabel1 );
 
 	replacementTable = new QTable( this, "replacementTable" );
