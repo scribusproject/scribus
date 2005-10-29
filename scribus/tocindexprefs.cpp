4a0f7daddedecf890283b1d2a309313be7932b64
Craig Bradney <mrb@scribus.info>
2005-10-29 23:38:11 +0000

    Update copyright notice in tocindexprefs
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@3741 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/tocindexprefs.cpp
+++ scribus/tocindexprefs.cpp
@@ -1,11 +1,17 @@
-/****************************************************************************
-** Form implementation generated from reading ui file './tocindexprefs.ui'
-**
-** Created: Sun Oct 30 00:44:47 2005
-**      by: The User Interface Compiler ($Id$)
-**
-** WARNING! All changes made in this file will be lost!
-****************************************************************************/
+/***************************************************************************
+	begin                : 2005
+	copyright            : (C) 2005 by Craig Bradney
+	email                : cbradney@zip.com.au
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   ScApp program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
 
 #include "tocindexprefs.h"
 #include "tocindexprefs.moc"
--- scribus/tocindexprefs.h
+++ scribus/tocindexprefs.h
@@ -1,3 +1,17 @@
+/***************************************************************************
+	begin                : 2005
+	copyright            : (C) 2005 by Craig Bradney
+	email                : cbradney@zip.com.au
+***************************************************************************/
+
+/***************************************************************************
+*                                                                         *
+*   ScApp program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
 #ifndef TOCINDEXPREFS_H
 #define TOCINDEXPREFS_H
 
