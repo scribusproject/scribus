1aa442d78dc1f2e977874311ba4ba072f3f42f80
Craig Bradney <mrb@scribus.info>
2005-11-14 10:10:37 +0000

    Move the ScText struct out of scribusstructs.h like TT have done for the Qt4 branch
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@3878 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/sctextstruct.h
@@ -0,0 +1,71 @@
+#ifndef SCTEXTSTRUCT_H
+#define SCTEXTSTRUCT_H
+
+#ifdef HAVE_CONFIG_H
+#include "scconfig.h"
+#endif
+
+#include <qstring.h>
+
+class Foi;
+class PageItem;
+
+/* Struktur fuer Pageitem Text */
+struct ScText
+{
+	bool cselect;
+	int csize;
+	short cshade;
+	short cshade2;
+	short cstyle;
+/**	Meaning of the values in cstyle
+	1			= Superscript
+	2 			= Subscript
+	4 			= Outline
+	8 			= Underline
+	16 		= Strikethru
+	32 		= All Caps
+	64 		= Small Caps
+	128 		= Hyphenation possible here (Smart Hyphen)
+	256 	= Shadowed
+	512 	= Underline Words
+	1024 	= free, not used in the moment
+	2048 	= Char is a DropCap
+	4096 	= internal use in PageItem (Suppresses spaces when in Block alignment)
+	8192 	= Smart Hyphen visible at line end
+	16384	= Start of Line
+*/
+	short cab;
+	short cscale;
+	short cscalev;
+	short cbase;
+	short cshadowx;
+	short cshadowy;
+	short coutline;
+	short cunderpos;
+	short cunderwidth;
+	short cstrikepos;
+	short cstrikewidth;
+	short cextra;
+	float xp;
+	float yp;
+	float PtransX;
+	float PtransY;
+	float PRot;
+	Foi* cfont;
+	PageItem* cembedded;
+	QString ccolor;
+	QString cstroke;
+	QString ch;
+ /** Special Characters used:
+ 	QChar(24) =	Non breaking Hyphen
+ 	QChar(25) =	Marks an inline Object
+ 	QChar(26) =	Column Break
+ 	QChar(27) =	Frame Break
+ 	QChar(28) =	New Line, doesn't break Paragraph
+ 	QChar(29) =	Non breaking Space
+ 	QChar(30) =	Automatic Pagenumbering  */
+};
+
+#endif // SCTEXTSTRUCT_H
+
