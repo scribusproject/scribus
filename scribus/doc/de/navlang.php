05934d1cdd3ac8dcad3bc72de879f31f7fa84b59
Peter Linnell <mrdocs@scribus.info>
2005-11-16 22:19:05 +0000

    adding .de docs to ship them with 1.2.4 step # 2
    git-svn-id: svn://scribus.net/trunk/Scribus@3889 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/doc/de/navlang.php
@@ -0,0 +1,141 @@
+<?php
+$navlang["docs.scribus.net"]="docs.scribus.net";
+$navlang["News"]="Aktuelles";
+$navlang["Read Me First"]="Erst-Info (Bitte lesen!)";
+$navlang["Download"]="Downloads";
+$navlang["Documentation Changes"]="Dokumentations&auml;nderungen";
+	
+$navlang["Documentation"]="Dokumentation";
+$navlang["Introduction"]="Einleitung";
+$navlang["About the team"]="&Uuml;ber das Team";
+$navlang["Specifications"]="Spezifikationen";
+$navlang["Scribus Basics"]="Scribus-Grundlagen";
+
+$navlang["Installation"]="Installation";
+$navlang["Scribus Source & CVS"]="Scribus-Quellen &amp; CVS";
+$navlang["Requirements"]="Systemanforderungen";
+$navlang["Compiling & Installing"]="Kompilieren & Installieren";
+$navlang["Platforms"]="Plattformen";
+$navlang["Parallel Install"]="Parallele Installation";
+$navlang["Debian Repository"]="Debian-Repository";
+
+$navlang["Cross Platform"]="Cross-Plattform";
+$navlang["Mac OSX-Fink"]="Mac OSX-Fink";
+$navlang["Mac OSX-Fink & CVS"]="Mac OSX-Fink &amp; CVS";
+$navlang["Windows with Cygwin"]="Windows mit Cygwin";
+
+$navlang["Setup"]="Setup";
+$navlang["Keyboard Shortcuts"]="Tastenkombinationen";
+$navlang["Mouse Usage"]="Mausbenutzung";
+$navlang["Hyphenation"]="Silbentrennung";
+$navlang["Font Setup"]="Schriftarten-Verwaltung";
+$navlang["Fonts in Depth"]="Schriftarten im Detail";
+$navlang["Ghostscript Font Hints"]="Tipps zu Ghostscript-Schriften";
+$navlang["Non-Latin Fonts"]="Nicht-lateinische Schriften";
+$navlang["Basic Font Tests"]="Schriftarten im Vergleich";
+$navlang["Basic Font Tests 2"]="Schriftarten im Vergleich (2)";
+
+$navlang["The Menus"]="Die Men&uuml;s";
+
+$navlang["Importing"]="Importieren";
+$navlang["Importing (Adv.)"]="Importieren (erweitert)";
+$navlang["From OpenOffice.org"]="Import aus OpenOffice.orgF";
+$navlang["Importing SVGs"]="Import von SVG-Dateien";
+
+$navlang["Exporting to PDF"]="Export nach PDF";
+$navlang["Export Workflow"]="Export-Verlauf";
+$navlang["PDF 1.3 and 1.4"]="PDF 1.3 und 1.4";
+$navlang["PDF/X-3"]="PDF/X-3";
+$navlang["PDF Forms"]="PDF-Formulare";
+$navlang["PDF Forms and PHP"]="PDF-Formulare und PHP";
+$navlang["PDF and Javascript"]="PDF und Javascript";
+$navlang["PDF Presentations"]="PDF-Pr&auml;sentationen";
+
+
+$navlang["Printing"]="Drucken";
+$navlang["Print Preview"]="Druckvorschau";
+$navlang["CUPS"]="CUPS";
+$navlang["Preparing for Press"]="Druckvorbereitung";
+$navlang["Pre-Press"]="Druckvorstufe";
+
+$navlang["Color Management"]="Farb-Management";
+$navlang["Color Management 2"]="Farb-Management 2";
+$navlang["Color Management 3"]="Farb-Management 3";
+$navlang["Color Management 4"]="Farb-Management 4";
+
+$navlang["Monitor Profiling"]="Monitor-Profilierung";
+$navlang["Corel PhotoPaint"]="Corel PhotoPaint";
+
+$navlang["DTP Toolbox"]="DTP-Toolbox";
+$navlang["Acrobat Reader"]="Acrobat Reader";
+$navlang["Other PDF Viewers"]="Andere PDF-Betrachter";
+$navlang["Batik"]="Batik";
+$navlang["Gimp"]="Gimp";
+$navlang["Ghostscript"]="Ghostscript";
+$navlang["GSView"]="GSView";
+$navlang["Advanced Ghostscript &#038; GSview"]="Ghostscript (erweitert) &#038; GSview";
+$navlang["Imagemagick"]="Imagemagick";
+$navlang["Inkscape"]="Inkscape";
+$navlang["KSnapshot"]="KSnapshot";
+$navlang["Tracing Tools"]="Vektorisierprogramme";
+$navlang["WINE"]="WINE";
+$navlang["Xnview"]="Xnview";
+
+$navlang["Hints & FAQs"]="Tipps &amp; FAQs";
+$navlang["Help"]="Hilfe";
+$navlang["Miscellaneous"]="Sonstiges";
+$navlang["Development"]="Entwicklung";
+
+$navlang["Screenshots"]="Screenshots";
+
+/*TUTORIALS*/
+$navlang["Tutorials"]="Anleitungen";
+$navlang["Get Started With Scribus"]="Erste Schritte mit Scribus";
+
+/*FOR DEVELOPERS*/
+$navlang["For Developers"]="F&uuml;r Entwickler";
+
+$navlang["Scribus Document Format"]="Scribus Dateiformat";
+$navlang["Scripter"]="Scripter";
+$navlang["Scripter FAQ"]="Scripter-FAQ";
+
+$navlang["Scripter API"]="Scripter-API";
+$navlang["Document Commands"]="Dokumenten-Befehle";
+$navlang["Page Commands"]="Seitenbefehle";
+$navlang["Creating &amp; Destroying Objects"]="Objekte erzeugen &amp; entfernen";
+$navlang["Selecting Objects"]="Objekte ausw&auml;hlen";
+$navlang["Setting Object Properties"]="Objekt-Eigenschaften festlegen";
+$navlang["Getting Object Properties"]="Objekt-Eigenschaften erhalten";
+$navlang["Manipulating Objects"]="Objekte ver&auml;ndern";
+$navlang["Handling Text Frames"]="Textrahmen benutzen";
+$navlang["Other Commands"]="Andere Befehle";
+$navlang["Color Commands"]="Farbbefehle";
+$navlang["Font Commands"]="Schriftarten-Befehle";
+$navlang["Layer Commands"]="Ebenen-Befehle";
+$navlang["PDF Commands"]="PDF-Befehle";
+$navlang["Printer Commands"]="Drucker-Befehle";
+$navlang["Using Dialogs"]="Dialogfenster";
+$navlang["Predefined Constants"]="Vordefinierte Konstanten";
+$navlang["Python Documentation"]="Python-Dokumentation";
+
+$navlang["Plugin How-to"]="Ratgeber: Plugins";
+$navlang["Get Text How-to"]="Ratgeber: GetText-Plugin";
+$navlang["Coding Standards"]="Programmierregeln";
+$navlang["Translation How-to"]="Ratgeber: &Uuml;bersetzen";
+$navlang["1.2.x Translation Statistics"]="1.2.x &Uuml;bersetzungsstatistik";
+$navlang["1.3.x Translation Statistics"]="1.3.x &Uuml;bersetzungsstatistik";
+	
+//OTHER INFORMATION MENU
+$navlang["Other Information"]="Weitere Informationen";
+	
+$navlang["Community"]="Kommunikation";
+$navlang["IRC"]="IRC";
+$navlang["Printers"]="Druckereien";
+$navlang["Contributions"]="Mitwirkung &amp; Beitr&auml;ge";
+$navlang["Scribus Copyright"]="Scribus-Urheberrecht";
+$navlang["Documentation Copyright"]="Schutzrecht der Dokumentation";
+
+//NON MENU ITEMS
+$tr["Page"]="Seite";
+
+?>
