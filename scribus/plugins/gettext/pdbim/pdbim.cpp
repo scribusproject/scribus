188ce0832abc3c3d2a3ca8443f25b03110f21d48
Petr Vaněk <petr@yarpen.cz>
2006-02-13 17:25:07 +0000

    #3234: Palm PDB importer
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@4622 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/plugins/gettext/pdbim/pdbim.cpp
@@ -0,0 +1,226 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+#include <qfiledialog.h>
+#include <qcstring.h>
+#include <qfile.h>
+#include <qfileinfo.h>
+#include <qstring.h>
+#include <qstringlist.h>
+#include <qtextcodec.h>
+#include <qcursor.h>
+
+#include "pdbim.h"
+#include "gtwriter.h"
+#include "gtparagraphstyle.h"
+#include "scribusstructs.h"
+#include "scribus.h"
+
+
+/*! \brief A dummy 0 filler (helper procedure)
+\param p binary structure to fill with 0
+\param len how much 0 to go */
+static void _zero_fill(  Byte *p,  int len )
+{
+	while ( len-- > 0 )
+		*p++ = '\0';
+}
+
+QString FileFormatName()
+{
+	return QObject::tr("Palm PDB Documents", "PDB Importer");
+}
+
+QStringList FileExtensions()
+{
+    return QStringList("pdb");
+}
+
+void GetText(QString filename, QString encoding, bool /* textOnly */, gtWriter *writer)
+{
+	if (filename.isNull())
+		return;
+	qApp->setOverrideCursor(QCursor(Qt::WaitCursor), true);
+	ScMW->mainWindowProgressBar->reset();
+	PdbIm *im = new PdbIm(filename, encoding, writer);
+	im->write();
+	delete im;
+	ScMW->mainWindowProgressBar->reset();
+	qApp->restoreOverrideCursor();
+}
+
+
+PdbIm::PdbIm(const QString& fname, const QString& enc, gtWriter *w)
+{
+	m_buf = new buffer;
+	_zero_fill(m_buf->buf, BUFFER_SIZE);
+	m_buf->len = BUFFER_SIZE;
+	m_buf->position = 0;
+	writer = w;
+	encoding = enc;
+	selectSwap();
+	loadFile(fname);
+}
+
+void PdbIm::write()
+{
+	QTextCodec *codec;
+	if (encoding.isEmpty())
+		codec = QTextCodec::codecForLocale();
+	else
+		codec = QTextCodec::codecForName(encoding);
+	data = codec->toUnicode(data);
+	gtParagraphStyle *pstyle = new gtParagraphStyle(*(writer->getDefaultStyle()));
+	pstyle->setName(writer->getFrameName() + "-" + QObject::tr("PDB_data", "PDB Importer"));
+	writer->append(data, pstyle);
+	delete pstyle;
+}
+
+void PdbIm::loadFile(QString fname)
+{
+	FILE *m_pdfp = fopen(fname, "rb");
+	pdb_header m_header;
+	DWord file_size, offset;
+	doc_record0 m_rec0;
+
+	if (!m_pdfp)
+	{
+		QMessageBox::warning(ScMW, QObject::tr("PDB Import", "PDB Importer"),
+							 "<qt>" + QObject::tr("Could not open file %1", "PDB Importer").arg(fname) + "</qt>",
+							 0);
+		return;
+	}
+	fread( &m_header, PDB_HEADER_SIZE, 1, m_pdfp );
+	if (strncmp(m_header.type, DOC_TYPE, sizeof(m_header.type) ) ||
+		strncmp( m_header.creator, DOC_CREATOR, sizeof(m_header.creator)))
+	{
+		QMessageBox::warning(ScMW, QObject::tr("PDB Import", "PDB Importer"),
+							 "<qt>" + QObject::tr("This file is not recognized as a PDB document propably. Please, report this as a bug if you are sure it is one.", "PDB Importer") + "</qt>",
+							 0);
+		return;
+	}
+
+	// progressbar
+	int num_records = swap_Word( m_header.numRecords ) - 1;
+	ScMW->mainWindowProgressBar->setTotalSteps(num_records);
+	fseek(m_pdfp, PDB_HEADER_SIZE, SEEK_SET);
+	GET_DWord(m_pdfp, offset);
+	fseek(m_pdfp, offset, SEEK_SET);
+	fread(&m_rec0, sizeof(m_rec0), 1, m_pdfp);
+
+	if (swap_Word( m_rec0.version ) == 2 )
+		bCompressed = true;
+
+	fseek( m_pdfp, 0, SEEK_END );
+	file_size = ftell( m_pdfp );
+	for (int rec_num = 1; rec_num <= num_records; ++rec_num )
+	{
+		DWord next_offset;
+
+		ScMW->mainWindowProgressBar->setProgress(rec_num);
+		fseek( m_pdfp, PDB_HEADER_SIZE + PDB_RECORD_HEADER_SIZE * rec_num, SEEK_SET);
+		GET_DWord( m_pdfp, offset );
+		if( rec_num < num_records )
+		{
+			fseek( m_pdfp, PDB_HEADER_SIZE + PDB_RECORD_HEADER_SIZE * (rec_num + 1), SEEK_SET);
+			GET_DWord( m_pdfp, next_offset );
+		}
+		else
+			next_offset = file_size;
+		fseek( m_pdfp, offset, SEEK_SET );
+		// be overly cautious here
+		_zero_fill (m_buf->buf, BUFFER_SIZE);
+		m_buf->position = fread( m_buf->buf, 1, next_offset - offset, m_pdfp );
+
+		if ( bCompressed )
+			uncompress( m_buf );
+
+		m_buf->position = 0;
+		while ( (m_buf->position) < (m_buf->len) )
+		{
+			if (m_buf->buf[m_buf->position] == '\0')
+			{
+				++m_buf->position;
+				continue;
+			}
+			data += m_buf->buf[m_buf->position];
+			++m_buf->position;
+		} 
+	}
+}
+
+void PdbIm::selectSwap()
+{
+	union { char c[2];  Word n; }  w;
+	strncpy(  w.c, "\1\2",     2 );
+
+	if ( w.n == 0x0201 )
+		m_littlendian = true;
+	else
+		m_littlendian = false;
+
+}  
+
+Word PdbIm::swap_Word( Word r )
+{
+	if (m_littlendian)
+		return (r >> 8) | (r << 8);
+	else
+		return r;
+}
+
+DWord PdbIm::swap_DWord( DWord r )
+{
+	if (m_littlendian)
+		return ( (r >> 24) & 0x00FF ) | (r << 24) | ( (r >> 8) & 0xFF00 ) | ( (r << 8) & 0xFF0000 );
+	else
+		return r;
+}
+
+void PdbIm::uncompress( buffer *m_buf )
+{
+	buffer *m_new_buf = new buffer;
+	UT_uint16 i, j;
+	Byte c;
+
+	_zero_fill (m_new_buf->buf, BUFFER_SIZE);
+
+	for (i = j = 0; i < m_buf->position && j < BUFFER_SIZE; )
+	{
+		c = m_buf->buf[ i++ ];
+
+		if ( c >= 1 && c <= 8 )
+			while ( c-- && j < BUFFER_SIZE-1)
+				m_new_buf->buf[ j++ ] = m_buf->buf[ i++ ];
+
+		else if ( c <= 0x7F )
+			m_new_buf->buf[ j++ ] = c;
+
+		else if ( c >= 0xC0 && j < BUFFER_SIZE-2)
+		{
+			m_new_buf->buf[ j++ ] = ' ';
+			m_new_buf->buf[ j++ ] = c ^ 0x80;
+		}
+		else
+		{
+			int di, n;
+			unsigned int temp_c = c;
+		    // c--> temp_c //tomy 2001.11.13 
+			temp_c = (temp_c << 8) ;
+			temp_c = temp_c + m_buf->buf[ i++ ];
+			di = (temp_c & 0x3FFF) >> COUNT_BITS;
+			for (n = (temp_c & ((1 << COUNT_BITS) - 1)) + 3; n-- && j < BUFFER_SIZE; ++j )
+				m_new_buf->buf[ j ] = m_new_buf->buf[ j - di ];
+			temp_c = 0;
+		}
+	}
+	//UT_ASSERT(j <= BUFFER_SIZE);
+
+	memcpy( static_cast<void *>(m_buf->buf), static_cast<void *>(m_new_buf->buf), static_cast<size_t>(j) );
+
+	m_buf->position = j;
+	delete( m_new_buf );
+}
---
+++ scribus/plugins/gettext/pdbim/pdbim.h
@@ -0,0 +1,180 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+#ifndef PDBIM_H
+#define PDBIM_H
+
+#include "pluginapi.h"
+
+class gtWriter;
+class gtParagraphStyle;
+class QString;
+class QStringList;
+
+extern "C" PLUGIN_API void GetText(QString filename, QString encoding, bool textOnly, gtWriter *writer);
+
+extern "C" PLUGIN_API QString FileFormatName();
+
+extern "C" PLUGIN_API QStringList FileExtensions();
+
+
+/*! \brief Define integral type Byte, Word, and DWord to match those on the
+Pilot being 8, 16, and 32 bits, respectively. Max  8-bit unsigned */
+#define U8_MAX 255u
+#if	UCHAR_MAX == U8_MAX
+typedef unsigned char	Byte;
+#else
+#error machine does not seem to support an 8-bit integral type
+#endif
+
+/*! \brief max 16-bit unsigned */
+#define	U16_MAX 65535u
+#if	USHRT_MAX == U16_MAX
+typedef unsigned short	Word;
+#else
+#error machine does not seem to support a 16-bit integral type
+#endif
+
+/*! \brief max 32-bit unsigned */
+#define	U32_MAX 4294967295ul
+#if	USHRT_MAX == U32_MAX
+typedef unsigned short	DWord;
+#elif	UINT_MAX  == U32_MAX
+typedef unsigned int	DWord;
+#elif	ULONG_MAX == U32_MAX
+typedef unsigned long	DWord;
+#else
+#error machine does not seem to support a 32-bit integral type
+#endif
+
+/*! \brief Abiword's internal data types */
+typedef unsigned int UT_uint32;
+typedef unsigned short UT_uint16;
+
+/*! \brief Pilots have a fixed 4K record size */
+#define RECORD_SIZE_MAX	4096
+#define BUFFER_SIZE 4096
+#define COUNT_BITS 3
+#define DISP_BITS 11
+#define	DOC_CREATOR "REAd"
+#define	DOC_TYPE "TEXt"
+/*! \brief 31 chars + 1 null terminator */
+#define	dmDBNameLength 32
+
+/*! \brief PDB document header
+http://www.pyrite.org/doc_format.html
+version  2 bytes  0x0002 if data is compressed, 0x0001 if uncompressed
+spare  2 bytes  purpose unknown (set to 0 on creation)
+length  4 bytes  total length of text before compression
+records  2 bytes  number of text records
+record_size  2 bytes  maximum size of each record (usually 4096; see below)
+position  4 bytes  currently viewed position in the document
+sizes  2*records bytes  record size array
+78 bytes total
+*/
+typedef struct
+{
+	char	name[ dmDBNameLength ];
+	Word	attributes;
+	Word	version;
+	DWord	create_time;
+	DWord	modify_time;
+	DWord	backup_time;
+	DWord	modificationNumber;
+	DWord	appInfoID;
+	DWord	sortInfoID;
+	char	type[4];
+	char	creator[4];
+	DWord	id_seed;
+	DWord	nextRecordList;
+	Word	numRecords;
+} pdb_header;
+
+/*! \brief Some compilers pad structures out to DWord boundaries so using 
+sizeof() doesn't give the intended result.
+*/
+#define PDB_HEADER_SIZE 78
+#define	PDB_RECORD_HEADER_SIZE 8
+
+/*! \brief PDB Document record.
+16 bytes total. */
+typedef struct {
+	Word    version; /* 1 = plain text, 2 = compressed */
+	Word    reserved1;
+	DWord   doc_size; /* in bytes, when uncompressed */
+	Word    numRecords; /* text rec's only; = pdb_header.numRecords-1 */
+	Word    rec_size; /* usually RECORD_SIZE_MAX */
+	DWord   reserved2;
+} doc_record0;
+
+/*! \brief Binary buffer */
+typedef struct {
+	Byte buf[BUFFER_SIZE];
+	UT_uint32   len;
+	UT_uint32   position;
+} buffer;
+
+#define GET_Word(f,n)   { fread( &n, 2, 1, f ); n = swap_Word ( n ); }
+#define GET_DWord(f,n)  { fread( &n, 4, 1, f ); n = swap_DWord( n ); }
+
+/*! \brief An import filter for Palm Documents (PDB files).
+PDB documents are simple non-formatted texts in binary forms used
+mainly for e-books distribution.
+It's taken from Abiword's PDB import/export plugin http://www.abisource.com
+I've simplified the importer guts to fit special Scribus needs.
+\warning This plugin uses a very special low-level IO and memory operations.
+It uses big/little endian handling for bit related operations.
+Welcome in the "macro and gtk hell" ;)
+\note User should specify file encoding correctly.
+\author Copyright (C) 2001 AbiSource, Inc.
+\author Petr Vanek <petr@scribus.info>
+*/
+class PdbIm
+{
+public:
+	/*! \brief Parse and decode the PDB file
+	\param fname a file name of the document
+	\param enc user selected text encoding. See encoding attr.
+	\param w a reference to the gtWriter instance */
+	PdbIm(const QString& fname, const QString& enc, gtWriter *w);
+	~PdbIm(){};
+	/*! \brief Write data into Scribus text frame.
+	User should specify encoding of the imported text - it's recoded here. */
+	void write();
+private:
+	//! \brief Binary buffer for extraction tasks
+	buffer *m_buf;
+	//! \brief Store the extracted text here
+	QString data;
+	//! \brief Name of the codec/encoding to recode
+	QString encoding;
+	//! \brief Imp plugin handler
+	gtWriter *writer;
+	//! \brief A "bit order" flag. True on little endian systems.
+	bool m_littlendian;
+	//! \brief A "document uses that strange compress algorithm" flag.
+	bool bCompressed;
+	
+	/*! \brief Parse the PDB file.
+	\param fname a filename to open */
+	void loadFile(QString fname);
+	/*! \brief Learn which endian to use.
+	It fills the m_littlendian flag */
+	void selectSwap();
+	/*! \brief Recompute the binary value for given endian
+	\param r a binary Word
+	\retval Word a correct endian Word */
+	Word swap_Word(Word r);
+	/*! \brief Recompute the binary value for given endian
+	\param r a binary DWord
+	\retval DWord a correct endian Word */
+	DWord swap_DWord(DWord r);
+	/*! \brief PDB strange decompress algorithm implementation.
+	\param m_buf a reference to the current buffer */
+	void uncompress(buffer *m_buf);
+};
+
+#endif
