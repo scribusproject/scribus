c6a8e8c2d91e2e74940747e70081c0e556fd0d53
Craig Bradney <mrb@scribus.info>
2006-04-07 23:27:35 +0000

    Add in cmake build system. Requires cmake cvs!
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5142 11d20701-8431-0410-a711-e3c959e3b870

---
+++ config.h.cmake
@@ -0,0 +1,14 @@
+#cmakedefine VERSION "1.3.4cvs"
+
+#cmakedefine HAVE_CMS 1
+#cmakedefine HAVE_XML 1
+#cmakedefine CMS_INC "lcms.h"
+#cmakedefine HAVE_CUPS 1
+#cmakedefine HAVE_TIFF 1
+#cmakedefine HAVE_LIBZ 1
+#cmakedefine HAVE_FONTCONFIG 1
+#cmakedefine HAVE_SYS_TYPES_H 1
+#cmakedefine HAVE_SYS_STAT_H 1
+#cmakedefine FT_FREETYPE_H
+#cmakedefine COMPILE_PYTHON 1
+
