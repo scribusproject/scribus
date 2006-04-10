702f89a2e314c9d65f45a172bc114919e51cd21d
Peter Linnell <mrdocs@scribus.info>
2006-04-10 22:53:14 +0000

    fix a misspell
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5214 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -193,7 +193,7 @@ About::About( QWidget* parent ) : QDialog( parent, "About", true, 0 )
 											"<tr><td> </td><td> </td></tr>" +
 											"<tr><td><b>" + tr("Tango Project Icons:").utf8()  + "</b></td><td></td></tr>" +
 											"<tr><td>Andreas Nilsson</td><td>nisses.mail@home.se</td></tr>" +
-											"<tr><td>Jacob Stiener</td><td>jimmac@ximian.com</td></tr>"  + 
+											"<tr><td>Jakub Stiener</td><td>jimmac@ximian.com</td></tr>"  + 
 											"<tr><td> </td><td> </td></tr>" + "</table>"));
 	textView1->setTextFormat( QTextView::RichText );
 	tabLayout->addWidget( textView1 );
