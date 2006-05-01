405199e1686dd0e8422da271a6b1fa60a1e13bb5
Peter Linnell <mrdocs@scribus.info>
2006-05-01 14:34:14 +0000

    bump the date and update the change log
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5374 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -54,8 +54,8 @@ About::About( QWidget* parent ) : QDialog( parent, "About", true, 0 )
 	tabLayout1->addWidget( pixmapLabel1 );
 	buildID = new QLabel( tab, "BB" );
 	buildID->setAlignment(Qt::AlignCenter);
-	QString BUILD_DAY = "27";
-	QString BUILD_MONTH = CommonStrings::april;
+	QString BUILD_DAY = "01";
+	QString BUILD_MONTH = CommonStrings::may;
 	QString BUILD_YEAR = "2006";
 	QString BUILD_TIME = "";
 	QString BUILD_TZ = "";
