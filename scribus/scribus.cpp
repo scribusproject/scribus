130f5e673491854db54abd2cae4b3d1cf8173386
Andreas Vox <avox@arcor.de>
2006-05-02 12:29:03 +0000

    jghali's fix for crash with CMS profiles when reading CMYK tiffs, related to #3716
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5383 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribus.cpp
+++ scribus/scribus.cpp
@@ -3274,6 +3274,13 @@ bool ScribusMainWindow::loadDoc(QString fileName)
 				replacement.append(prefsManager->appPrefs.DCMSset.DefaultImageRGBProfile);
 				doc->CMSSettings.DefaultImageRGBProfile = prefsManager->appPrefs.DCMSset.DefaultImageRGBProfile;
 			}
+			if (!ScCore->InputProfilesCMYK.contains(doc->CMSSettings.DefaultImageCMYKProfile))
+			{
+				cmsWarning = true;
+				missing.append(doc->CMSSettings.DefaultImageCMYKProfile);
+				replacement.append(prefsManager->appPrefs.DCMSset.DefaultImageCMYKProfile);
+				doc->CMSSettings.DefaultImageCMYKProfile = prefsManager->appPrefs.DCMSset.DefaultImageCMYKProfile;
+			}
 			if (!ScCore->InputProfiles.contains(doc->CMSSettings.DefaultSolidColorRGBProfile))
 			{
 				cmsWarning = true;
