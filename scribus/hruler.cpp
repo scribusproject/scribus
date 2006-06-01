ee994a9ac2b94d9c088fe26c299adeaa97d828e0
Jean Ghali <jghali@libertysurf.fr>
2006-06-01 19:36:03 +0000

    fix for #3057: code update following cursor image changes
    git-svn-id: svn://scribus.net/branches/Version133x/Scribus@5631 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/hruler.cpp
+++ scribus/hruler.cpp
@@ -383,7 +383,7 @@ void Hruler::mouseMoveEvent(QMouseEvent *m)
 		}
 		if ((Mpressed) && (RulerCode == 5) && ((m->y() > height()) || (m->y() < 0)))
 		{
-			qApp->setOverrideCursor(QCursor(loadIcon("DelPoint.png"), 4, 3), true);
+			qApp->setOverrideCursor(QCursor(loadIcon("DelPoint.png"), 1, 1), true);
 			return;
 		}
 		qApp->setOverrideCursor(QCursor(ArrowCursor), true);
