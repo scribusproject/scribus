addf000467268950b4e5341f687a3ac7bd89debb
Craig Bradney <mrb@scribus.info>
2006-06-15 22:18:23 +0000

    #3916: Set Insert Glyph table to be readonly so we dont go into edit mode and crash
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5738 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/charselect.cpp
+++ scribus/charselect.cpp
@@ -307,6 +307,7 @@ void CharSelect::run( QWidget* /*parent*/, PageItem *item, ScribusMainWindow *pl
 	zTabelle->setSelectionMode(QTable::NoSelection);
 	zTabelle->setColumnMovingEnabled(false);
 	zTabelle->setRowMovingEnabled(false);
+	zTabelle->setReadOnly(true);
 	scanFont();
 	zAuswahlLayout->addWidget( zTabelle );
 	
