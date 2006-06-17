368d0660bc356d5bd098e4faabef09d1508e41e0
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2006-06-17 17:49:31 +0000

    Fixed Bug #3922
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@5754 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusdoc.cpp
+++ scribus/scribusdoc.cpp
@@ -2496,6 +2496,26 @@ const bool ScribusDoc::copyPageToMasterPage(const int pageNumber, const int left
 			newItem->moveBy(-sourcePage->xOffset() + targetPage->xOffset(), -sourcePage->yOffset() + targetPage->yOffset());
 			newItem->BoundingX = OldBX - sourcePage->xOffset() + targetPage->xOffset();
 			newItem->BoundingY = OldBY - sourcePage->yOffset() + targetPage->yOffset();
+			bool upDtImg = false;
+			if (itemToCopy->pixm.imgInfo.valid)
+			{
+				newItem->pixm.imgInfo = itemToCopy->pixm.imgInfo;
+				upDtImg = true;
+			}
+			if (itemToCopy->effectsInUse.count() != 0)
+			{
+				newItem->effectsInUse = itemToCopy->effectsInUse;
+				upDtImg = true;
+			}
+			if (upDtImg)
+			{
+				int fho = newItem->imageFlippedH();
+				int fvo = newItem->imageFlippedV();
+				LoadPict(newItem->Pfile, newItem->ItemNr, true);
+				newItem->setImageFlippedH(fho);
+				newItem->setImageFlippedV(fvo);
+				newItem->AdjustPictScale();
+			}
 
 		}
 	}
