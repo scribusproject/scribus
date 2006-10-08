452071864973edeff547becbcc312ad1af9139aa
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2006-10-08 10:07:41 +0000

    Fixed Bug #4374
    git-svn-id: svn://scribus.net/branches/Version133x/Scribus@6594 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pdflib.cpp
+++ scribus/pdflib.cpp
@@ -3810,9 +3810,9 @@ QString PDFlib::PDF_Gradient(PageItem *currItem)
 		case 6:
 		case 7:
 			StartX = currItem->GrStartX;
-			StartY = currItem->GrStartY;
+			StartY = -currItem->GrStartY;
 			EndX = currItem->GrEndX;
-			EndY = currItem->GrEndY;
+			EndY = -currItem->GrEndY;
 			break;
 	}
 	StopVec.clear();
