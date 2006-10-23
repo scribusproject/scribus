3582a8c55010f481d3f6495a708a4aa97d03890e
Andreas Vox <avox@arcor.de>
2006-10-23 11:16:53 +0000

    missing header file
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@6734 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/desaxe/saxhandler.h
@@ -0,0 +1,31 @@
+/*
+ *  saxhandler.h
+ *  
+ *
+ *  Created by Andreas Vox on 21.09.06.
+ *  Copyright 2006 under GPL2. All rights reserved.
+ *
+ */
+
+
+
+#ifndef SAXHANDLER_H
+#define SAXHANDLER_H
+
+#include "desaxe_conf.h"
+
+/**
+  Interface class which describes the ability of reading a SAX stream.
+  Methods for handling namespaces and encodings might be added later.
+ */
+class SaxHandler {
+public:
+	virtual void beginDoc() = 0;
+	virtual void endDoc() = 0;
+	virtual void begin(Xml_string tag, Xml_attr attr) = 0;
+	virtual void end(Xml_string tag) = 0;
+	virtual void chars(Xml_string text) = 0;
+	virtual ~SaxHandler() {}
+};
+
+#endif
