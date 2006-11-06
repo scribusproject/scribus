503ef2711d52364360613fc8a274e980ea3c4eb5
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2006-11-06 22:46:06 +0000

    Fixed PDF Producer string.
    git-svn-id: svn://scribus.net/branches/Version133x/Scribus@6889 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pdflib.cpp
+++ scribus/pdflib.cpp
@@ -500,7 +500,7 @@ bool PDFlib::PDF_Begin_Doc(const QString& fn, SCFonts &AllFonts, QMap<QString,in
 	QString IDg(Datum);
 	IDg += Options.Datei;
 	IDg += "Scribus "+QString(VERSION);
-	IDg += "Libpdf for Scribus "+QString(VERSION);
+	IDg += "Scribus PDF Library "+QString(VERSION);
 	IDg += doc.documentInfo.getTitle();
 	IDg += doc.documentInfo.getAuthor();
 	IDg += "/False";
@@ -544,7 +544,7 @@ bool PDFlib::PDF_Begin_Doc(const QString& fn, SCFonts &AllFonts, QMap<QString,in
 	Datum += tmp;
 	StartObj(2);
 	PutDoc("<<\n/Creator "+EncString("(Scribus "+QString(VERSION)+")",2)+"\n");
-	PutDoc("/Producer "+EncString("Scribus PDF Library "+QString(VERSION)+")",2)+"\n");
+	PutDoc("/Producer "+EncString("(Scribus PDF Library "+QString(VERSION)+")",2)+"\n");
 	QString docTitle = doc.documentInfo.getTitle();
 	if ((Options.Version == 12) && (docTitle.isEmpty()))
 		PutDoc("/Title "+EncString("("+doc.DocName+")",2)+"\n");
