88887daa62ce3a06218678cd3cc930824d52bef9
Craig Bradney <mrb@scribus.info>
2006-11-12 00:54:08 +0000

    CMake compile issues - include mocs where u should!
    git-svn-id: svn://scribus.net/branches/Version133x/Scribus@6973 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/fileloader/oldscribusformat/oldscribusformat.cpp
+++ scribus/plugins/fileloader/oldscribusformat/oldscribusformat.cpp
@@ -5,6 +5,7 @@ a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
 #include "oldscribusformat.h"
+#include "oldscribusformat.moc"
 #include "oldscribusformatimpl.h"
 
 #include "scconfig.h"
