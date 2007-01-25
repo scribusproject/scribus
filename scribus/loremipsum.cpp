0d6b4608be1c30a1238fad4736375666496a60ef
Andreas Vox <avox@arcor.de>
2007-01-25 21:01:11 +0000

    fix paragraph separator
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@8064 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/loremipsum.cpp
+++ scribus/loremipsum.cpp
@@ -99,7 +99,7 @@ QString LoremParser::createLorem(uint parCount)
 	QString lorem(loremIpsum[0]);
 	if (!loremIpsum.isEmpty())
 		for (uint i = 1; i < parCount + 1; ++i)
-			lorem += loremIpsum[rand()%loremIpsum.count()] + '\n';
+			lorem += loremIpsum[rand()%loremIpsum.count()] + SpecialChars::PARSEP;
 	return lorem.stripWhiteSpace();
 }
 
