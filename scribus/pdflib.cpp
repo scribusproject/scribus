0cf5fe36903430c51e0d3a96056320fb335bfb04
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2007-02-03 09:26:20 +0000

    Fixed Bug #5288
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@8112 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pdflib.cpp
+++ scribus/pdflib.cpp
@@ -6175,12 +6175,10 @@ void PDFlib::PDF_End_Doc(const QString& PrintPr, const QString& Name, int Compon
 			Inhal = "";
 			Bmc++;
 			Inhal += QString::number(ip->ItemNr+Basis)+ " 0 obj\n";
-			QString encText = QString(QChar(254))+QString(QChar(255));
+			QString encText = "";
 			for (uint telen = 0; telen < ip->Titel.length(); telen++)
 			{
-				QChar ch = ip->Titel.at(telen);
-				encText += QChar(ch.row());
-				encText += QChar(ch.cell());
+				encText += ip->Titel.at(telen);
 			}
 			Inhal += "<<\n/Title "+EncStringUTF16("("+encText+")", ip->ItemNr+Basis)+"\n";
 			if (ip->Pare == 0)
