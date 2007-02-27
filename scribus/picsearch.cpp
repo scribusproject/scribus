bb667c7138a4d4408fbcf838d5bdd49550046009
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2007-02-27 13:59:12 +0000

    Manage Picture Dialog: better preview in the search results dialog.
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@8381 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/picsearch.cpp
+++ scribus/picsearch.cpp
@@ -7,10 +7,12 @@ for which a new license (GPL+exception) is in place.
 #include "picsearch.h"
 #include "picsearch.moc"
 #include <qpixmap.h>
+#include <qpainter.h>
 #include "scribusstructs.h"
 #include "scimage.h"
 #include "commonstrings.h"
 #include "scpaths.h"
+#include "util.h"
 
 extern QPixmap loadIcon(QString nam);
 
@@ -58,21 +60,76 @@ void PicSearch::foundFilesBox_clicked(QListBoxItem *c)
 
 void PicSearch::createPreview()
 {
-	QPixmap pm;
+	QPixmap pm(200, 200);
+	QFileInfo fi = QFileInfo(currentImage);
+	int w = 200;
+	int h = 200;
+	bool mode = false;
+	QString ext = fi.extension(false).lower();
+	if (ext.isEmpty())
+		ext = getImageType(currentImage);
 	ScImage im;
+	//No doc to send data anyway, so no doc to get into scimage.
 	CMSettings cms(0, "", 0);
-	im.LoadPicture(currentImage, cms, false, false, ScImage::RGBData, 72);
-	QImage im2;
-	if ((im.width() > 200) || (im.height() > 200))
+	if (im.LoadPicture(currentImage, cms, false, false, ScImage::Thumbnail, 72, &mode))
 	{
-		double sx = im.width() / 200.0;
-		double sy = im.height() / 200.0;
-		im2 = sy < sx ?
-			im2 = im.smoothScale(qRound(im.width() / sx), qRound(im.height() / sx)) :
-			im2 = im.smoothScale(qRound(im.width() / sy), qRound(im.height() / sy));
+		int ix,iy;
+		if ((im.imgInfo.exifDataValid) && (!im.imgInfo.exifInfo.thumbnail.isNull()))
+		{
+			ix = im.imgInfo.exifInfo.width;
+			iy = im.imgInfo.exifInfo.height;
+		}
+		else
+		{
+			ix = im.width();
+			iy = im.height();
+		}
+		int xres = im.imgInfo.xres;
+		int yres = im.imgInfo.yres;
+		QString tmp = "";
+		QString tmp2 = "";
+		QImage im2;
+		if ((ix > w-5) || (iy > h-44))
+		{
+			double sx = im.width() / static_cast<double>(w-5);
+			double sy = im.height() / static_cast<double>(h-44);
+			im2 = sy < sx ?  im.smoothScale(qRound(im.width() / sx), qRound(im.height() / sx)) :
+								im.smoothScale(qRound(im.width() / sy), qRound(im.height() / sy));
+		}
+		else
+			im2 = im.qImage(); // no need to copy
+		QPainter p;
+		QBrush b(QColor(205,205,205), loadIcon("testfill.png"));
+		p.begin(&pm);
+		p.fillRect(0, 0, w, h-44, b);
+		p.fillRect(0, h-44, w, 44, QColor(255, 255, 255));
+		p.drawImage((w - im2.width()) / 2, (h - 44 - im2.height()) / 2, im2);
+		p.drawText(2, h-29, tr("Size:")+" "+tmp.setNum(ix)+" x "+tmp2.setNum(iy));
+		p.drawText(2, h-17, tr("Resolution:")+" "+tmp.setNum(xres)+" x "+tmp2.setNum(yres)+" "+ tr("DPI"));
+		QString cSpace;
+		if (((ext == "pdf") || (ext == "eps") || (ext == "epsi") || (ext == "ps")) && (im.imgInfo.type != 7))
+			cSpace = tr("Unknown");
+		else
+		{
+			switch (im.imgInfo.colorspace)
+			{
+				case 0:
+					cSpace = tr("RGB");
+					break;
+				case 1:
+					cSpace = tr("CMYK");
+					break;
+				case 2:
+					cSpace = tr("Grayscale");
+					break;
+				case 3:
+					cSpace = tr("Duotone");
+					break;
+			}
+		}
+		p.drawText(2, h-5, tr("Colorspace:")+" "+cSpace);
+		p.end();
+		repaint();
 	}
-	else
-		im2 = im.qImage();
-	pm.convertFromImage(im2);
 	previewLabel->setPixmap(pm);
 }
