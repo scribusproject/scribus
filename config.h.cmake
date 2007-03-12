5e407c7432a1b02dec920c125d131c500844ed6e
Craig Bradney <mrb@scribus.info>
2007-03-12 20:24:49 +0000

    Hopefuly this commit is correct now... gr
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@8498 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.3.4cvs"
+#cmakedefine VERSION "1.3.5cvs"
 
 #cmakedefine BUILD_MAC_BUNDLE
 #cmakedefine HAVE_CMS 1
