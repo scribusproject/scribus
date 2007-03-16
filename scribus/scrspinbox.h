e983cee428bfbdd192f8b1c010f12d016f516c81
Craig Bradney <mrb@scribus.info>
2007-03-16 23:34:18 +0000

    First commit of ScrSpinBox using Qt4 to replace MSpinBox. Will integrate in other code soon. Code here not 100% complete but tested for brokenness shows safe for broken distros
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@8568 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/scrspinbox.h
@@ -0,0 +1,51 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+/***************************************************************************
+ *   Craig Bradney, cbradney@zip.com.au                                    *
+ ***************************************************************************/
+
+#ifndef SCRSPINBOX
+#define SCRSPINBOX
+
+#include <QDoubleSpinBox>
+#include <QLineEdit>
+#include <QWidget>
+
+#include "fparser.h"
+
+//!brief ScrSpinBox is a Qt4 replacement for our old MSpinBox using Qt3
+class ScrSpinBox : public QDoubleSpinBox
+{
+	Q_OBJECT
+	public:
+		ScrSpinBox(QWidget *parent, int unitIndex);
+		ScrSpinBox(double minValue, double maxValue, QWidget *pa, int unitIndex);
+		~ScrSpinBox();
+		
+		//overridden members
+		double valueFromText ( const QString & text ) const;
+		QValidator::State validate ( QString & input, int & pos ) const;
+		
+		//custom
+		void init(int unitIndex);
+		void setNewUnit(int unitIndex);
+		double getValue(int unitIndex=0);
+		
+	public slots:
+		void setValues(double min, double max, int deci, double val);
+		void getValues(double *min, double *max, int *deci, double *val);
+	
+	protected:
+		void setParameters( int s );
+		uint m_unitIndex;
+		bool eventFilter ( QObject * watched, QEvent * event );
+		
+	protected slots:
+		void textChanged();
+
+};
+#endif
