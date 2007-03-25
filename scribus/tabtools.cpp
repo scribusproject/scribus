7258973d9092f7d7e360517b43d8d33afbd225ad
Jean Ghali <jghali@libertysurf.fr>
2007-03-25 17:59:47 +0000

    fix #5480
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@8709 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/tabtools.cpp
+++ scribus/tabtools.cpp
@@ -512,8 +512,8 @@ void TabTools::enableSignals(bool on)
 		connect(colorComboTextBackground, SIGNAL(activated(int)), this, SLOT(setSample()));
 		connect(shadingTextBack, SIGNAL(valueChanged(int)), this, SLOT(setSample()));
 		connect(shadingText, SIGNAL(valueChanged(int)), this, SLOT(setSample()));
-		connect(buttonGroup3, SIGNAL(clicked(int)), this, SLOT(changeImageScalingFree(int)));
-		connect(buttonGroup5, SIGNAL(clicked(int)), this, SLOT(changeImageScalingRatio(int)));
+		connect(buttonGroup3, SIGNAL(toggled(bool)), this, SLOT(changeImageScalingFree(bool)));
+		connect(buttonGroup5, SIGNAL(toggled(bool)), this, SLOT(changeImageScalingRatio(bool)));
 		connect(chainButton, SIGNAL(clicked()), this, SLOT(toggleChain()));
 		connect(scalingHorizontal, SIGNAL(valueChanged(int)), this, SLOT(hChange()));
 		connect(scalingVertical, SIGNAL(valueChanged(int)), this, SLOT(vChange()));
@@ -534,8 +534,8 @@ void TabTools::enableSignals(bool on)
 		disconnect(colorComboTextBackground, SIGNAL(activated(int)), this, SLOT(setSample()));
 		disconnect(shadingTextBack, SIGNAL(valueChanged(int)), this, SLOT(setSample()));
 		disconnect(shadingText, SIGNAL(valueChanged(int)), this, SLOT(setSample()));
-		disconnect(buttonGroup3, SIGNAL(clicked(int)), this, SLOT(changeImageScalingFree(int)));
-		disconnect(buttonGroup5, SIGNAL(clicked(int)), this, SLOT(changeImageScalingRatio(int)));
+		disconnect(buttonGroup3, SIGNAL(toggled(bool)), this, SLOT(changeImageScalingFree(bool)));
+		disconnect(buttonGroup5, SIGNAL(toggled(bool)), this, SLOT(changeImageScalingRatio(bool)));
 		disconnect(chainButton, SIGNAL(clicked()), this, SLOT(toggleChain()));
 		disconnect(scalingHorizontal, SIGNAL(valueChanged(int)), this, SLOT(hChange()));
 		disconnect(scalingVertical, SIGNAL(valueChanged(int)), this, SLOT(vChange()));
@@ -870,7 +870,7 @@ void TabTools::vChange()
 		scalingHorizontal->setValue(scalingVertical->value());
 }
 
-void TabTools::changeImageScalingFree(int)
+void TabTools::changeImageScalingFree(bool)
 {
 	if (buttonGroup3->isChecked())
 		buttonGroup5->setChecked(false);
@@ -878,7 +878,7 @@ void TabTools::changeImageScalingFree(int)
 		buttonGroup5->setChecked(true);
 }
 
-void TabTools::changeImageScalingRatio(int)
+void TabTools::changeImageScalingRatio(bool)
 {
 	if (buttonGroup5->isChecked())
 		buttonGroup3->setChecked(false);
