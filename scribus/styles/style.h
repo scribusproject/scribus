331a8ea35d62cb3ab78a85de761c97c09377a19c
Craig Bradney <mrb@scribus.info>
2007-04-02 22:07:56 +0000

    Add in extra parameters as discussed with Jean
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@8803 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/styles/style.h
+++ scribus/styles/style.h
@@ -16,9 +16,9 @@ inline
 void  Style::desaxeRules(Xml_string prefixPattern, desaxe::Digester& ruleset, Xml_string elemtag)
 {
 	Xml_string stylePrefix(Digester::concat(prefixPattern,elemtag));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setName, "name", &parse<const QString&>));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setParent, "parent", &parse<const QString&>));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setShortcut, "shortcut", &parse<const QString&>));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setName, "name", &parse<const QString&>, ""));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setParent, "parent", &parse<const QString&>, ""));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setShortcut, "shortcut", &parse<const QString&>, ""));
 }
 
 #endif
