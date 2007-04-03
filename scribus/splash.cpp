1519eb9c224523ddf12bf7f742ff2e68835b84ab
Jean Ghali <jghali@libertysurf.fr>
2007-04-03 19:43:22 +0000

    #5514: use lucida sans unicode for splash screen, works better than lucida sans for languages such as russian
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@8818 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/splash.cpp
+++ scribus/splash.cpp
@@ -62,7 +62,7 @@ void SplashScreen::setStatus( const QString &message )
 	QPixmap textPix(pix);
 	QPainter painter( &textPix);// Qt4, this );
 #if defined _WIN32
-	QFont font("Lucida Sans", 10);
+	QFont font("Lucida Sans Unicode", 10);
 #elif defined(__INNOTEK_LIBC__)
 	QFont font("WarpSans", 9);
 #else
