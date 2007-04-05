58b2983e4778ecc33ae39a58c4161421c10ec0f0
Andreas Vox <avox@arcor.de>
2007-04-05 20:17:48 +0000

    be me circumspect when converting Xml_string -> const char*
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@8846 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/desaxe/desaxe_conf.h
+++ scribus/desaxe/desaxe_conf.h
@@ -28,7 +28,7 @@ typedef QMap<Xml_string, Xml_string> Xml_attr;
 
 inline Xml_string Xml_key(Xml_attr::iterator& it) { return it.key(); }
 inline Xml_string Xml_data(Xml_attr::iterator& it) { return it.data(); }
-
+inline const char* fromXMLString(const Xml_string& s) { return s.ascii(); }
 #else
 
 #include <string>
@@ -40,6 +40,7 @@ typedef std::map<Xml_string, Xml_string> Xml_attr;
 
 inline Xml_string Xml_key(Xml_attr::iterator& it) { return it->first; }
 inline Xml_string Xml_data(Xml_attr::iterator& it) { return it->second; }
+inline const char* fromXMLString(const Xml_string& s) { return s.c_str(); }
 
 #endif
 
