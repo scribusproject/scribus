c01fe7616489624d5c9c7c9d14e8505a8f5a69a3
Craig Bradney <mrb@scribus.info>
2007-04-26 22:03:03 +0000

    Comment out qdebug now scrspinbox is behaving better
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@9091 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scrspinbox.cpp
+++ scribus/scrspinbox.cpp
@@ -190,7 +190,7 @@ QValidator::State ScrSpinBox::validate ( QString & input, int & pos ) const
 
 void ScrSpinBox::textChanged()
 {
-	qDebug() << "v:" << value() << "t:" << text() << "ct:" << cleanText();
+	//qDebug() << "v:" << value() << "t:" << text() << "ct:" << cleanText();
 }
 
 bool ScrSpinBox::eventFilter( QObject* watched, QEvent* event )
