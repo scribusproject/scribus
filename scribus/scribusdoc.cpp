bdbb998eac0f8e0ab0a505a01e50c16259257dc5
Jean Ghali <jghali@libertysurf.fr>
2007-05-14 18:12:20 +0000

    fix #5676 : deleting colors and replacing them doesn't work
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@9432 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusdoc.cpp
+++ scribus/scribusdoc.cpp
@@ -834,9 +834,9 @@ void ScribusDoc::replaceNamedResources(ResourceCollection& newNames)
 	{
 		for (uint i=0; i < itemlist->count(); ++i)
 		{
-			PageItem_TextFrame * currItem = itemlist->at(i)->asTextFrame();
+			PageItem * currItem = itemlist->at(i);
 			if (currItem)
-				currItem->itemText.replaceNamedResources(newNames);
+				currItem->replaceNamedResources(newNames);
 		}
 		if (itemlist == &MasterItems)
 			itemlist = &DocItems;
