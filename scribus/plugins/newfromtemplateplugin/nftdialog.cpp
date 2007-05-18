e0abd9001784c6179b2f0f82a51d76253c2beb1e
Petr Vaněk <petr@yarpen.cz>
2007-05-18 15:33:41 +0000

    first part of the full Qt4 migration. I'll continue later from home.
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@9510 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/newfromtemplateplugin/nftdialog.cpp
+++ scribus/plugins/newfromtemplateplugin/nftdialog.cpp
@@ -11,24 +11,28 @@ for which a new license (GPL+exception) is in place.
 //#include "nftdialog.moc"
 #include "scconfig.h"
 #include "scribusapi.h"
-#include "commonstrings.h"
-//Added by qt3to4:
-#include <Q3HBoxLayout>
-#include <Q3Frame>
-#include <QPixmap>
-#include <Q3PopupMenu>
-#include <Q3VBoxLayout>
+// #include "commonstrings.h"
+// //Added by qt3to4:
+// #include <Q3HBoxLayout>
+// #include <Q3Frame>
+// #include <QPixmap>
+// #include <Q3PopupMenu>
+// #include <Q3VBoxLayout>
+#include <QPushButton>
+
 
 extern QPixmap SCRIBUS_API loadIcon(QString nam);
 
+
 nftdialog::nftdialog(QWidget* parent, QString lang, QString templateDir) : QDialog(parent, "nftdialog", true)
 {
+	setupUi(this);
+
 	settings = new nftsettings(lang, templateDir);
-	
-	setCaption( tr("New From Template"));
+
 	setIcon(loadIcon("AppIcon.png"));
 	
-	Q3BoxLayout* blo = new Q3VBoxLayout(this,0,5,"blo");
+/*	Q3BoxLayout* blo = new Q3VBoxLayout(this,0,5,"blo");
 	QWidget* top = new QWidget(this);
 	top->setMaximumHeight(5);
 	top->setMinimumHeight(5);
@@ -40,11 +44,11 @@ nftdialog::nftdialog(QWidget* parent, QString lang, QString templateDir) : QDial
 	blo->addWidget(top);
 	blo->addWidget(middle);
 	blo->addWidget(bottom);
-	Q3BoxLayout* bottomlo = new Q3HBoxLayout(bottom,0,5,"bottomlo");
+	Q3BoxLayout* bottomlo = new Q3HBoxLayout(bottom,0,5,"bottomlo");*/
 // 	settingsButton = new QPushButton(tr("Settings"),bottom,"settingsButton");
 // 	bottomlo->insertSpacing(0,5);
 // 	bottomlo->addWidget(settingsButton);
-	bottomlo->addStretch(10);
+/*	bottomlo->addStretch(10);
 	okButton = new QPushButton( CommonStrings::tr_OK,bottom,"okButton");
 	okButton->setEnabled(false);
 	bottomlo->addWidget(okButton);
@@ -55,22 +59,24 @@ nftdialog::nftdialog(QWidget* parent, QString lang, QString templateDir) : QDial
 	
 	mainlo = new Q3HBoxLayout(middle,0,5,"mainlo");
 	QSplitter* splitter = new QSplitter(middle);
-	mainlo->addWidget(splitter);
-	
-	categoryList = new Q3ListBox(splitter,"categoryList",0);
+	mainlo->addWidget(splitter);*/
+/*	
+	categoryList = new Q3ListBox(splitter,"categoryList",0);*/
 	categoryList->setMinimumWidth(130);
 	categoryList->setMaximumWidth(200);
 	
-	tnailGrid = new Q3IconView(splitter,"tnailGrid",0);
+// 	tnailGrid = new Q3IconView(splitter,"tnailGrid",0);
 	tnailGrid->setMinimumWidth(300);
 	tnailGrid->setSpacing(5);
-	tnailGrid->setItemsMovable(false);
-	
+// 	tnailGrid->setItemsMovable(false);
+
+/* TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	popUp = new Q3PopupMenu(tnailGrid, "popUp");
 	popUp->insertItem( tr("&Remove"), this, SLOT(removeTemplate()));
 	popUp->insertItem( tr("&Open"), this, SLOT(exitOK()));
+*/
 	
-	QWidget* dtTmp = new QWidget(splitter, "dtTmp", 0);
+/*	QWidget* dtTmp = new QWidget(splitter, "dtTmp", 0);
 	dtTmp->setMinimumWidth(235);
 	mainlo->insertSpacing(0,5);
 	mainlo->insertSpacing(-1,5);
@@ -102,22 +108,24 @@ nftdialog::nftdialog(QWidget* parent, QString lang, QString templateDir) : QDial
 	aboutLabel->setFrameShape(Q3Frame::Panel);
 	aboutLabel->setFrameShadow(Q3Frame::Sunken);
 	detaillo->addWidget(aboutLabel);
-	aboutLabel->setHidden(true);
+	aboutLabel->setHidden(true);*/
 	setupAbout();
 	
-	dtbarlo = new Q3HBoxLayout(detailBar,0,5);
-	imgButton = new QToolButton(detailBar,"imgButton");
-	QIcon imgSet(loadIcon("16/image-x-generic.png"));
-	imgButton->setIconSet(imgSet);
+/*	dtbarlo = new Q3HBoxLayout(detailBar,0,5);
+	imgButton = new QToolButton(detailBar,"imgButton");*/
+// 	QIcon imgSet(loadIcon("16/image-x-generic.png"));
+	toolBox->setItemIcon(0, loadIcon("16/information.png"));
+/*	imgButton->setIconSet(imgSet);
 	imgButton->setToggleButton(true);
 	imgButton->setMinimumWidth(30);
 	imgButton->setMinimumHeight(30);
 	imgButton->setMaximumWidth(30);
 	imgButton->setMaximumHeight(30);
 	dtbarlo->addWidget(imgButton);
-	infoButton = new QToolButton(detailBar,"infoButton");
-	QIcon imgSet2(loadIcon("16/information.png"));
-	infoButton->setIconSet(imgSet2);
+	infoButton = new QToolButton(detailBar,"infoButton");*/
+// 	QIcon imgSet2(loadIcon("16/information.png"));
+	toolBox->setItemIcon(1, loadIcon("16/image-x-generic.png"));
+/*	infoButton->setIconSet(imgSet2);
 	infoButton->setToggleButton(true);
 	infoButton->setMinimumWidth(30);
 	infoButton->setMinimumHeight(30);
@@ -127,27 +135,29 @@ nftdialog::nftdialog(QWidget* parent, QString lang, QString templateDir) : QDial
 	dtbarlo->addWidget(infoButton);
 	dtbarlo->addStretch(10);
 	dtbarlo->insertSpacing(0,5);
-	aboutButton = new QToolButton(detailBar,"aboutButton");
-	QIcon imgSet3(loadIcon("16/help-browser.png"));
-	aboutButton->setIconSet(imgSet3);
+	aboutButton = new QToolButton(detailBar,"aboutButton");*/
+// 	QIcon imgSet3(loadIcon("16/help-browser.png"));
+	toolBox->setItemIcon(2, loadIcon("16/help-browser.png"));
+/*	aboutButton->setIconSet(imgSet3);
 	aboutButton->setToggleButton(true);
 	aboutButton->setMinimumWidth(30);
 	aboutButton->setMinimumHeight(30);
 	aboutButton->setMaximumWidth(30);
 	aboutButton->setMaximumHeight(30);
-	dtbarlo->addWidget(aboutButton);
+	dtbarlo->addWidget(aboutButton);*/
 	
 	// Signals and Slots Connections
-	connect(categoryList, SIGNAL(selectionChanged()), this, SLOT(setTNails()));
-	connect(infoButton, SIGNAL(toggled(bool)), this, SLOT(infoToggle()));
-	connect(imgButton, SIGNAL(toggled(bool)), this, SLOT(imgToggle()));
-	connect(aboutButton, SIGNAL(toggled(bool)), this, SLOT(aboutToggle()));
-	connect(okButton, SIGNAL(clicked()), this, SLOT(exitOK()));
-	connect(cancelButton, SIGNAL(clicked()), this, SLOT(exitCancel()));
-	connect(tnailGrid, SIGNAL(selectionChanged(Q3IconViewItem*)),this,SLOT(setInfo(Q3IconViewItem*)));
-	connect(tnailGrid, SIGNAL(doubleClicked(Q3IconViewItem*)),this,SLOT(exitOK()));
-	connect(tnailGrid, SIGNAL(rightButtonPressed(Q3IconViewItem*, const QPoint&)), 
-	        this, SLOT(showPopup(Q3IconViewItem*, const QPoint&)));
+	connect(categoryList, SIGNAL(itemSelectionChanged()), this, SLOT(setTNails()));
+// 	connect(infoButton, SIGNAL(toggled(bool)), this, SLOT(infoToggle()));
+// 	connect(imgButton, SIGNAL(toggled(bool)), this, SLOT(imgToggle()));
+// 	connect(aboutButton, SIGNAL(toggled(bool)), this, SLOT(aboutToggle()));
+// 	connect(okButton, SIGNAL(clicked()), this, SLOT(exitOK()));
+// 	connect(cancelButton, SIGNAL(clicked()), this, SLOT(exitCancel()));
+	connect(tnailGrid, SIGNAL(itemSelectionChanged()), this, SLOT(setInfo()));
+	connect(tnailGrid, SIGNAL(itemDoubleClicked(QListWidgetItem*)), this, SLOT(accept()));
+// FIXME: !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// 	connect(tnailGrid, SIGNAL(rightButtonPressed(Q3IconViewItem*, const QPoint&)), 
+// 	        this, SLOT(showPopup(Q3IconViewItem*, const QPoint&)));
 	
 	setupListItems();
 	setupCategories();
@@ -157,18 +167,18 @@ void nftdialog::setupCategories()
 {
 	QString categories("");
 	categoryList->clear();
+	categoryList->addItem(tr("All"));
 	for (uint i = 0; i < settings->templates.size(); i++)
 	{
 		if ((categories.find(settings->templates[i]->templateCategory) == -1) &&
 		    (!settings->templates[i]->isDeleted))
 		{
-			categoryList->insertItem(settings->templates[i]->templateCategory);
+			categoryList->addItem(settings->templates[i]->templateCategory);
 			categories += settings->templates[i]->templateCategory;
 		}
 	}
-	categoryList->sort();
-	categoryList->insertItem(QString( tr("All")), 0 );
-	categoryList->setCurrentItem(0);
+	categoryList->sortItems();
+	categoryList->setCurrentRow(0);
 }
 
 void nftdialog::setupListItems()
@@ -191,16 +201,16 @@ void nftdialog::setTNails()
 		tnailGrid->clear();
 		for (uint i = 0; i < iconItems.size(); ++i) 
 		{
-			Q3IconViewItem* tmpQIVI = new Q3IconViewItem(tnailGrid,
-			                                           iconItems[i]->first->name, 
-			                                           QPixmap(iconItems[i]->first->tnail));
+			QListWidgetItem* tmpQIVI = new QListWidgetItem(QPixmap(iconItems[i]->first->tnail),
+			                                           iconItems[i]->first->name, tnailGrid);
+// 			tnailGrid->addItem(tmpQIVI);
 			iconItems[i]->second = tmpQIVI;
 		}
-		tnailGrid->sort();
+		tnailGrid->sortItems();
 		return;
 	}
 	
-	QString curtype = categoryList->text(categoryList->currentItem());
+	QString curtype(categoryList->currentItem()->text());
 	if (curtype != QString::null)
 	{
 		tnailGrid->clear();
@@ -208,20 +218,21 @@ void nftdialog::setTNails()
 		{
 			if (curtype.find(iconItems[i]->first->templateCategory) != -1)
 			{
-				Q3IconViewItem* tmpQIVI = new Q3IconViewItem(tnailGrid,
-			                                              iconItems[i]->first->name, 
-			                                              QPixmap(iconItems[i]->first->tnail));
-				iconItems[i]->second = tmpQIVI;
+			QListWidgetItem* tmpQIVI = new QListWidgetItem(QPixmap(iconItems[i]->first->tnail),
+			                                           iconItems[i]->first->name, tnailGrid);
+// 			tnailGrid->addItem(tmpQIVI);
+			iconItems[i]->second = tmpQIVI;
 			} 
 			else
 				iconItems[i]->second = NULL;
 		}
-		tnailGrid->sort();
+		tnailGrid->sortItems();
 	}
 }
 
-void nftdialog::setInfo(Q3IconViewItem* item) 
+void nftdialog::setInfo() 
 {
+	QListWidgetItem* item = tnailGrid->currentItem();
 	getCurrentDocumentTemplate(item);
 	QString infoText = "<b>"+ tr("Name")+"</b><br>";
 	infoText += currentDocumentTemplate->name + "<br>";
@@ -240,92 +251,92 @@ void nftdialog::setInfo(Q3IconViewItem* item)
 	infoText += "<b>"+ tr("Author")+"</b><br>";
 	infoText += currentDocumentTemplate->author + "<br>";
 	infoText += currentDocumentTemplate->email + "<br>";
-	infoLabel->setText(infoText);
-	imgLabel->clear();
+	textBrowser->setText(infoText);
+	imageView->clear();
 	QPixmap tmplImg(currentDocumentTemplate->img);
-	Q3IconViewItem* qvi = new Q3IconViewItem(imgLabel,currentDocumentTemplate->name,tmplImg);
-	qvi->setRenameEnabled(false);
-	qvi->setDragEnabled(false);
-	qvi->setDropEnabled(false);
+	/*QListWidgetItem* qvi = */new QListWidgetItem(tmplImg, currentDocumentTemplate->name, imageView);
+// 	qvi->setRenameEnabled(false);
+// 	qvi->setDragEnabled(false);
+// 	qvi->setDropEnabled(false);
 	
-	if (!okButton->isEnabled())
+	if (!buttonBox->button(QDialogButtonBox::Ok)->isEnabled())
 	{
-		okButton->setEnabled(true);
-		okButton->setDefault(true);
+		buttonBox->button(QDialogButtonBox::Ok)->setEnabled(true);
+		buttonBox->button(QDialogButtonBox::Ok)->setDefault(true);
 	}
 
 }
 
-void nftdialog::infoToggle()
-{
-	if (infoButton->isDown())
-	{
-		tmpSpacer->setHidden(true);
-		infoLabel->setHidden(false);
-		if (imgButton->isDown())
-			imgButton->toggle();
-		if (aboutButton->isDown())
-			aboutButton->toggle();
-	} 
-	else
-	{
-		if (imgButton->isDown() == false)
-			tmpSpacer->setHidden(false);
-		infoLabel->setHidden(true);
-	}
-	
-}
+// void nftdialog::infoToggle()
+// {
+// 	if (infoButton->isDown())
+// 	{
+// 		tmpSpacer->setHidden(true);
+// 		infoLabel->setHidden(false);
+// 		if (imgButton->isDown())
+// 			imgButton->toggle();
+// 		if (aboutButton->isDown())
+// 			aboutButton->toggle();
+// 	} 
+// 	else
+// 	{
+// 		if (imgButton->isDown() == false)
+// 			tmpSpacer->setHidden(false);
+// 		infoLabel->setHidden(true);
+// 	}
+// 	
+// }
 
-void nftdialog::imgToggle()
-{	
-	if (imgButton->isDown())
-	{
-		tmpSpacer->setHidden(true);
-		imgLabel->setHidden(false);
-		if (infoButton->isDown()) 
-			infoButton->toggle();
-		if (aboutButton->isDown()) 
-			aboutButton->toggle();
-	}
-	else
-	{
-		if (infoButton->isDown() == false)
-			tmpSpacer->setHidden(false);
-		imgLabel->setHidden(true);
-	}
-}
+// void nftdialog::imgToggle()
+// {	
+// 	if (imgButton->isDown())
+// 	{
+// 		tmpSpacer->setHidden(true);
+// 		imgLabel->setHidden(false);
+// 		if (infoButton->isDown()) 
+// 			infoButton->toggle();
+// 		if (aboutButton->isDown()) 
+// 			aboutButton->toggle();
+// 	}
+// 	else
+// 	{
+// 		if (infoButton->isDown() == false)
+// 			tmpSpacer->setHidden(false);
+// 		imgLabel->setHidden(true);
+// 	}
+// }
 
-void nftdialog::aboutToggle()
-{
-	if (aboutButton->isDown())
-	{
-		tmpSpacer->setHidden(true);
-		aboutLabel->setHidden(false);
-		if (imgButton->isDown()) 
-			imgButton->toggle();
-		if (infoButton->isDown())
-			infoButton->toggle();
-	}
-	else
-	{
-		if (aboutButton->isDown() == false)
-			tmpSpacer->setHidden(false);
-		aboutLabel->setHidden(true);
-	}
-}
+// void nftdialog::aboutToggle()
+// {
+// 	if (aboutButton->isDown())
+// 	{
+// 		tmpSpacer->setHidden(true);
+// 		aboutLabel->setHidden(false);
+// 		if (imgButton->isDown()) 
+// 			imgButton->toggle();
+// 		if (infoButton->isDown())
+// 			infoButton->toggle();
+// 	}
+// 	else
+// 	{
+// 		if (aboutButton->isDown() == false)
+// 			tmpSpacer->setHidden(false);
+// 		aboutLabel->setHidden(true);
+// 	}
+// }
 
-void nftdialog::exitCancel() 
-{
-	reject();
-}
-
-void nftdialog::exitOK() 
-{
-	if (currentDocumentTemplate == NULL)
-		reject();
-	else
-		accept();
-}
+// void nftdialog::exitCancel() 
+// {
+// 	reject();
+// }
+// 
+// void nftdialog::exitOK() 
+// {
+// 	if (currentDocumentTemplate == NULL)
+// 		reject();
+// 	else
+// 		accept();
+// }
 
 void nftdialog::setupAbout() 
 {
@@ -359,31 +370,31 @@ void nftdialog::setupAbout()
 	text +=  tr("Translating template.xml");
 	text +=  "</b><br>";
 	text += tr("Copy an existing template.xml to a file called template.lang_COUNTRY.xml (use the same lang code that is present in the qm file for your language), for example template.fi.xml for Finnish language template.xml. The copy must be located in the same directory as the original template.xml so Scribus can load it.");
-	aboutLabel->setText(text);
+	helpBrowser->setText(text);
 }
 
-void nftdialog::showPopup(Q3IconViewItem* item, const QPoint& point)
-{
-	if (item != NULL) 
-	{
-		getCurrentDocumentTemplate(item);
-		if (currentDocumentTemplate->canWrite())
-			popUp->popup(point);
-	}
-}
+// void nftdialog::showPopup(Q3IconViewItem* item, const QPoint& point)
+// {
+// 	if (item != NULL)
+// 	{
+// 		getCurrentDocumentTemplate(item);
+// 		if (currentDocumentTemplate->canWrite())
+// 			popUp->popup(point);
+// 	}
+// }
 
 void nftdialog::removeTemplate()
 {
 	currentDocumentTemplate->isDeleted = true;
-	infoLabel->clear();
-	imgLabel->clear();
+	textBrowser->clear();
+	imageView->clear();
 	currentDocumentTemplate = NULL;
-	okButton->setEnabled(false);
+	buttonBox->button(QDialogButtonBox::Ok)->setEnabled(false);
 	setupListItems();
 	setupCategories();
 }
 
-void nftdialog::getCurrentDocumentTemplate(Q3IconViewItem* item)
+void nftdialog::getCurrentDocumentTemplate(QListWidgetItem* item)
 {
 	for (uint i = 0; i < iconItems.size(); ++i)
 	{
--- scribus/plugins/newfromtemplateplugin/nftdialog.h
+++ scribus/plugins/newfromtemplateplugin/nftdialog.h
@@ -13,49 +13,35 @@ for which a new license (GPL+exception) is in place.
 
 #include <utility>
 #include <vector>
-#include <qcursor.h>
-#include <qdialog.h>
-#include <q3boxlayout.h>
-#include <q3frame.h>
-#include <q3iconview.h> 
-#include <qicon.h>
-#include <q3iconview.h> 
-#include <qlabel.h>
-#include <qlayout.h>
-#include <q3listbox.h>
-#include <qpixmap.h> 
-#include <q3popupmenu.h>
-#include <qpushbutton.h>
-#include <qsplitter.h>
-#include <q3textbrowser.h> 
-#include <qtoolbutton.h>
+
+#include "ui_nftdialog.h"
 #include "nftsettings.h"
 
-typedef std::pair<nfttemplate*, Q3IconViewItem*> ListItem;
+typedef std::pair<nfttemplate*, QListWidgetItem*> ListItem;
 
-class nftdialog: public QDialog
+class nftdialog: public QDialog, public Ui::nftdialog
 {
 	Q_OBJECT
 
 private:
 	nftsettings* settings;
-	Q3ListBox* categoryList;
-	Q3IconView* tnailGrid;
-	Q3Frame* detailBar;
-	Q3TextBrowser* infoLabel;
-	Q3IconView* imgLabel;
-	Q3TextBrowser* aboutLabel;
-	QWidget* tmpSpacer;
-	QToolButton* imgButton;
-	QToolButton* infoButton;
-	QToolButton* aboutButton;
-	QPushButton* settingsButton;
-	QPushButton* okButton;
-	QPushButton* cancelButton;
-	Q3BoxLayout* mainlo;
-	Q3BoxLayout* detaillo;
-	Q3BoxLayout* dtbarlo;
-	Q3PopupMenu* popUp;
+// 	Q3ListBox* categoryList;
+// 	Q3IconView* tnailGrid;
+// 	Q3Frame* detailBar;
+// 	Q3TextBrowser* infoLabel;
+// 	Q3IconView* imgLabel;
+// 	Q3TextBrowser* aboutLabel;
+// 	QWidget* tmpSpacer;
+// 	QToolButton* imgButton;
+// 	QToolButton* infoButton;
+// 	QToolButton* aboutButton;
+// 	QPushButton* settingsButton;
+// 	QPushButton* okButton;
+// 	QPushButton* cancelButton;
+// 	Q3BoxLayout* mainlo;
+// 	Q3BoxLayout* detaillo;
+// 	Q3BoxLayout* dtbarlo;
+// 	Q3PopupMenu* popUp;
 	std::vector<ListItem*> iconItems;
 	void setupCategories();
 	void setupListItems();
@@ -66,14 +52,14 @@ public:
 	~nftdialog();
 private slots: 
 	void setTNails();
-	void infoToggle();
-	void imgToggle();
-	void aboutToggle();
-	void setInfo(Q3IconViewItem* item);
-	void exitOK();
-	void exitCancel();
-	void getCurrentDocumentTemplate(Q3IconViewItem* item);
-	void showPopup(Q3IconViewItem* item, const QPoint& point);
+// 	void infoToggle();
+// 	void imgToggle();
+// 	void aboutToggle();
+	void setInfo();
+// 	void exitOK();
+// 	void exitCancel();
+	void getCurrentDocumentTemplate(QListWidgetItem* item);
+// 	void showPopup(QListWidgetItem* item, const QPoint& point);
 	void removeTemplate();
 };
 
