1ef0ea9a9a0e986efb23ec86a0a4a7f86cdfe2ab
Andreas Vox <avox@arcor.de>
2007-05-23 08:24:50 +0000

    include <Python.h> first since on Mac those headers use 'slots' as varname
    git-svn-id: svn://scribus.net/branches/Version135x/Scribus@9625 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/scripterprefsgui.cpp
+++ scribus/plugins/scriptplugin/scripterprefsgui.cpp
@@ -4,6 +4,10 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
+
+// include the python header first because on OSX that uses the name "slots"
+#include <Python.h>
+
 #include <QColorDialog>
 #include <QFileDialog>
 
