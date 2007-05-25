541d040b40c62f7d1077a4db8f83ebcdc885f1ad
Andreas Vox <avox@arcor.de>
2007-05-25 22:29:51 +0000

    surely we want to set CuItem to NULL when we change docs... (I got a crash when closing a doc)
    git-svn-id: svn://scribus.net/branches/Version13x/Scribus@9648 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/mpalette.cpp
+++ scribus/mpalette.cpp
@@ -1179,6 +1179,7 @@ void Mpalette::setDoc(ScribusDoc *d)
 	disconnect(this->Cpal, SIGNAL(NewPatternProps(double, double, double, double, double)), 0, 0);
 
 	doc = d;
+	CurItem = NULL;
 	Cpal->setDocument(doc);
 	Cpal->setCurrentItem(NULL);
 	Umrech=doc->unitRatio();
@@ -1249,6 +1250,7 @@ void Mpalette::unsetDoc()
 	HaveDoc = false;
 	HaveItem = false;
 	doc=NULL;
+	CurItem = NULL;
 	Cpal->setCurrentItem(NULL);
 	Cpal->setDocument(NULL);
 	paraStyleCombo->setDoc(0);
