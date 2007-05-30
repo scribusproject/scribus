3fb5cf1fede5ffd1897f30d4e76d15ee879a1962
Jean Ghali <jghali@libertysurf.fr>
2007-05-30 23:14:33 +0000

    #5787: fix crash when copy/pasting text frames
    git-svn-id: svn://scribus.net/trunk/Scribus@9708 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/styles/style.h
+++ scribus/styles/style.h
@@ -15,10 +15,11 @@ template<class SUBSTYLE>
 inline
 void  Style::desaxeRules(const Xml_string& prefixPattern, desaxe::Digester& ruleset, Xml_string elemtag)
 {
+	static QString emptyString;
 	Xml_string stylePrefix(Digester::concat(prefixPattern,elemtag));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setName, "name", &parse<const QString&>, QString::null));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setParent, "parent", &parse<const QString&>, QString::null));
-	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setShortcut, "shortcut", &parse<const QString&>, QString::null));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setName, "name", &parse<const QString&>, emptyString));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setParent, "parent", &parse<const QString&>, emptyString));
+	ruleset.addRule(stylePrefix, SetAttributeWithConversion<SUBSTYLE, const QString&>( & SUBSTYLE::setShortcut, "shortcut", &parse<const QString&>, emptyString));
 }
 
 #endif
