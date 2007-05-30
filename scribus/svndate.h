3b5211db99eef18503f070948f2abc966bbf36d8
Craig Bradney <mrb@scribus.info>
2007-05-30 06:22:04 +0000

    git-svn-id: svn://scribus.net/trunk/Scribus@9696 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/svndate.h
+++ scribus/svndate.h
@@ -34,10 +34,12 @@ inline QDateTime getSvnDateTime()
     // SCRIBUS_SVN_DATE is in the format "$Date$"
     const QString dateStr = QString( SCRIBUS_SVN_DATE ).section( QChar(' '), 1, 2);
     QDateTime tm = QDateTime::fromString( dateStr, Qt::ISODate );
-    // Adjust the time based on time zone information. We discard the minutes from the timezone
-    // since we just don't care that much. We need to convert to UTC because QDateTime's fromString
-    // can't understand the timezone natively, and there's no guarantee that the local time where
-    // this copy of svn was checked out is the same as the local time the program is being run in.
+    // Adjust the time based on time zone information. We discard the minutes
+    // from the timezone since we just don't care that much. We need to convert
+    // to UTC because QDateTime's fromString can't understand the timezone
+    // natively, and there's no guarantee that the local time where this copy
+    // of svn was checked out is the same as the local time the program is
+    // being run in.
     tm.setTimeSpec(Qt::UTC);
     const int tzSecs = QString( SCRIBUS_SVN_DATE ).section( QChar(' '), 3, 3 ).left(3).toInt() * -3600;
     return tm.addSecs(tzSecs);
