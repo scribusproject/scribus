6438decda936b40a19a5ad4a4d2160452c547895
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2007-06-10 08:24:41 +0000

    Mad the "Delete Color" dialog Qt-4 native.
    git-svn-id: svn://scribus.net/trunk/Scribus@9821 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/dcolor.cpp
+++ scribus/dcolor.cpp
@@ -4,17 +4,14 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#include <qdialog.h>
-#include <qlayout.h>
-#include <qlabel.h>
-#include <qpushbutton.h>
-#include <qcombobox.h>
-#include <qpixmap.h>
-#include <qicon.h>
-//Added by qt3to4:
-#include <Q3GridLayout>
-#include <Q3HBoxLayout>
-#include <Q3VBoxLayout>
+
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QIcon>
+#include <QLabel>
+#include <QSpacerItem>
+#include <QPushButton>
 
 #include "commonstrings.h"
 #include "scribusdoc.h"
@@ -23,64 +20,64 @@ for which a new license (GPL+exception) is in place.
 #include "prefsmanager.h"
 
 #include "dcolor.h"
-//#include "dcolor.moc"
 
 
 extern QPixmap loadIcon(QString nam);
 
-DelColor::DelColor( QWidget* parent, ColorList colorList, QString colorName, bool haveDoc)
-    : QDialog( parent, "dd", true, 0 )
+DelColor::DelColor( QWidget* parent, ColorList colorList, QString colorName, bool haveDoc) : QDialog( parent )
 {
-	setName( "DelColor" );
-    setCaption( tr( "Delete Color" ) );
-  	setIcon(loadIcon("AppIcon.png"));
-    dialogLayout = new Q3VBoxLayout( this, 10, 5 );
-    delColorLayout = new Q3GridLayout(this);
-    delColorLayout->setSpacing( 6 );
-    delColorLayout->setMargin( 5 );
-    deleteLabel = new QLabel( tr( "Delete Color:" ), this, "deleteLabel" );
-    delColorLayout->addWidget( deleteLabel, 0, 0 );
-    colorToDelLabel = new QLabel( colorName, this, "colorToDelLabel" );
-    delColorLayout->addWidget( colorToDelLabel, 0, 1 );
+	setModal(true);
+	setWindowTitle( tr( "Delete Color" ) );
+	setWindowIcon(QIcon(loadIcon ( "AppIcon.png" )));
+	dialogLayout = new QVBoxLayout( this );
+	dialogLayout->setMargin(10);
+	dialogLayout->setSpacing(5);
+	delColorLayout = new QGridLayout;
+	delColorLayout->setSpacing( 5 );
+	delColorLayout->setMargin( 5 );
+	deleteLabel = new QLabel( tr( "Delete Color:" ), this, "deleteLabel" );
+	delColorLayout->addWidget( deleteLabel, 0, 0 );
+	colorToDelLabel = new QLabel( colorName, this, "colorToDelLabel" );
+	delColorLayout->addWidget( colorToDelLabel, 0, 1 );
 
 	PrefsManager* prefsManager = PrefsManager::instance();
 	bool isToolColor = prefsManager->isToolColor(colorName);
 	if (haveDoc || isToolColor)
 	{
-    	replaceLabel = new QLabel( tr( "Replace With:" ), this, "replaceLabel" );
-    	delColorLayout->addWidget( replaceLabel, 1, 0 );
-    	replacementColData = new ColorCombo(false, this);
+		replaceLabel = new QLabel( tr( "Replace With:" ), this, "replaceLabel" );
+		delColorLayout->addWidget( replaceLabel, 1, 0 );
+		replacementColData = new ColorCombo(false, this);
 		colorList.remove(colorName);
 		// 10/26/2004 pv - user can replace deleted color with "None"
 		replacementColData->insertItem(CommonStrings::tr_NoneColor);
 		replacementColData->insertItems(colorList, ColorCombo::smallPixmaps);
-    	delColorLayout->addWidget( replacementColData, 1, 1 );
-    	replacementColor = replacementColData->text(0);
+		delColorLayout->addWidget( replacementColData, 1, 1 );
+	replacementColor = replacementColData->text(0);
 	}
-    dialogLayout->addLayout( delColorLayout );
+	dialogLayout->addLayout( delColorLayout );
 
-    okCancelLayout = new Q3HBoxLayout(this);
-    okCancelLayout->setSpacing( 6 );
-    okCancelLayout->setMargin( 0 );
-    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    okCancelLayout->addItem( spacer );
-    okButton = new QPushButton( CommonStrings::tr_OK, this, "okButton" );
-    okCancelLayout->addWidget( okButton );
-    cancelButton = new QPushButton( CommonStrings::tr_Cancel, this, "PushButton13" );
-    cancelButton->setDefault( true );
-    okCancelLayout->addWidget( cancelButton );
-    dialogLayout->addLayout( okCancelLayout );
-    setMaximumSize(sizeHint());
+	okCancelLayout = new QHBoxLayout;
+	okCancelLayout->setSpacing( 5 );
+	okCancelLayout->setMargin( 0 );
+	QSpacerItem* spacer = new QSpacerItem( 2, 2, QSizePolicy::Expanding, QSizePolicy::Minimum );
+	okCancelLayout->addItem( spacer );
+	okButton = new QPushButton( CommonStrings::tr_OK, this, "okButton" );
+	okCancelLayout->addWidget( okButton );
+	cancelButton = new QPushButton( CommonStrings::tr_Cancel, this, "PushButton13" );
+	cancelButton->setDefault( true );
+	okCancelLayout->addWidget( cancelButton );
+	dialogLayout->addLayout( okCancelLayout );
+	setMaximumSize(sizeHint());
 
-    connect( okButton, SIGNAL( clicked() ), this, SLOT( accept() ) );
-    connect( cancelButton, SIGNAL( clicked() ), this, SLOT( reject() ) );
+	connect( okButton, SIGNAL( clicked() ), this, SLOT( accept() ) );
+	connect( cancelButton, SIGNAL( clicked() ), this, SLOT( reject() ) );
 	if (haveDoc || isToolColor)
-    	connect( replacementColData, SIGNAL(activated(int)), this, SLOT( ReplaceColor(int) ) );
+		connect( replacementColData, SIGNAL(activated(int)), this, SLOT( ReplaceColor(int) ) );
 }
 
 void DelColor::ReplaceColor(int id)
 {
-    replacementColor = replacementColData->text(id);
+	replacementColor = replacementColData->text(id);
 }
 
 const QString DelColor::getReplacementColor()
--- scribus/dcolor.h
+++ scribus/dcolor.h
@@ -1,9 +1,3 @@
-//Added by qt3to4:
-#include <Q3VBoxLayout>
-#include <Q3GridLayout>
-#include <Q3HBoxLayout>
-#include <QDialog>
-#include <QLabel>
 /*
 For general Scribus (>=1.3.2) copyright and licensing information please refer
 to the COPYING file provided with the program. Following this notice may exist
@@ -13,10 +7,12 @@ for which a new license (GPL+exception) is in place.
 #ifndef DELCOLOR_H
 #define DELCOLOR_H
 
-class QLayout;
 class QLabel;
 class QPushbutton;
 class ColorCombo;
+class QVBoxLayout;
+class QHBoxLayout;
+class QGridLayout;
 
 #include <QDialog>
 #include "scribusapi.h"
@@ -27,22 +23,21 @@ class SCRIBUS_API DelColor : public QDialog
     Q_OBJECT
 
 public:
-    DelColor(QWidget* parent, ColorList colorList, QString colorName, bool haveDoc );
-    ~DelColor() {};
+	DelColor(QWidget* parent, ColorList colorList, QString colorName, bool haveDoc );
+	~DelColor() {};
 	const QString getReplacementColor();
 
 private:
-    QLabel* deleteLabel;
-    QLabel* colorToDelLabel;
-    QLabel* replaceLabel;
-    QPushButton* okButton;
-    QPushButton* cancelButton;
-    ColorCombo* replacementColData;
-    Q3VBoxLayout* dialogLayout;
-    Q3GridLayout* delColorLayout;
-    Q3HBoxLayout* okCancelLayout;
-
-    QString replacementColor;
+	QLabel* deleteLabel;
+	QLabel* colorToDelLabel;
+	QLabel* replaceLabel;
+	QPushButton* okButton;
+	QPushButton* cancelButton;
+	ColorCombo* replacementColData;
+	QVBoxLayout* dialogLayout;
+	QGridLayout* delColorLayout;
+	QHBoxLayout* okCancelLayout;
+	QString replacementColor;
 
 private slots:
     virtual void ReplaceColor(int);
