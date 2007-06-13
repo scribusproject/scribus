a49072612431a5f5afe9047f973303aefda17fbc
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2007-06-13 18:39:37 +0000

    Ported to Qt-4.
    git-svn-id: svn://scribus.net/trunk/Scribus@9863 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/buttonicon.cpp
+++ scribus/buttonicon.cpp
@@ -4,70 +4,82 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
+#include "buttonicon.h"
+
+#include <QPixmap>
+#include <QGridLayout>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QSpacerItem>
+#include <QComboBox>
+#include <QGroupBox>
+#include <QSlider>
+#include <QPushButton>
+#include <QLabel>
 
 #include "scfonts.h"
-#include "buttonicon.h"
-//#include "buttonicon.moc"
 #include "scribusstructs.h"
 #include "pageitem.h"
-//Added by qt3to4:
-#include <QPixmap>
-#include <Q3GridLayout>
-#include <Q3HBoxLayout>
-#include <Q3VBoxLayout>
-#include <QLabel>
 
 extern QPixmap loadIcon(QString nam);
 
-ButtonIcon::ButtonIcon(QWidget* parent, PageItem* ite)
-		: QDialog( parent, "bu", true, 0 )
+ButtonIcon::ButtonIcon(QWidget* parent, PageItem* ite) : QDialog( parent )
 {
 	Item = ite;
-	setCaption( tr( "Icon Placement" ) );
-	setIcon(loadIcon("AppIcon.png"));
-	ButtonIconLayout = new Q3VBoxLayout( this, 11, 6, "ButtonIconLayout");
-	Layout1 = new Q3GridLayout( 0, 0, 6, "Layout6");
+	setModal(true);
+	setWindowTitle( tr( "Icon Placement" ) );
+	setWindowIcon(loadIcon("AppIcon.png"));
+	ButtonIconLayout = new QVBoxLayout( this );
+	ButtonIconLayout->setMargin(10);
+	ButtonIconLayout->setSpacing(5);
+	Layout1 = new QGridLayout;
+	Layout1->setMargin(0);
+	Layout1->setSpacing(5);
 	TextLabel1_2 = new QLabel( this, "TextLabel1_2" );
 	TextLabel1_2->setText( tr( "Layout:" ) );
 	Layout1->addWidget( TextLabel1_2, 0, 0 );
-	Place = new QComboBox( true, this, "Place" );
+	Place = new QComboBox(this);
 	QString tmp_pla[] = { tr("Caption only"), tr("Icon only"), tr("Caption below Icon"), tr("Caption above Icon"),
 	                      tr("Caption right to Icon"), tr("Caption left to Icon"), tr("Caption overlays Icon")};
 	size_t array_pla = sizeof(tmp_pla) / sizeof(*tmp_pla);
 	/* PFJ - 29/02/04 - Changed from uint to int and var name */
 	for (uint prop = 0; prop < array_pla; ++prop)
-		Place->insertItem(tmp_pla[prop]);
+		Place->addItem(tmp_pla[prop]);
 	Place->setEditable(false);
 	Layout1->addWidget( Place, 0, 1 );
 	TextLabel1 = new QLabel( this, "TextLabel1" );
 	TextLabel1->setText( tr( "Scale:" ) );
 	Layout1->addWidget( TextLabel1, 1, 0 );
-	ScaleW = new QComboBox( true, this, "ScaleW" );
+	ScaleW = new QComboBox(this);
 	/* PFJ - 29/02/04 - Changed to QString/size_t/for style. It saves a small
 						of time during the setup */
 	QString scalew[] = { tr("Always"), tr("When Icon is too small"),
 	                    tr("When Icon is too big"), tr("Never")};
 	size_t scalewArray = sizeof(scalew) / sizeof(*scalew);
 	for (uint prop = 0; prop < scalewArray; ++prop)
-		ScaleW->insertItem(scalew[prop]);
+		ScaleW->addItem(scalew[prop]);
 	ScaleW->setEditable(false);
 	Layout1->addWidget( ScaleW, 1, 1 );
 	TextLabel2 = new QLabel( this, "TextLabel2" );
 	TextLabel2->setText( tr( "Scale How:" ) );
 	Layout1->addWidget( TextLabel2, 2, 0 );
-	ScaleH = new QComboBox( true, this, "ScaleH" );
-	ScaleH->insertItem( tr( "Proportional" ) );
-	ScaleH->insertItem( tr( "Non Proportional" ) );
+	ScaleH = new QComboBox(this);
+	ScaleH->addItem( tr( "Proportional" ) );
+	ScaleH->addItem( tr( "Non Proportional" ) );
 	ScaleH->setEditable(false);
 	Layout1->addWidget( ScaleH, 2, 1 );
 	ButtonIconLayout->addLayout( Layout1 );
 	QSpacerItem* spacer = new QSpacerItem( 0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding );
 	ButtonIconLayout->addItem( spacer );
-	Layout5 = new Q3HBoxLayout( 0, 0, 6, "Layout5");
+	Layout5 = new QHBoxLayout;
+	Layout5->setMargin(0);
+	Layout5->setSpacing(5);
 	QSpacerItem* spacer_2 = new QSpacerItem( 0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum );
 	Layout5->addItem( spacer_2 );
-	Layout3 = new Q3GridLayout( 0, 1, 1, 0, 6, "Layout3");
-	GroupButton = new Q3GroupBox( this, "GroupButton" );
+	Layout3 = new QGridLayout;
+	Layout3->setMargin(0);
+	Layout3->setSpacing(5);
+	GroupButton = new QGroupBox( this, "GroupButton" );
 	GroupButton->setMinimumSize( QSize( 150, 150 ) );
 	GroupButton->setMaximumSize( QSize( 150, 150 ) );
 	GroupButton->setTitle( "" );
@@ -91,6 +103,7 @@ ButtonIcon::ButtonIcon(QWidget* parent, PageItem* ite)
 	SliderY->setOrientation( Qt::Vertical );
 	SliderY->setTickmarks( QSlider::Left );
 	SliderY->setTickInterval( 100 );
+	SliderY->setInvertedAppearance(true);
 	Layout3->addWidget( SliderY, 0, 1 );
 	Layout5->addLayout( Layout3 );
 	QSpacerItem* spacer_3 = new QSpacerItem( 0, 0, QSizePolicy::Expanding, QSizePolicy::Minimum );
@@ -98,7 +111,9 @@ ButtonIcon::ButtonIcon(QWidget* parent, PageItem* ite)
 	ButtonIconLayout->addLayout( Layout5 );
 	QSpacerItem* spacer_4 = new QSpacerItem( 0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding );
 	ButtonIconLayout->addItem( spacer_4 );
-	Layout4 = new Q3HBoxLayout( 0, 0, 6, "Layout4");
+	Layout4 = new QHBoxLayout;
+	Layout4->setMargin(0);
+	Layout4->setSpacing(5);
 	OK = new QPushButton( this, "OK" );
 	OK->setText( tr( "OK" ) );
 	OK->setDefault( true );
@@ -137,10 +152,10 @@ void ButtonIcon::MoveIconX(int x)
 
 void ButtonIcon::SetAllVals()
 {
-	Place->setCurrentItem(Item->annotation().IPlace());
-	ScaleW->setCurrentItem(Item->annotation().ScaleW());
-	ScaleH->setCurrentItem(Item->imageXScale() != Item->imageYScale() ? 1 : 0);
-	SetScaleHow(ScaleW->currentItem());
+	Place->setCurrentIndex(Item->annotation().IPlace());
+	ScaleW->setCurrentIndex(Item->annotation().ScaleW());
+	ScaleH->setCurrentIndex(Item->imageXScale() != Item->imageYScale() ? 1 : 0);
+	SetScaleHow(ScaleW->currentIndex());
 	SliderX->setValue(static_cast<int>(Item->imageXOffset() / (Item->width() - Item->pixm.width()) * 1000));
 	SliderY->setValue(static_cast<int>(Item->imageYOffset() / (Item->height() - Item->pixm.height()) * 1000));
 	TextLabel3->setGeometry(QRect(static_cast<int>(SliderX->value() / 1000.0 * 100),
--- scribus/buttonicon.h
+++ scribus/buttonicon.h
@@ -7,18 +7,16 @@ for which a new license (GPL+exception) is in place.
 #ifndef BUTTONICON_H
 #define BUTTONICON_H
 
-#include <qdialog.h>
-#include <qcombobox.h>
-#include <q3groupbox.h>
-#include <qlabel.h>
-#include <qpushbutton.h>
-#include <qslider.h>
-#include <qlayout.h>
-#include <qtooltip.h>
-//Added by qt3to4:
-#include <Q3VBoxLayout>
-#include <Q3GridLayout>
-#include <Q3HBoxLayout>
+#include <QDialog>
+
+class QGridLayout;
+class QHBoxLayout;
+class QVBoxLayout;
+class QComboBox;
+class QGroupBox;
+class QSlider;
+class QPushButton;
+class QLabel;
 
 #include "scribusapi.h"
 
@@ -29,22 +27,22 @@ class SCRIBUS_API ButtonIcon : public QDialog
     Q_OBJECT
 
 public:
-    ButtonIcon(QWidget* parent, PageItem* ite);
-    ~ButtonIcon() {};
+	ButtonIcon(QWidget* parent, PageItem* ite);
+	~ButtonIcon() {};
 
-    QLabel* TextLabel1_2;
-    QComboBox* Place;
-    QLabel* TextLabel1;
-    QComboBox* ScaleW;
-    QLabel* TextLabel2;
-    QComboBox* ScaleH;
-    Q3GroupBox* GroupButton;
-    QLabel* TextLabel3;
-    QSlider* SliderX;
-    QSlider* SliderY;
-    QPushButton* OK;
-    QPushButton* Cancel;
-    QPushButton* Reset;
+	QLabel* TextLabel1_2;
+	QComboBox* Place;
+	QLabel* TextLabel1;
+	QComboBox* ScaleW;
+	QLabel* TextLabel2;
+	QComboBox* ScaleH;
+	QGroupBox* GroupButton;
+	QLabel* TextLabel3;
+	QSlider* SliderX;
+	QSlider* SliderY;
+	QPushButton* OK;
+	QPushButton* Cancel;
+	QPushButton* Reset;
 	PageItem* Item;
 	int IcPlace;
 	int IcScaleW;
@@ -59,11 +57,11 @@ private slots:
 	void SetScaleHow(int s);
 
 protected:
-    Q3VBoxLayout* ButtonIconLayout;
-    Q3GridLayout* Layout1;
-    Q3HBoxLayout* Layout5;
-    Q3GridLayout* Layout3;
-    Q3HBoxLayout* Layout4;
+	QVBoxLayout* ButtonIconLayout;
+	QGridLayout* Layout1;
+	QHBoxLayout* Layout5;
+	QGridLayout* Layout3;
+	QHBoxLayout* Layout4;
 };
 
 #endif // BUTTONICON_H
