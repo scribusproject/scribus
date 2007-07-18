a7de3ffff1130a42ac86800ecb2970bcc1fc7605
Craig Bradney <mrb@scribus.info>
2007-07-18 21:53:15 +0000

    Split out loadIcon from util.cpp to util_icon.cpp Use an include instead of the extern lines Clean and sort a few of the include lists in some files Implement a (default off, and in-code setting only for now) greayscale conversion on startup - a nice test and effect. Added force colour bool to loadIcon so we can force certain icons to colour, like the splashscreen
    git-svn-id: svn://scribus.net/trunk/Scribus@10201 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/util_icon.cpp
@@ -0,0 +1,71 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+/***************************************************************************
+                          util.cpp  -  description
+                             -------------------
+    begin                : Fri Sep 14 2001
+    copyright            : (C) 2001 by Franz Schmid
+    email                : Franz.Schmid@altmuehlnet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QFile>
+#include <QPixmap>
+#include <QRgb>
+
+#include "util_icon.h"
+
+#include "prefsmanager.h"
+#include "scpaths.h"
+#include "scpixmapcache.h"
+
+
+QPixmap loadIcon(const QString nam, bool forceUseColor)
+{
+	static ScPixmapCache<QString> pxCache;
+	if (pxCache.contains(nam))
+		return *pxCache[nam];
+
+	QString iconFilePath(QString("%1%2").arg(ScPaths::instance().iconDir()).arg(nam));
+	QPixmap *pm = new QPixmap();
+	
+	if (!QFile::exists(iconFilePath))
+		qWarning("Unable to load icon %s: File not found", iconFilePath.ascii());
+	else
+	{
+		pm->load(iconFilePath);
+		if (pm->isNull())
+			qWarning("Unable to load icon %s: Got null pixmap", iconFilePath.ascii());
+		if (PrefsManager::instance()->appPrefs.grayscaleIcons && !forceUseColor)
+		{
+			QImage qi(pm->toImage());
+			int h=qi.height();
+			int w=qi.width();
+			QRgb c_rgb;
+			for (int i=0;i<w;++i)
+			{
+				for (int j=0;j<h;++j)
+				{
+					c_rgb=qi.pixel(i,j);
+					int k = qMin(qRound(0.3 * qRed(c_rgb) + 0.59 * qGreen(c_rgb) + 0.11 * qBlue(c_rgb)), 255);
+					qi.setPixel(i, j, qRgba(k, k, k, qAlpha(c_rgb)));
+				}
+			}
+			*pm=QPixmap::fromImage(qi);
+		}
+	}
+	pxCache.insert(nam, pm);
+	return *pm;
+}
---
+++ scribus/util_icon.h
@@ -0,0 +1,18 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+#ifndef _UTIL_IMAGE_H
+#define _UTIL_IMAGE_H
+
+
+#include <QString>
+
+#include "scribusapi.h"
+
+QPixmap SCRIBUS_API loadIcon(const QString nam, bool forceUseColor=false);
+
+
+#endif
