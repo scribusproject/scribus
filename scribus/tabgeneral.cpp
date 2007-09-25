c2420f60b01bd0b20fe3ebb0373564b5053fa1e8
Petr Vaněk <petr@yarpen.cz>
2007-09-25 11:23:54 +0000

    part of #6259: Severe issues with the preferences
    git-svn-id: svn://scribus.net/trunk/Scribus@10709 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/tabgeneral.cpp
+++ scribus/tabgeneral.cpp
@@ -40,6 +40,7 @@ TabGeneral::TabGeneral(QWidget* parent, const char* name)
 	// qt styles
 	QStringList STtest;
 	STtest = QStyleFactory::keys();
+	GUICombo->addItem("Default");
 	for (int stt = 0; stt < STtest.count(); ++stt)
 		GUICombo->addItem(STtest[stt]);
 	//GUICombo->setEditable(false);
