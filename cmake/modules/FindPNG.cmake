6609428f449727f55ca5d27869e43cbc4a1f3549
Craig Bradney <mrb@scribus.info>
2007-10-03 11:50:42 +0000

    More win32+cmake updates: - Where available, find and link to debug versions of required libraries   automatically when doing a debug build - Ensure all used CMake Find scripts can use a library names variable   to specify alternate names libs may have. Not all the stock ones   permit this. - Permit use of the zlib and libpng libraries statically linked into   qtcore.dll rather than standalone versions (see build instructions) - Updates to build script in win32 build instructions
    git-svn-id: svn://scribus.net/trunk/Scribus@10814 11d20701-8431-0410-a711-e3c959e3b870

---
+++ cmake/modules/FindPNG.cmake
@@ -0,0 +1,83 @@
+# - Find the native PNG includes and library
+#
+# This module defines
+#  PNG_INCLUDE_DIR, where to find png.h, etc.
+#  PNG_LIBRARIES, the libraries to link against to use PNG.
+#  PNG_DEFINITIONS - You should ADD_DEFINITONS(${PNG_DEFINITIONS}) before compiling code that includes png library files.
+#  PNG_FOUND, If false, do not try to use PNG.
+# also defined, but not for general use are
+#  PNG_LIBRARY, where to find the PNG library.
+# None of the above will be defined unles zlib can be found.
+# PNG depends on Zlib
+INCLUDE(FindZLIB)
+
+IF(ZLIB_FOUND)
+
+  FIND_PATH(PNG_PNG_INCLUDE_DIR png.h
+  /usr/local/include/libpng             # OpenBSD
+  )
+
+  # On win32, qt statically links to zlib and libpng, and exports their symbols.
+  # We can just link to Qt to get zlib and libpng. We still require the user to
+  # supply their own headers on the search path, but they can and should just
+  # specify ${QTDIR}/src/3rdparty/zlib/include .
+  # To use this, you must use FindQt before FindPng.
+  IF(QT_QTCORE_LIBRARY AND USE_QT_ZLIB_PNGLIB)
+
+    MESSAGE("Using libpng from qt")
+    IF(NOT "${ZLIB_LIBRARY}" STREQUAL "${QT_QTCORE_LIBRARY}")
+       MESSAGE("WARNING: We may be using an external zlib but qt's libpng")
+    ENDIF(NOT "${ZLIB_LIBRARY}" STREQUAL "${QT_QTCORE_LIBRARY}")
+    SET(PNG_LIBRARY_RELEASE ${QT_QTCORE_LIBRARY_RELEASE})
+    SET(PNG_LIBRARY_DEBUG ${QT_QTCORE_LIBRARY_DEBUG})
+    SET(PNG_LIBRARY ${QT_QTCORE_LIBRARY})
+
+  ELSE(QT_QTCORE_LIBRARY AND USE_QT_ZLIB_PNGLIB)
+
+    SET(PNG_NAMES_RELEASE ${PNG_NAMES_RELEASE} ${PNG_NAMES} png libpng png13 libpng13)
+    FIND_LIBRARY(PNG_LIBRARY_RELEASE NAMES ${PNG_NAMES_RELEASE} )
+
+    SET(PNG_NAMES_DEBUG ${PNG_NAMES} pngd libpngd png13d libpng13d)
+    FIND_LIBRARY(PNG_LIBRARY_DEBUG NAMES ${PNG_NAMES_DEBUG} )
+
+    SET(PNG_LIBRARY)
+    IF(PNG_LIBRARY_RELEASE AND PNG_LIBRARY_DEBUG)
+      SET(PNG_LIBRARY "optimized;${PNG_LIBRARY_RELEASE};debug;${PNG_LIBRARY_DEBUG}")
+    ELSE(PNG_LIBRARY_RELEASE AND PNG_LIBRARY_DEBUG)
+      IF(PNG_LIBRARY_DEBUG)
+        SET(PNG_LIBRARY ${PNG_LIBRARY_DEBUG})
+      ELSE(PNG_LIBRARY_DEBUG)
+        SET(PNG_LIBRARY ${PNG_LIBRARY_RELEASE})
+      ENDIF(PNG_LIBRARY_DEBUG)
+    ENDIF(PNG_LIBRARY_RELEASE AND PNG_LIBRARY_DEBUG)
+
+  ENDIF(QT_QTCORE_LIBRARY AND USE_QT_ZLIB_PNGLIB)
+
+  IF (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
+      # png.h includes zlib.h. Sigh.
+      SET(PNG_INCLUDE_DIR ${PNG_PNG_INCLUDE_DIR} ${ZLIB_INCLUDE_DIR} )
+      IF(NOT USE_QT_ZLIB_PNGLIB)
+        # libpng requires zlib unless we're linking to both in the form of Qt
+        SET(PNG_LIBRARIES ${PNG_LIBRARY} ${ZLIB_LIBRARY})
+      ENDIF(NOT USE_QT_ZLIB_PNGLIB)
+
+      IF (CYGWIN)
+        IF(BUILD_SHARED_LIBS)
+           # No need to define PNG_USE_DLL here, because it's default for Cygwin.
+        ELSE(BUILD_SHARED_LIBS)
+          SET (PNG_DEFINITIONS -DPNG_STATIC)
+        ENDIF(BUILD_SHARED_LIBS)
+      ENDIF (CYGWIN)
+
+  ENDIF (PNG_LIBRARY AND PNG_PNG_INCLUDE_DIR)
+
+ENDIF(ZLIB_FOUND)
+
+# handle the QUIETLY and REQUIRED arguments and set PNG_FOUND to TRUE if 
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PNG DEFAULT_MSG PNG_LIBRARY PNG_PNG_INCLUDE_DIR)
+
+MESSAGE("Using PNG Library: ${PNG_LIBRARY}")
+
+MARK_AS_ADVANCED(PNG_PNG_INCLUDE_DIR PNG_LIBRARY )
