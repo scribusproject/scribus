2d9e273a726b9d64449adeb37e90dbade1fdd073
Peter Linnell <mrdocs@scribus.info>
2008-01-07 21:09:46 +0000

    version bumps
    git-svn-id: svn://scribus.net/branches/Version133x/Scribus@11365 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.3.3.10cvs"
+#cmakedefine VERSION "1.3.3.10"
 
 #cmakedefine BUILD_MAC_BUNDLE
 #cmakedefine HAVE_CMS 1
