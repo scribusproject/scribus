42eda23e0c631ff839dc7b253838815226bf2f34
Jean Ghali <jghali@libertysurf.fr>
2008-02-21 23:55:57 +0000

    make nicer code :)
    git-svn-id: svn://scribus.net/trunk/Scribus@11710 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/langmgr.cpp
+++ scribus/langmgr.cpp
@@ -64,7 +64,7 @@ void LanguageManager::generateLangList()
 	langList.insert("eo",       langPair("Esperanto",           QObject::tr( "Esperanto" )) );
 	langList.insert("et",       langPair("Estonian",            QObject::tr( "Estonian" )) );
 	langList.insert("de",       langPair("German",              QObject::tr( "German" )) );
-	langList.insert("de_CH",    langPair("German (Swiss)",              QObject::tr( "German (Swiss)" )) );
+	langList.insert("de_CH",    langPair("German (Swiss)",      QObject::tr( "German (Swiss)" )) );
 	langList.insert("de_ol",    langPair("German (Trad.)",      QObject::tr( "German (Trad.)" )) );
 	langList.insert("fi",       langPair("Finnish",             QObject::tr( "Finnish" )) );
 	langList.insert("fr",       langPair("French",              QObject::tr( "French" )) );
