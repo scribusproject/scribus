5d16623195b969612c535381ced14471b2370feb
Craig Bradney <mrb@scribus.info>
2008-02-28 23:24:39 +0000

    Include cstdlib as required by malloc/free
    git-svn-id: svn://scribus.net/trunk/Scribus@11746 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scgzfile.cpp
+++ scribus/scgzfile.cpp
@@ -5,6 +5,7 @@ a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
 
+#include <cstdlib>
 #include <zlib.h>
 #include <QDataStream>
 #include <QDir>
