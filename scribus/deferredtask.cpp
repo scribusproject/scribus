c75a51b61d022a774aeb2aa63fd364c2f6b32390
Craig Bradney <mrb@scribus.info>
2008-03-22 20:46:33 +0000

    Fix include methods, might help with 6879
    git-svn-id: svn://scribus.net/trunk/Scribus@11846 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/deferredtask.cpp
+++ scribus/deferredtask.cpp
@@ -4,10 +4,9 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#include "deferredtask.h"
-#include "qtimer.h"
 
-//#include "deferredtask.moc"
+#include <QTimer>
+#include "deferredtask.h"
 
 enum FileSearchStatus
 {
