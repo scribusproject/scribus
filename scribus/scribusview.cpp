a851a7c8430b1600784cb7f7ea4d89d8305826b8
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2008-03-31 18:18:31 +0000

    Redraw only once when a forced redraw with the middle mouse button is triggered.
    git-svn-id: svn://scribus.net/trunk/Scribus@11906 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusview.cpp
+++ scribus/scribusview.cpp
@@ -4336,10 +4336,10 @@ bool ScribusView::eventFilter(QObject *obj, QEvent *event)
 		QMouseEvent* m = static_cast<QMouseEvent*> (event);
 		m_canvasMode->mouseReleaseEvent(m);
 		m_canvas->m_viewMode.m_MouseButtonPressed = false;
-		if (m->button() == Qt::MidButton)
+/*		if (m->button() == Qt::MidButton)
 		{
 			DrawNew();
-		}
+		} */
 		return true;
 	}
 	else if (obj == widget() && event->type() == QEvent::MouseButtonPress)
