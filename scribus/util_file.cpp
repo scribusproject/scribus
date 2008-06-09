90635aeeb24386bd99ee23b21cb5c88034da916d
Craig Bradney <mrb@scribus.info>
2008-06-09 20:31:23 +0000

    Fix incorrect initialisation
    git-svn-id: svn://scribus.net/trunk/Scribus@12231 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/util_file.cpp
+++ scribus/util_file.cpp
@@ -98,7 +98,7 @@ bool copyFileAtomic(const QString& source, const QString& target)
 bool copyFileToFilter(const QString& source, ScStreamFilter& target)
 {
 	bool copySucceed = true;
-	int  bytesread = -1;
+	int  bytesread = 0;
 	if (source.isEmpty())
 		return false;
 	if (!QFile::exists(source))
