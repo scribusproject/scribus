48d0e4ba7614d3990799332bc0c62ef019773c73
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2008-07-09 20:35:12 +0000

    CMS preferences widget is now based on an .ui file.
    git-svn-id: svn://scribus.net/trunk/Scribus@12379 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/cmsprefs.cpp
+++ scribus/cmsprefs.cpp
@@ -7,151 +7,15 @@ for which a new license (GPL+exception) is in place.
 #include "cmsprefs.h"
 #include "scribusdoc.h"
 #include "scribusstructs.h"
-#include <QGridLayout>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QCheckBox>
-#include <QComboBox>
-#include <QGroupBox>
-#include <QToolTip>
-#include <QLabel>
-
 
 CMSPrefs::CMSPrefs( QWidget* parent, CMSData *Vor, ProfilesL *InputProfiles, ProfilesL *InputProfilesCMYK, ProfilesL *PrinterProfiles, ProfilesL *MonitorProfiles)
 	: QWidget( parent )
 {
 	prefs = Vor;
 	changed = false;
-	setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-	cmsPrefsLayout = new QVBoxLayout( this );
-	cmsPrefsLayout->setAlignment( Qt::AlignTop );
-	cmsPrefsLayout->setSpacing( 5 );
-	cmsPrefsLayout->setMargin( 0 );
-
-	checkBox1 = new QCheckBox( tr( "&Activate Color Management" ), this );
-	cmsPrefsLayout->addWidget( checkBox1 );
-
-	sysProfiles = new QGroupBox( tr( "System Profiles" ), this);
-	sysProfilesLayout = new QGridLayout(sysProfiles);
-	sysProfilesLayout->setAlignment( Qt::AlignTop );
-	sysProfilesLayout->setSpacing( 5 );
-	sysProfilesLayout->setMargin( 10 );
-
-	inputPRGBIm = new QComboBox( sysProfiles );
-	inputPRGBIm->setMinimumSize( QSize( 190, 22 ) );
-	inputPRGBIm->setEditable(false);
-	text1 = new QLabel( tr( "&RGB Images:" ), sysProfiles );
-	text1->setBuddy(inputPRGBIm);
-	sysProfilesLayout->addWidget( text1, 0, 0 );
-	sysProfilesLayout->addWidget( inputPRGBIm, 0, 1 );
-
-	inputPCMYKIm = new QComboBox( sysProfiles );
-	inputPCMYKIm->setMinimumSize( QSize( 190, 22 ) );
-	inputPCMYKIm->setEditable(false);
-	text1CMYK = new QLabel( tr( "&CMYK Images:" ), sysProfiles );
-	text1CMYK->setBuddy(inputPCMYKIm);
-	sysProfilesLayout->addWidget( text1CMYK, 1, 0 );
-	sysProfilesLayout->addWidget( inputPCMYKIm, 1, 1 );
-
-	inputPRGB = new QComboBox( sysProfiles );
-	inputPRGB->setMinimumSize( QSize( 190, 22 ) );
-	inputPRGB->setEditable(false);
-	text4 = new QLabel( tr( "&RGB Solid Colors:" ), sysProfiles );
-	text4->setBuddy(inputPRGB);
-	sysProfilesLayout->addWidget( text4, 2, 0 );
-	sysProfilesLayout->addWidget( inputPRGB, 2, 1 );
-
-	inputPCMYK = new QComboBox( sysProfiles );
-	inputPCMYK->setMinimumSize( QSize( 190, 22 ) );
-	inputPCMYK->setEditable(false);
-	text5 = new QLabel( tr( "&CMYK Solid Colors:" ), sysProfiles );
-	text5->setBuddy(inputPCMYK);
-	sysProfilesLayout->addWidget( text5, 3, 0 );
-	sysProfilesLayout->addWidget( inputPCMYK, 3, 1 );
-
-	monitorP = new QComboBox( sysProfiles );
-	monitorP->setMinimumSize( QSize( 190, 22 ) );
-	monitorP->setEditable(false);
-	text2 = new QLabel( tr( "&Monitor:" ), sysProfiles );
-	text2->setBuddy(monitorP);
-	sysProfilesLayout->addWidget( text2, 4, 0 );
-	sysProfilesLayout->addWidget( monitorP, 4, 1 );
-
-	printerP = new QComboBox( sysProfiles );
-	printerP->setMinimumSize( QSize( 190, 22 ) );
-	printerP->setEditable(false);
-	text3 = new QLabel( tr( "P&rinter:" ), sysProfiles );
-	text3->setBuddy(printerP);
-	sysProfilesLayout->addWidget( text3, 5, 0 );
-	sysProfilesLayout->addWidget( printerP, 5, 1 );
-	cmsPrefsLayout->addWidget( sysProfiles );
-
-	render = new QGroupBox( tr( "Rendering Intents" ), this );
-	renderLayout = new QGridLayout(render);
-	renderLayout->setAlignment( Qt::AlignTop );
-	renderLayout->setSpacing( 5 );
-	renderLayout->setMargin( 10 );
-
-	imagesI = new QComboBox( render );
-	imagesI->setMinimumSize( QSize( 190, 22 ) );
-	imagesI->setEditable(false);
-	text21 = new QLabel( tr( "Images:" ), render );
-	text21->setBuddy(imagesI);
-	renderLayout->addWidget( text21, 0, 0 );
-	renderLayout->addWidget( imagesI, 0, 1 );
-
-	colorsI = new QComboBox( render );
-	colorsI->setMinimumSize( QSize( 190, 22 ) );
-	colorsI->setEditable(false);
-	text22 = new QLabel( tr( "Sol&id Colors:" ), render );
-	text22->setBuddy(colorsI);
-	renderLayout->addWidget( text22, 1, 0 );
-	renderLayout->addWidget( colorsI, 1, 1 );
-
-	cmsPrefsLayout->addWidget( render );
-
-	simulate = new QCheckBox( tr( "Sim&ulate Printer on the Screen" ), this );
-	cmsPrefsLayout->addWidget( simulate );
-
-	convertLayout = new QHBoxLayout;
-	convertLayout->setAlignment( Qt::AlignTop );
-	convertLayout->setMargin( 0 );
-	convertLayout->addSpacing( 30 );
-
-	convertAll = new QCheckBox( tr( "Convert all colors to printer space" ), this);
-	convertLayout->addWidget( convertAll );
-	cmsPrefsLayout->addLayout( convertLayout );
-
-	gamutLayout = new QHBoxLayout;
-	gamutLayout->setAlignment( Qt::AlignTop );
-	gamutLayout->setMargin( 0 );
-	gamutLayout->addSpacing( 30 );
-
-	gamutC = new QCheckBox( tr( "Mark Colors out of &Gamut" ), this );
-	gamutLayout->addWidget( gamutC );
-	cmsPrefsLayout->addLayout( gamutLayout );
-
-	blackP = new QCheckBox( tr( "Use &Blackpoint Compensation" ), this );
-	cmsPrefsLayout->addWidget( blackP );
-	// switched off as it's called in main prefs classes - PV
+	setupUi(this);
 	restoreDefaults(prefs, InputProfiles, InputProfilesCMYK, PrinterProfiles, MonitorProfiles);
-
-	inputPCMYKIm->setToolTip( tr( "Default color profile for imported CMYK images" ) );
-	inputPRGBIm->setToolTip( tr( "Default color profile for imported RGB images" ) );
-	inputPRGB->setToolTip( tr( "Default color profile for solid RGB colors on the page" ) );
-	inputPCMYK->setToolTip( tr( "Default color profile for solid CMYK colors on the page" ) );
-	monitorP->setToolTip( tr( "Color profile that you have generated or received from the manufacturer.\nThis profile should be specific to your monitor and not a generic profile (i.e. sRGB)." ) );
-	printerP->setToolTip( tr( "Color profile for your printer model from the manufacturer.\nThis profile should be specific to your printer and not a generic profile (i.e. sRGB)." ) );
-	colorsI->setToolTip( tr( "Default rendering intent for solid colors. Unless you know why to change it,\nRelative Colorimetric or Perceptual should be chosen." ) );
-	imagesI->setToolTip( tr( "Default rendering intent for images. Unless you know why to change it,\nRelative Colorimetric or Perceptual should be chosen." ) );
-	simulate->setToolTip( tr( "Enable 'soft proofing' of how your document colors will print,\nbased on the chosen printer profile." ) );
-	convertAll->setToolTip( tr( "Simulate a full color managed environment :\nall colors, rgb or cmyk, are converted to printer color space." ) );
-	gamutC->setToolTip( tr( "Method of showing colors on the screen which may not print properly.\nThis requires very accurate profiles and serves only as a warning." ) );
-	blackP->setToolTip( tr( "Black Point Compensation is a method of improving contrast in photos.\nIt is recommended that you enable this if you have photos in your document." ) );
-
-	// signals and slots connections
 	connect( checkBox1, SIGNAL( clicked() ), this, SLOT( slotCMSon() ) );
-	connect( simulate, SIGNAL( clicked() ), this, SLOT( slotSimula() ) );
 }
 
 void CMSPrefs::restoreDefaults(CMSData *prefs, ProfilesL *InputProfiles, ProfilesL *InputProfilesCMYK, ProfilesL *PrinterProfiles, ProfilesL *MonitorProfiles)
@@ -219,16 +83,10 @@ void CMSPrefs::restoreDefaults(CMSData *prefs, ProfilesL *InputProfiles, Profile
 	gamutC->setChecked(prefs->GamutCheck);
 	blackP->setChecked(prefs->BlackPoint);
 
-	if (!checkBox1->isChecked())
-	{
-		simulate->setEnabled( false );
-		convertAll->setEnabled( false );
-		blackP->setEnabled( false );
-		sysProfiles->setEnabled( false );
-		render->setEnabled( false );
-	}
-	convertAll->setEnabled(simulate->isEnabled() && simulate->isChecked());
-	gamutC->setEnabled(simulate->isEnabled() && simulate->isChecked());
+	simulate->setEnabled( checkBox1->isChecked() );
+	blackP->setEnabled( checkBox1->isChecked() );
+	sysProfiles->setEnabled( checkBox1->isChecked() );
+	renderInt->setEnabled( checkBox1->isChecked() );
 }
 
 void CMSPrefs::setValues()
@@ -293,27 +151,8 @@ void CMSPrefs::updateDocSettings(ScribusDoc* doc)
 	doc->CMSSettings.BlackPoint = blackP->isChecked();
 }
 
-void CMSPrefs::slotSimula()
-{
-	bool setter = simulate->isChecked() ? true : false;
-	convertAll->setEnabled(setter);
-	if (!setter)
-		convertAll->setChecked(false);
-	gamutC->setEnabled(setter);
-}
-
 void CMSPrefs::slotCMSon()
 {
-	bool setter = checkBox1->isChecked() ? true : false;
-	sysProfiles->setEnabled(setter);
-	render->setEnabled(setter);
-	simulate->setEnabled(setter);
-	convertAll->setEnabled(setter);
-	blackP->setEnabled(setter);
-	if (setter == true)
-		slotSimula();
-	else
-		gamutC->setEnabled(setter);
-	emit cmsOn(setter);
+	emit cmsOn(checkBox1->isChecked());
 }
 
--- scribus/cmsprefs.h
+++ scribus/cmsprefs.h
@@ -7,21 +7,14 @@ for which a new license (GPL+exception) is in place.
 #ifndef CMSPREFS_H
 #define CMSPREFS_H
 
-#include <QWidget>
+#include "ui_cmsprefsbase.h"
 #include "scribusapi.h"
 
-class QGridLayout;
-class QHBoxLayout;
-class QVBoxLayout;
-class QCheckBox;
-class QComboBox;
-class QGroupBox;
-class QLabel;
 class ScribusDoc;
 struct CMSData;
 typedef QMap<QString,QString> ProfilesL;
 
-class SCRIBUS_API CMSPrefs : public QWidget
+class SCRIBUS_API CMSPrefs : public QWidget, Ui::CMSPrefsBase
 {
 	Q_OBJECT
 
@@ -31,47 +24,15 @@ public:
 
 	void restoreDefaults(CMSData *prefs, ProfilesL *InputProfiles, ProfilesL *InputProfilesCMYK, ProfilesL *PrinterProfiles, ProfilesL *MonitorProfiles);
 	void updateDocSettings(ScribusDoc* doc);
-
-	QCheckBox* checkBox1;
-	QGroupBox* sysProfiles;
-	QLabel* text2;
-	QLabel* text1;
-	QLabel* text1CMYK;
-	QLabel* text3;
-	QLabel* text4;
-	QLabel* text5;
-	QComboBox* inputPRGBIm;
-	QComboBox* inputPCMYKIm;
-	QComboBox* inputPRGB;
-	QComboBox* inputPCMYK;
-	QComboBox* monitorP;
-	QComboBox* printerP;
-	QGroupBox* render;
-	QLabel* text22;
-	QComboBox* imagesI;
-	QComboBox* colorsI;
-	QLabel* text21;
-	QCheckBox* simulate;
-	QCheckBox* convertAll;
-	QCheckBox* gamutC;
-	QCheckBox* blackP;
 	CMSData *prefs;
 	bool changed;
 
 public slots:
 	virtual void slotCMSon();
 	virtual void setValues();
-	virtual void slotSimula();
 
 signals:
 	void cmsOn(bool);
-
-protected:
-	QVBoxLayout* cmsPrefsLayout;
-	QGridLayout* sysProfilesLayout;
-	QGridLayout* renderLayout;
-	QHBoxLayout* convertLayout;
-	QHBoxLayout* gamutLayout;
 };
 
 #endif // CMSPREFS_H
