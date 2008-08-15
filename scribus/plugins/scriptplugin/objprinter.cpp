a5e95b56585ac185ab03d5167062c36de5a96a58
Craig Bradney <mrb@scribus.info>
2008-08-15 20:03:59 +0000

    Reorder include for universal build
    git-svn-id: svn://scribus.net/trunk/Scribus@12460 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/objprinter.cpp
+++ scribus/plugins/scriptplugin/objprinter.cpp
@@ -4,6 +4,8 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
+#include <vector>
+
 #include "objprinter.h"
 #include "cmdutil.h"
 #include "prefsmanager.h"
@@ -11,7 +13,7 @@ for which a new license (GPL+exception) is in place.
 #include <structmember.h>
 #include <QFileInfo>
 #include <QDir>
-#include <vector>
+
 #include "pslib.h"
 #include "scpaths.h"
 #include "scribuscore.h"
