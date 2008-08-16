02a0ffadf6d35ce5ee9a80f180ba3c50bab5a063
Craig Bradney <mrb@scribus.info>
2008-08-16 21:38:34 +0000

    bump about date
    git-svn-id: svn://scribus.net/trunk/Scribus@12465 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -69,7 +69,7 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	tabLayout1->addWidget( pixmapLabel1 );
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
-	QString BUILD_DAY = "15";
+	QString BUILD_DAY = "16";
 	QString BUILD_MONTH = CommonStrings::august;
 	QString BUILD_YEAR = "2008";
 	QString BUILD_TIME = "";
