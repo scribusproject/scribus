b7275a856701025ea2973f4f1496bd75e9fe3cb5
Craig Bradney <mrb@scribus.info>
2008-09-06 22:27:13 +0000

    Remove inserted $  - oops
    git-svn-id: svn://scribus.net/trunk/Scribus@12529 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -1,4 +1,4 @@
-$/*
+/*
 For general Scribus (>=1.3.2) copyright and licensing information please refer
 to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
