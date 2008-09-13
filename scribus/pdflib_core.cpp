07d291edc753830ff6149faeae74538a4f449b5d
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2008-09-13 12:55:03 +0000

    Fixed Bug #7378: "Rotated PDFs are not handled correctly when embedded"
    git-svn-id: svn://scribus.net/trunk/Scribus@12551 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pdflib_core.cpp
+++ scribus/pdflib_core.cpp
@@ -6229,12 +6229,31 @@ bool PDFLibCore::PDF_EmbeddedPDF(PageItem* c, const QString& fn, double sx, doub
 			StartObj(xObj);
 			PutDoc("<<\n/Type /XObject\n/Subtype /Form\n/FormType 1");
 			PoDoFo::PdfRect pagesize = page->GetPageSize();
+			int rotation = page->GetRotation();
+			QMatrix pageM;
+			pageM.scale(1.0/pagesize.GetWidth(), 1.0/pagesize.GetHeight());
+			pageM.rotate(-rotation);
 			PutDoc("\n/BBox [" + QString::number(pagesize.GetLeft()));
 			PutDoc(" " + QString::number(pagesize.GetBottom()));
 			PutDoc(" " + QString::number(pagesize.GetLeft() + pagesize.GetWidth()));
 			PutDoc(" " + QString::number(pagesize.GetBottom() + pagesize.GetHeight()));
 			PutDoc("]");
-			PutDoc("\n/Matrix [" + QString::number(1.0/pagesize.GetWidth()) + " 0 0 " + QString::number(1.0/pagesize.GetHeight()) + " 0 0]");
+			PutDoc("\n/Matrix [" + QString::number(pageM.m11()) + " "
+								 + QString::number(pageM.m12()) + " "
+								 + QString::number(pageM.m21()) + " "
+								 + QString::number(pageM.m22()) + " ");
+			if (rotation == 0)
+				PutDoc("0 0");
+			else if (rotation == 90)
+				PutDoc("0 1");
+			else if (rotation == 180)
+				PutDoc("1 1");
+			else if (rotation == 270)
+				PutDoc(QString::number(pagesize.GetHeight() / pagesize.GetWidth()) + " " + QString::number(1.0 - 1.0 / (pagesize.GetHeight() / pagesize.GetWidth())));
+			else
+				PutDoc("0 0");
+			PutDoc("]");
+//			PutDoc("\n/Matrix [" + QString::number(1.0/pagesize.GetWidth()) + " 0 0 " + QString::number(1.0/pagesize.GetHeight()) + " 0 0]");
 			PutDoc("\n/Resources " + QString::number(xResources) + " 0 R");
 			nextObj = page->GetObject()->GetIndirectKey("Group");
 			if (nextObj)
