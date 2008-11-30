d11a02ba2da8ba6ddd34e850d7c82f6e5774ff14
Peter Linnell <mrdocs@scribus.info>
2008-11-30 21:37:11 +0000

    update the changelog, some minor tweaks and bump the about date
    git-svn-id: svn://scribus.net/trunk/Scribus@12910 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -113,7 +113,7 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	tabLayout1->addWidget( pixmapLabel1 );
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
-	QString BUILD_DAY = "26";
+	QString BUILD_DAY = "30";
 	QString BUILD_MONTH = CommonStrings::november;
 	QString BUILD_YEAR = "2008";
 	QString BUILD_TIME = "";
