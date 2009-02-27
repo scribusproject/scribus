1828bff984be4e2cb6068f6eff5dbdeac955136f
Oleksandr Moskalenko <malex@scribus.net>
2009-02-27 01:44:07 +0000

    Fix a FTBFS
    git-svn-id: svn://scribus.net/trunk/Scribus@13231 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/canvasmode_eyedropper.cpp
+++ scribus/canvasmode_eyedropper.cpp
@@ -148,7 +148,8 @@ void CanvasMode_EyeDropper::mouseReleaseEvent(QMouseEvent *m)
 	QColor selectedColor=i.pixel(0, 0);
 
 	bool found=false;
-	for (ColorList::Iterator it = m_doc->PageColors.begin(); it != m_doc->PageColors.end(); ++it)
+    ColorList::Iterator it = m_doc->PageColors.begin();
+	for (; it != m_doc->PageColors.end(); ++it)
 	{
 		if (selectedColor== ScColorEngine::getRGBColor(it.value(), m_doc))
 		{
