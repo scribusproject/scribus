6e03723c5d9ffb2beb17041ecb933da1e85cd292
Peter Linnell <mrdocs@scribus.info>
2009-04-17 08:40:39 +0000

    push the new version changes 1.3.5.beta and some minor doc updates
    git-svn-id: svn://scribus.net/trunk/Scribus@13397 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.3.5svn"
+#cmakedefine VERSION "1.3.5.beta"
 
 #cmakedefine BUILD_WINDOWS_VERSION 1
 #cmakedefine SC_USE_GDI 1
