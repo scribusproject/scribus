dadd607cc25b5e525938c4a13975cc13d2055fe7
Pierre Marchand <pierre@scribus.info>
2009-05-08 18:47:03 +0000

    Added - ScCLocale is a singleton used to ensure that we always interpret strings  as double under a C locale context
    git-svn-id: svn://scribus.net/trunk/Scribus@13453 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/scclocale.cpp
@@ -0,0 +1,37 @@
+//
+// C++ Implementation: sclocale
+//
+// Description: 
+//
+//
+// Author: Pierre Marchand <pierremarc@oep-h.com>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#include "scclocale.h"
+#include <QDebug>
+
+ScCLocale * ScCLocale::m_instance = 0;
+ScCLocale::ScCLocale()
+	:QLocale(QLocale::C)
+{
+}
+
+ScCLocale * ScCLocale::that()
+{
+	if(!m_instance)
+	{
+		m_instance = new ScCLocale();
+		Q_ASSERT(m_instance);
+	}
+	return m_instance;
+}
+
+double ScCLocale::toDoubleC(const QString & str, bool * ok) 
+{
+	double ret( that()->toDouble(str, ok) );
+//  	qDebug()<<"toDoubleC "<<str<<"="<<ret;
+	return ret;
+}
---
+++ scribus/scclocale.h
@@ -0,0 +1,30 @@
+//
+// C++ Interface: sclocale
+//
+// Description: 
+//
+//
+// Author: Pierre Marchand <pierremarc@oep-h.com>, (C) 2009
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+
+#ifndef SCCLOCALE_H
+#define SCCLOCALE_H
+
+#include <QLocale>
+
+class ScCLocale : public QLocale
+{
+	ScCLocale();
+	static ScCLocale * m_instance;
+	static ScCLocale * that();
+	
+	public:
+		static double toDoubleC(const QString& str, bool * ok = 0);
+		
+};
+
+#endif // SCCLOCALE_H
+
