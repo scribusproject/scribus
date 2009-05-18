fb3fac1e6248c29962dd4003d63528de82ef84f9
Peter Linnell <mrdocs@scribus.info>
2009-05-18 20:47:02 +0000

    commit the new versioning scheme to trunk 1.5.0 :)
    git-svn-id: svn://scribus.net/trunk/Scribus@13511 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.3.5.rc1"
+#cmakedefine VERSION "1.5.0.svn"
 
 #cmakedefine BUILD_WINDOWS_VERSION 1
 #cmakedefine SC_USE_GDI 1
