9d9c8a864a3ba8fc31b7a4f358dc6dfeab77fea5
Craig Bradney <mrb@scribus.info>
2009-06-02 21:33:08 +0000

    Remove extra about.cpp as we have it in the ui dir now, bump that one's date
    git-svn-id: svn://scribus.net/trunk/Scribus@13629 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -114,8 +114,8 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
 	buildID->setTextInteractionFlags(Qt::TextSelectableByMouse);
-	QString BUILD_DAY = "25";
-	QString BUILD_MONTH = CommonStrings::may;
+	QString BUILD_DAY = "2";
+	QString BUILD_MONTH = CommonStrings::june;
 	QString BUILD_YEAR = "2009";
 	QString BUILD_TIME = "";
 	QString BUILD_TZ = "";
