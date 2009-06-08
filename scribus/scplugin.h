b60b5637254e06721234f3ac49eaf1e2e2678ffa
Craig Bradney <mrb@scribus.info>
2009-06-08 18:58:13 +0000

    Bump plugin version for 1.5.0+
    git-svn-id: svn://scribus.net/trunk/Scribus@13662 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scplugin.h
+++ scribus/scplugin.h
@@ -489,7 +489,7 @@ class SCRIBUS_API ScPersistentPlugin : public ScPlugin
 //
 // The API version is currently simply incremented with each incompatible
 // change. Future versions may introduce a minor/major scheme if necessary.
-#define PLUGIN_API_VERSION 0x00000008
+#define PLUGIN_API_VERSION 0x00000010
 
 
 #endif
