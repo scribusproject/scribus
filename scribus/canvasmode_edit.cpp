4d6172036844d0694a34a813ae56cb163bcb6fe2
Jean Ghali <jghali@libertysurf.fr>
2009-06-08 18:56:59 +0000

    #8129: highlighted text deselected when right mouse button is pressed
    git-svn-id: svn://scribus.net/trunk/Scribus@13661 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/canvasmode_edit.cpp
+++ scribus/canvasmode_edit.cpp
@@ -1003,11 +1003,10 @@ void CanvasMode_Edit::mousePressEvent(QMouseEvent *m)
 				m_view->requestMode(modeNormal);
 				return;
 			}
-			else
-				currItem->asTextFrame()->deselectAll();
 
 			if (m->button() != Qt::RightButton)
 			{
+				currItem->asTextFrame()->deselectAll();
 				//<<CB Add in shift select to text frames
 				if (m->modifiers() & Qt::ShiftModifier && currItem->itemText.lengthOfSelection() > 0)
 				{
