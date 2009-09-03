e48ecd69a1b0f77c7402a9b7b1e6401906689aa5
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2009-09-03 22:14:20 +0000

    Fixed build issue when building with Qt's QPainter selected as rendering engine.
    git-svn-id: svn://scribus.net/trunk/Scribus@13952 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scpainter.cpp
+++ scribus/scpainter.cpp
@@ -1095,7 +1095,7 @@ void ScPainter::drawVPath(int mode)
 			qmatrix.scale(patternScaleX, patternScaleY);
 			qmatrix.scale(m_pattern->width / static_cast<double>(m_pattern->getPattern()->width()), m_pattern->height / static_cast<double>(m_pattern->getPattern()->height()));
 			QBrush brush = QBrush(*m_pattern->getPattern());
-			brush.setMatrix(qmatrix);
+			brush.setTransform(qmatrix);
 			painter.rotate(0.0001);	// hack to get Qt-4's strange pattern rendering working
 			painter.setOpacity(fill_trans);
 			painter.fillPath(m_path, brush);
