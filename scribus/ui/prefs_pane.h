a9a8e518d4795caacc63cd3cdb5d78f0c5560482
Craig Bradney <mrb@scribus.info>
2009-09-26 23:27:37 +0000

    Add missing prefs_pane.h
    git-svn-id: svn://scribus.net/trunk/Scribus@14054 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/ui/prefs_pane.h
@@ -0,0 +1,24 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+
+#ifndef PREFS_PANE_H
+#define PREFS_PANE_H
+
+#include <QWidget>
+
+class Prefs_Pane : public QWidget
+{
+	Q_OBJECT
+
+	public:
+		Prefs_Pane(QWidget* parent) : QWidget(parent) {};
+		~Prefs_Pane() {} ;
+		virtual void restoreDefaults(struct ApplicationPrefs *prefsData) = 0;
+		virtual void saveGuiToPrefs(struct ApplicationPrefs *prefsData) const = 0;
+};
+
+#endif // PREFS_PANE_H
