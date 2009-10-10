a1986afe4c5c903aee17d749994a5323ba3da3be
Craig Bradney <mrb@scribus.info>
2009-10-10 22:00:49 +0000

    Add X11/Control Char hack to 1.3.6svn
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@14137 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem_textframe.cpp
+++ scribus/pageitem_textframe.cpp
@@ -3156,13 +3156,23 @@ void PageItem_TextFrame::handleModeEditKey(QKeyEvent *k, bool& keyRepeat)
 		bool doUpdate = false;
 		if (itemText.lengthOfSelection() > 0) // && (kk < 0x1000)
 		{
-			if (!k->text().isEmpty())
+			bool x11Hack=false;
+			bool controlCharHack=false;
+			if ((k->text().size()==1) && (k->text().at(0).unicode()<32))
+				controlCharHack=true;
+#if defined (Q_WS_X11)
+			if ((k->text().size()==1) && (k->modifiers() & Qt::ShiftModifier) && (k->modifiers() & Qt::ControlModifier) && (k->nativeVirtualKey() < 1000))
+				x11Hack=true;
+#endif
+			if (!controlCharHack && !x11Hack && !k->text().isEmpty())
 			{
 				deleteSelectedTextFromFrame();
 				doUpdate = true;
 			}
 			/*
+			qDebug()<<"Key:"<<k->key();
 			qDebug()<<"Text:"<<k->text();
+			qDebug()<<"Size:"<<k->text().size();
 			qDebug()<<"Modifiers:"<<k->modifiers();
 			qDebug()<<"Native Modifiers:"<<k->nativeModifiers();
 			qDebug()<<"Native Scan Code:"<<k->nativeScanCode();
