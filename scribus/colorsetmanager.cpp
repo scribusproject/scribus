e9b4125474bc3e9ed60edc0c8f3aa577c4335ab3
Craig Bradney <mrb@scribus.info>
2009-10-19 18:06:21 +0000

    Allow only xml and gpl files to be loaded by the colour manager so we dont try to load the license files
    git-svn-id: svn://scribus.net/trunk/Scribus@14195 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/colorsetmanager.cpp
+++ scribus/colorsetmanager.cpp
@@ -129,7 +129,7 @@ void ColorSetManager::findPalettes()
 	for ( QStringList::Iterator it = paletteLocations.begin(); it != paletteLocations.end(); ++it )
 	{
 		path=(*it);
-		QDir dir(path , "*", QDir::Name, QDir::Files | QDir::NoSymLinks);
+		QDir dir(path , "*.xml *.gpl", QDir::Name, QDir::Files | QDir::NoSymLinks);
 		if (dir.exists() && (dir.count() != 0))
 			for (uint i = 0; i < dir.count(); ++i) 
 			{
