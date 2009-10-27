941457b602eff21a81ac54d4bf98e1cf00d0460a
Craig Bradney <mrb@scribus.info>
2009-10-27 18:14:40 +0000

    Bump about date and comment out Qt3 related UTF-16 hack
    git-svn-id: svn://scribus.net/trunk/Scribus@14244 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -114,7 +114,7 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
 	buildID->setTextInteractionFlags(Qt::TextSelectableByMouse);
-	QString BUILD_DAY = "08";
+	QString BUILD_DAY = "27";
 	QString BUILD_MONTH = CommonStrings::october;
 	QString BUILD_YEAR = "2009";
 	QString BUILD_TIME = "";
