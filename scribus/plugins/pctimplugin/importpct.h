0e1e543ff15308180598b4ba36147044f2104be8
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2009-10-29 07:53:42 +0000

    Fixed incorrect #defines in the file headers.
    git-svn-id: svn://scribus.net/trunk/Scribus@14250 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/pctimplugin/importpct.h
+++ scribus/plugins/pctimplugin/importpct.h
@@ -4,8 +4,8 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#ifndef IMPORTCVG_H
-#define IMPORTCVG_H
+#ifndef IMPORTPCT_H
+#define IMPORTPCT_H
 
 
 #include "pluginapi.h"
--- scribus/plugins/pctimplugin/importpctplugin.h
+++ scribus/plugins/pctimplugin/importpctplugin.h
@@ -4,8 +4,8 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#ifndef IMPORTCVGPLUGIN_H
-#define IMPORTCVGPLUGIN_H
+#ifndef IMPORTPCTPLUGIN_H
+#define IMPORTPCTPLUGIN_H
 
 #include "pluginapi.h"
 #include "loadsaveplugin.h"
