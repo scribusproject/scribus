72fdff0d4047bcfcd0307913add0eb8c15f7e6a0
Jean Ghali <jghali@libertysurf.fr>
2009-12-11 14:57:00 +0000

    #8627: convert to outline then to image frames, click undo - page content disappears
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@14399 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem.h
+++ scribus/pageitem.h
@@ -518,7 +518,7 @@ public:
 	bool isSingleSel;
 	bool isGroupControl;
 	PageItem *groupsLastItem;
-	void setGroupsLastItem(PageItem* lnk) { groupsLastItem = lnk; }
+	void setGroupsLastItem(PageItem* item);
 	double BoundingX;
 	double BoundingY;
 	double BoundingW;
@@ -1046,6 +1046,7 @@ protected:
 	void restoreShapeType(SimpleState *state, bool isUndo);
 	void restoreLayer(SimpleState *state, bool isUndo);
 	void restoreGetImage(SimpleState *state, bool isUndo);
+	void restoreGroupsLastItem(SimpleState *state, bool isUndo);
 
 	void restoreShapeContour(UndoState *state, bool isUndo);
 	void restoreImageEffects(UndoState *state, bool isUndo);
