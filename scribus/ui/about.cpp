3d6859add9cb99a764c1ce4ece01207905b8d3d6
Craig Bradney <mrb@scribus.info>
2010-02-01 21:10:20 +0000

    Bump about date for the pedantic
    git-svn-id: svn://scribus.net/trunk/Scribus@14617 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -114,8 +114,8 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
 	buildID->setTextInteractionFlags(Qt::TextSelectableByMouse);
-	QString BUILD_DAY = "19";
-	QString BUILD_MONTH = CommonStrings::january;
+	QString BUILD_DAY = "1";
+	QString BUILD_MONTH = CommonStrings::february;
 	QString BUILD_YEAR = "2010";
 	QString BUILD_TIME = "";
 	QString BUILD_TZ = "";
