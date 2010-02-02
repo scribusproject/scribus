3538a736eed46bd8efc9bad321fcda19231f13dd
Craig Bradney <mrb@scribus.info>
2010-02-02 22:37:56 +0000

    Add functionality to new prefs pane for scripter (though I think the colour stuff is b0rken in the original anyway)
    git-svn-id: svn://scribus.net/trunk/Scribus@14630 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/prefs_scripter.cpp
+++ scribus/plugins/scriptplugin/prefs_scripter.cpp
@@ -5,14 +5,42 @@ a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
 
+// include the python header first because on OSX that uses the name "slots"
+#include "cmdvar.h"
+
+#include <QColorDialog>
+#include <QFileDialog>
+
 #include "prefs_scripter.h"
+#include "scriptercore.h"
+#include "pconsole.h"
 #include "prefsstructs.h"
 
 Prefs_Scripter::Prefs_Scripter(QWidget* parent)
 	: Prefs_Pane(parent)
 {
 	setupUi(this);
+	languageChange();
+	setupSyntaxColors();
 
+	// Set the state of the ext script enable checkbox
+	extensionScriptsChk->setChecked(scripterCore->extensionsEnabled());
+	// The startup script box should be disabled  if ext scripts are off
+	startupScriptEdit->setEnabled(extensionScriptsChk->isChecked());
+	connect(extensionScriptsChk, SIGNAL(toggled(bool)),
+			startupScriptEdit, SLOT(setEnabled(bool)));
+
+	// signals and slots connections
+	connect(extensionScriptsChk, SIGNAL(toggled(bool)), startupScriptEdit, SLOT(setEnabled(bool)));
+	// colors
+	connect(textButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(commentButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(keywordButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(errorButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(signButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(stringButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(numberButton, SIGNAL(clicked()), this, SLOT(setColor()));
+	connect(startupScriptChangeButton, SIGNAL(clicked()), this, SLOT(changeStartupScript()));
 }
 
 Prefs_Scripter::~Prefs_Scripter()
@@ -31,7 +59,65 @@ void Prefs_Scripter::saveGuiToPrefs(struct ApplicationPrefs *prefsData) const
 {
 }
 
+// Apply changes to prefs. Auto connected.
 void Prefs_Scripter::apply()
 {
-
+	scripterCore->setExtensionsEnabled(extensionScriptsChk->isChecked());
+	scripterCore->setStartupScript(startupScriptEdit->text());
+	// colors
+	SyntaxColors *syntax = new SyntaxColors();
+	syntax->textColor = textButton->palette().color(QPalette::Window);
+	syntax->commentColor = commentButton->palette().color(QPalette::Window);
+	syntax->keywordColor = keywordButton->palette().color(QPalette::Window);
+	syntax->errorColor = errorButton->palette().color(QPalette::Window);
+	syntax->signColor = signButton->palette().color(QPalette::Window);
+	syntax->stringColor = stringButton->palette().color(QPalette::Window);
+	syntax->numberColor = numberButton->palette().color(QPalette::Window);
+	delete(syntax);
 }
+
+void Prefs_Scripter::setColor()
+{
+	QPalette palette;
+	QPushButton* button = (QPushButton*)sender();
+	QColor color = QColorDialog::getColor(button->palette().color(QPalette::Window), this);
+	if (color.isValid())
+	{
+		palette.setColor(button->backgroundRole(), color);
+ 		button->setPalette(palette);
+	}
+}
+
+void Prefs_Scripter::setupSyntaxColors()
+{
+	QPalette palette;
+	SyntaxColors *syntax = new SyntaxColors();
+	palette.setColor(textButton->backgroundRole(), syntax->textColor);
+ 	textButton->setPalette(palette);
+	palette.setColor(commentButton->backgroundRole(), syntax->commentColor);
+ 	commentButton->setPalette(palette);
+	palette.setColor(keywordButton->backgroundRole(), syntax->keywordColor);
+ 	keywordButton->setPalette(palette);
+	palette.setColor(errorButton->backgroundRole(), syntax->errorColor);
+ 	errorButton->setPalette(palette);
+	palette.setColor(signButton->backgroundRole(), syntax->signColor);
+ 	signButton->setPalette(palette);
+	palette.setColor(stringButton->backgroundRole(), syntax->stringColor);
+ 	stringButton->setPalette(palette);
+	palette.setColor(numberButton->backgroundRole(), syntax->numberColor);
+ 	numberButton->setPalette(palette);
+	delete(syntax);
+}
+
+void Prefs_Scripter::changeStartupScript()
+{
+	QString currentScript=startupScriptEdit->text();
+	QFileInfo fi(startupScriptEdit->text());
+	if (!fi.exists())
+		currentScript = QDir::homePath();
+
+	QString s = QFileDialog::getOpenFileName(this, tr("Locate Startup Script"), currentScript, "Python Scripts (*.py *.PY)");
+	if (!s.isEmpty())
+		startupScriptEdit->setText(s);
+}
+
--- scribus/plugins/scriptplugin/prefs_scripter.h
+++ scribus/plugins/scriptplugin/prefs_scripter.h
@@ -10,9 +10,8 @@ for which a new license (GPL+exception) is in place.
 
 #include "ui_prefs_scripterbase.h"
 #include "ui/prefs_pane.h"
-#include "scribusapi.h"
 
-class SCRIBUS_API Prefs_Scripter : public Prefs_Pane, Ui::Prefs_Scripter
+class Prefs_Scripter : public Prefs_Pane, Ui::Prefs_Scripter
 {
 	Q_OBJECT
 
@@ -26,6 +25,17 @@ class SCRIBUS_API Prefs_Scripter : public Prefs_Pane, Ui::Prefs_Scripter
 		void languageChange();
 		//! \brief Apply changes to prefs. Auto connected.
 		void apply();
+
+
+	protected:
+		void setupSyntaxColors();
+
+	protected slots:
+		/*! \brief All requests for color change are handled here.
+		\author Petr Vanek
+		\warning I'm trying to handle multiple signals via single slot here. sender() Returns a pointer to the object that sent the signal, if called in a slot activated by a signal; otherwise the return value is undefined. This function will return something apparently correct in other cases as well. However, its value may change during any function call, depending on what signal-slot connections are activated during that call. In Qt 3.0 the value will change more often than in 2.x. This function violates the object-oriented principle of modularity. However, getting access to the sender might be useful when many signals are connected to a single slot. The sender is undefined if the slot is called as a normal C++ function. */
+		void setColor();
+		void changeStartupScript();
 };
 
 #endif // PREFS_SCRIPTER_H
