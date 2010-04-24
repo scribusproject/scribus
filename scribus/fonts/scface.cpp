091e4223c6596cd5618f10616eaafba1da9431bf
Jean Ghali <jghali@libertysurf.fr>
2010-04-24 21:15:19 +0000

    #9053: text alignment doesn't work with some fonts if an automatic page number is inserted
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@15011 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/fonts/scface.cpp
+++ scribus/fonts/scface.cpp
@@ -52,7 +52,7 @@ GlyphMetrics ScFace::ScFaceData::glyphBBox(uint gl, qreal sz) const
 	GlyphMetrics res;
 	if (gl == 0 || gl >= CONTROL_GLYPHS)
 	{	res.width   = glyphWidth(gl, sz);
-		res.ascent  = (gl == 0? sz : 0);
+		res.ascent  = (gl == 0? ascent(sz) : 0);
 		res.descent = 0;
 		return res;
 	}
