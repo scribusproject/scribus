1445a035b76f2fc5ab15215f2c7a26b007fa4efe
Jean Ghali <jghali@libertysurf.fr>
2010-06-16 22:32:34 +0000

    missing comment in storyeditor.cpp
    git-svn-id: svn://scribus.net/trunk/Scribus@15203 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/storyeditor.cpp
+++ scribus/ui/storyeditor.cpp
@@ -2888,6 +2888,8 @@ void StoryEditor::updateTextFrame()
 	}
 #endif
 	Editor->saveItemText(nextItem);
+	// #9180 : force relayout here, it appears that relayout is sometime disabled
+	// to speed up selection, but re layout() cannot be avoided here
 	nextItem->invalidateLayout();
 	nextItem->layout();
 #if 0
