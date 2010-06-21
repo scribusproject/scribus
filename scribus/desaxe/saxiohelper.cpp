18ab180fb36e339c41b026fa76c64d12a8158094
Jean Ghali <jghali@libertysurf.fr>
2010-06-21 18:56:25 +0000

    #9193 : Missing copyright info in some files
    git-svn-id: svn://scribus.net/trunk/Scribus@15250 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/desaxe/saxiohelper.cpp
+++ scribus/desaxe/saxiohelper.cpp
@@ -1,14 +1,17 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+
 #include "saxiohelper.h"
 #include "saxio.h"
 #include "scfonts.h"
 #include "fpointarray.h"
 #include "scclocale.h"
 
-
-
 const Xml_string SaxIO::saxxDefaultElem("");
-
-
 
 Xml_string mkXMLName(const QString& any)
 {
