061d4c17fd62284920a9d852c6ba140555ae6606
Jean Ghali <jghali@libertysurf.fr>
2010-06-26 10:48:02 +0000

    #9212 : line ending arrows are exported incorrectly to PDF if put on master page
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@15272 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pdflib_core.cpp
+++ scribus/pdflib_core.cpp
@@ -1986,6 +1986,7 @@ bool PDFLibCore::PDF_TemplatePage(const Page* pag, bool )
 						if (ite->endArrowIndex() != 0)
 						{
 							QMatrix arrowTrans;
+							arrowTrans.translate(ite->width(), 0);
 							PutPage(drawArrow(ite, arrowTrans, ite->endArrowIndex()));
 						}
 						break;
