37e0160c7f72816169332f3fc56213bea5b58237
Craig Bradney <mrb@scribus.info>
2010-08-17 19:18:35 +0000

    #9322: Add in freebsd build checks
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@15420 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scclocale.h
+++ scribus/scclocale.h
@@ -21,14 +21,14 @@
 #include <xlocale.h>
 #endif
 
-#if defined(Q_OS_SOLARIS) || defined (Q_OS_OPENBSD)
+#if defined(Q_OS_SOLARIS) || defined (Q_OS_OPENBSD) || defined (Q_OS_FREEBSD)
 #include <locale.h>
 #endif
 
 #if defined(Q_WS_WIN)
 #define XLocaleType _locale_t
 #else
-  #if defined (Q_OS_SOLARIS) || defined (Q_OS_OPENBSD)
+  #if defined (Q_OS_SOLARIS) || defined (Q_OS_OPENBSD) || defined (Q_OS_FREEBSD)
   #define XLocaleType char*   //dummy?
   #else
   #define XLocaleType locale_t
