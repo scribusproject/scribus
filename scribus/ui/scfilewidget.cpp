6f99798054e62e39ea99a508ef63cca312366be9
Craig Bradney <mrb@scribus.info>
2010-08-17 19:57:53 +0000

    #8367: Add Computer and Volumes to the sidebar of the custom file dialogs
    git-svn-id: svn://scribus.net/trunk/Scribus@15422 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/scfilewidget.cpp
+++ scribus/ui/scfilewidget.cpp
@@ -4,18 +4,33 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
+
+#include <QDesktopServices>
 #include <QPushButton>
+#include <QUrl>
 
 #include "scfilewidget.h"
 
 
-ScFileWidget::ScFileWidget(QWidget * parent)
-	: QFileDialog(parent, Qt::Widget)
+ScFileWidget::ScFileWidget(QWidget * parent) : QFileDialog(parent, Qt::Widget)
 {
 	setSizeGripEnabled(false);
 	setModal(false);
 	setViewMode(QFileDialog::List);
 	setWindowFlags(Qt::Widget);
+
+#ifdef Q_OS_MAC
+	QList<QUrl> urls;
+	QUrl computer(QUrl::fromLocalFile(QLatin1String("")));
+	if (!urls.contains(computer))
+		urls << computer;
+	QUrl volumes(QUrl::fromLocalFile("/Volumes"));
+	if (!urls.contains(volumes))
+		urls << volumes;
+	//desktop too?	QUrl computer(QUrl::fromLocalFile(QDesktopServices::storageLocation(QDesktopServices::DesktopLocation)));
+	setSidebarUrls(urls);
+#endif
+
 	QList<QPushButton *> b = findChildren<QPushButton *>();
 	QListIterator<QPushButton *> i(b);
 	while (i.hasNext())
