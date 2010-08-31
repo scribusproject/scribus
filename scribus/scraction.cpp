4f7e0fd811246a8bd780b7f53da2a40987c4cf26
Craig Bradney <mrb@scribus.info>
2010-08-31 20:18:31 +0000

    Fix oops commit
    git-svn-id: svn://scribus.net/trunk/Scribus@15462 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scraction.cpp
+++ scribus/scraction.cpp
@@ -41,15 +41,15 @@ ScrAction::ScrAction( ActionType aType, const QPixmap & icon16, const QPixmap & 
 {
 	setShortcut(accel);
 	initScrAction();
-	//icon().addPixmap(icon22, QIcon::Normal, QIcon::On);
+	icon().addPixmap(icon22, QIcon::Normal, QIcon::On);
 
-	QPixmap gray_icon16(icon16);
-	iconToGrayscale(&gray_icon16);
-	m_icon.addPixmap(icon16, QIcon::Active, QIcon::On);
-	m_icon.addPixmap(icon16, QIcon::Active, QIcon::Off);
-	m_icon.addPixmap(gray_icon16, QIcon::Normal, QIcon::On);
-	m_icon.addPixmap(gray_icon16, QIcon::Normal, QIcon::Off);
-	setIcon(m_icon.pixmap(QSize(16,16), QIcon::Normal, QIcon::Off));
+//	QPixmap gray_icon16(icon16);
+//	iconToGrayscale(&gray_icon16);
+//	m_icon.addPixmap(icon16, QIcon::Active, QIcon::On);
+//	m_icon.addPixmap(icon16, QIcon::Active, QIcon::Off);
+//	m_icon.addPixmap(gray_icon16, QIcon::Normal, QIcon::On);
+//	m_icon.addPixmap(gray_icon16, QIcon::Normal, QIcon::Off);
+//	setIcon(m_icon.pixmap(QSize(16,16), QIcon::Normal, QIcon::Off));
 	//connect (this, SIGNAL(hovered()), this, SLOT(hoverIcon()));
 	_actionType=aType;
 	if (_actionType!=Normal)
