d8d4c1675ee62c48dc143600092829114790488e
Jean Ghali <jghali@libertysurf.fr>
2010-09-29 06:09:35 +0000

    #9339: Translated template category names lead to the wrong category
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@15490 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/nftwidget.cpp
+++ scribus/nftwidget.cpp
@@ -49,7 +49,6 @@ void nftwidget::setupCategories()
 {
 	QStringList categories;
 	categoryList->clear();
-	categoryList->addItem(tr("All"));
 	for (uint i = 0; i < settings->templates.size(); i++)
 	{
 		if ((!categories.contains(settings->templates[i]->templateCategory)) &&
@@ -60,6 +59,7 @@ void nftwidget::setupCategories()
 		}
 	}
 	categoryList->sortItems();
+	categoryList->insertItem(0, tr("All"));
 	categoryList->setCurrentRow(0);
 }
 
