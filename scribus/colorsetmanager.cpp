856112fa661a2724c497454814f16129e5486d1b
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2010-10-15 19:55:33 +0000

    Scribus didn't find the predefined default swatches --> fixed.
    git-svn-id: svn://scribus.net/trunk/Scribus@15541 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/colorsetmanager.cpp
+++ scribus/colorsetmanager.cpp
@@ -111,7 +111,7 @@ void ColorSetManager::findPaletteLocations()
 {
 	paletteLocations.clear();
 	QStringList locations=ScPaths::instance().getSystemCreateSwatchesDirs();
-	locations << ScPaths::instance().shareDir()+"swatches/";
+	locations << ScPaths::instance().libDir()+"swatches/";
 	locations << ScPaths::instance().getDirsFromEnvVar("XDG_DATA_HOME", "scribus/swatches/");
 	for ( QStringList::Iterator it = locations.begin(); it != locations.end(); ++it )
 	{
