796f5662afc47d85ad85f09aa02c7cc0573b846c
Jean Ghali <jghali@libertysurf.fr>
2010-10-26 19:48:05 +0000

    #9337: the font of the first line of the text in the story editor cannot be changed after first change #9376: text Display Broken after reopening document #9428: copy/paste in text frame are modyfing other characters
    Revert commit r15368, cause more bugs than it fixes
    
    git-svn-id: svn://scribus.net/trunk/Scribus@15658 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/text/storytext.cpp
+++ scribus/text/storytext.cpp
@@ -633,21 +633,26 @@ void StoryText::applyCharStyle(int pos, uint len, const CharStyle& style )
 	ScText* itText;
 	for (uint i=pos; i < pos+len; ++i) {
 		itText = d->at(i);
-		// #6165 : applying style on last character applies style on whole text on next open
+		// #6165 : applying style on last character applies style on whole text on next open 
+		/*if (itText->ch == SpecialChars::PARSEP && itText->parstyle != NULL)
+			itText->parstyle->charStyle().applyCharStyle(style);*/
+		
+		// Does not work well, do not reenable before checking #9337, #9376 and #9428
 		// #9173 et. al.: move charstyle to parstyle if whole paragraph is affected
-		if (itText->ch == SpecialChars::PARSEP && itText->parstyle != NULL && lastParStart >= 0)
+		/*if (itText->ch == SpecialChars::PARSEP && itText->parstyle != NULL && lastParStart >= 0)
 		{
 			eraseCharStyle(lastParStart, i - lastParStart, style);
 			itText->parstyle->charStyle().applyCharStyle(style);
 			lastParStart = i + 1;
-		}
+		}*/
 		itText->applyCharStyle(style);
 	}
-	if (pos + signed(len) == length() && lastParStart >= 0)
+	// Does not work well, do not reenable before checking #9337, #9376 and #9428
+	/*if (pos + signed(len) == length() && lastParStart >= 0)
 	{
 		eraseCharStyle(lastParStart, length() - lastParStart, style);
 		d->trailingStyle.charStyle().applyCharStyle(style);
-	}
+	}*/
 	
 	invalidate(pos, pos + len);
 }
@@ -673,10 +678,11 @@ void StoryText::eraseCharStyle(int pos, uint len, const CharStyle& style )
 			itText->parstyle->charStyle().eraseCharStyle(style);
 		itText->eraseCharStyle(style);
 	}
-	if (pos + signed(len) == length())
+	// Does not work well, do not reenable before checking #9337, #9376 and #9428
+	/*if (pos + signed(len) == length())
 	{
 		d->trailingStyle.charStyle().eraseCharStyle(style);
-	}
+	}*/
 	
 	invalidate(pos, pos + len);
 }
