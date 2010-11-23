92f7e5035410d4ba158d8ffc8c1c990e99606de7
Jean Ghali <jghali@libertysurf.fr>
2010-11-23 00:29:02 +0000

    #9514: Potential crash in style manager after closing doc
    git-svn-id: svn://scribus.net/trunk/Scribus@15960 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/stylemanager.cpp
+++ scribus/ui/stylemanager.cpp
@@ -205,6 +205,7 @@ void StyleManager::setDoc(ScribusDoc *doc)
 
 	// clear the style list and reload from new doc
 	styleView->clear();
+	m_selectedStyleAction = 0;
 	m_styleActions.clear();
 	for (int i = 0; i < m_items.count(); ++i)
 	{
