bcea8c7b4b37d20c5fc865d8c234aafdb86711c9
Peter Linnell <mrdocs@scribus.info>
2010-11-26 22:06:01 +0000

    version bumps - here comes 1.3.9
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16004 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.3.9.svn"
+#cmakedefine VERSION "1.3.9"
 
 #cmakedefine BUILD_WINDOWS_VERSION 1
 #cmakedefine SC_USE_GDI 1
