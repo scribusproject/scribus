f771aef03d001760bd26ec213cb161bcac160d8a
Jean Ghali <jghali@libertysurf.fr>
2010-12-05 21:47:49 +0000

    - set version in win-config.h to 1.4.0svn - allow 134+ fileloader to load 1.4.0svn files
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16072 11d20701-8431-0410-a711-e3c959e3b870

--- win32/vc8/win-config.h
+++ win32/vc8/win-config.h
@@ -177,7 +177,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.3.9svn"
+#define VERSION "1.4.0svn"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
