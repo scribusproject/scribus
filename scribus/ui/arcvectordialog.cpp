4ccd843103066657802120706eacd47799c667b7
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2011-01-17 22:38:59 +0000

    Added the missing dialog files.
    git-svn-id: svn://scribus.net/trunk/Scribus@16269 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/ui/arcvectordialog.cpp
@@ -0,0 +1,85 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+/***************************************************************************
+                          arcvectordialog.cpp  -  description
+                             -------------------
+    begin                : Mon Jan 17 2011
+    copyright            : (C) 2011 by Franz Schmid
+    email                : Franz.Schmid@altmuehlnet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "arcvectordialog.h"
+
+ArcVectorDialog::ArcVectorDialog(QWidget* parent) : ScrPaletteBase( parent, "GradientVectorPalette", false, 0 )
+{
+	setupUi(this);
+	startAngle->setNewUnit(6);
+	startAngle->setValues(-360, 360, 1, 0);
+	sweepAngle->setNewUnit(6);
+	sweepAngle->setValues(-360, 360, 1, 0);
+	connect(startAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(sweepAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(arcHeight,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(arcWidth,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	languageChange();
+}
+
+void ArcVectorDialog::changeEvent(QEvent *e)
+{
+	if (e->type() == QEvent::LanguageChange)
+	{
+		languageChange();
+	}
+	else
+		QWidget::changeEvent(e);
+}
+
+void ArcVectorDialog::languageChange()
+{
+	retranslateUi(this);
+	resize(minimumSizeHint());
+}
+
+void ArcVectorDialog::setValues(double start, double sweep, double height, double width)
+{
+	disconnect(startAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	disconnect(sweepAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	disconnect(arcHeight,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	disconnect(arcWidth,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	startAngle->setValue(start);
+	sweepAngle->setValue(sweep);
+	arcHeight->setValue(height);
+	arcWidth->setValue(width);
+	connect(startAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(sweepAngle,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(arcHeight,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(arcWidth,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+}
+
+void ArcVectorDialog::changeVectors()
+{
+	emit NewVectors(startAngle->value(), sweepAngle->value(), arcHeight->value(), arcWidth->value());
+}
+
+void ArcVectorDialog::unitChange(int unitIndex)
+{
+	disconnect(arcHeight,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	disconnect(arcWidth,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	arcHeight->setNewUnit(unitIndex);
+	arcWidth->setNewUnit(unitIndex);
+	connect(arcHeight,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+	connect(arcWidth,   SIGNAL(valueChanged(double)), this, SLOT(changeVectors()));
+}
---
+++ scribus/ui/arcvectordialog.h
@@ -0,0 +1,59 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+/***************************************************************************
+                          arcvectordialog.h  -  description
+                             -------------------
+    begin                : Mon Jan 17 2011
+    copyright            : (C) 2011 by Franz Schmid
+    email                : Franz.Schmid@altmuehlnet.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef ARCVECTOR_H
+#define ARcVECTOR_H
+
+#include "ui_arcvectorbase.h"
+
+#include <QWidget>
+#include <QLayout>
+#include <QLabel>
+#include <QEvent>
+#include "scribusapi.h"
+#include "ui/scrpalettebase.h"
+
+/**
+  *@author Franz Schmid
+  */
+
+class SCRIBUS_API ArcVectorDialog :  public ScrPaletteBase, Ui::ArcVectorBase
+{
+	Q_OBJECT
+
+public:
+	ArcVectorDialog( QWidget* parent);
+	~ArcVectorDialog() {};
+	virtual void changeEvent(QEvent *e);
+
+public slots:
+	void languageChange();
+	void changeVectors();
+	void setValues(double start, double sweep, double height, double width);
+	void unitChange(int unitIndex);
+
+signals:
+	void NewVectors(double, double, double, double);
+
+};
+#endif
