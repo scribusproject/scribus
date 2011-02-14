4058ba38709b41c287ae10530f19aa9e68424d91
Craig Bradney <mrb@scribus.info>
2011-02-14 19:22:10 +0000

    #8232: Use the system libhyphen if available (rhel/fedora/debian/ubuntu at least)
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16358 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/hyphenator.h
+++ scribus/hyphenator.h
@@ -13,7 +13,7 @@ for which a new license (GPL+exception) is in place.
 #include <QSet>
 
 #include "scribusapi.h"
-#include "hyphen.h"
+#include <hyphen.h>
 class ScribusDoc;
 class ScribusMainWindow;
 class PageItem;
