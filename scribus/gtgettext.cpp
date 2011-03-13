95a422d9c4f57f49aaa6e6b47e45b261dc570956
Craig Bradney <mrb@scribus.info>
2011-03-13 00:12:31 +0000

    #3007: Enable searching for uppercase extensions in gettext plugins
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16444 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/gtgettext.cpp
+++ scribus/gtgettext.cpp
@@ -60,12 +60,18 @@ void gtGetText::launchImporter(int importer, const QString& filename, bool textO
 	if (importer == -1)
 	{
 		// Attempt to determine the importer based on the file's extension. 
-		// Create a Qstring with what could be an extension.
+		// Create a QString with what could be an extension.
 		QString fend = filename.right(filename.length() - filename.lastIndexOf(".") - 1);
+		QString fendL(fend.toLower());
 		// Look for that extension in the importer Qmap. 
 		if (importerMap.find(fend) != importerMap.end())
 			// If the map is found, assign ida to the corresponding struct in the map.
 			ida = *importerMap[fend];
+		// Otherwise, test for the lowercase version
+		else
+		if (importerMap.find(fendL) != importerMap.end())
+			// If the map is found, assign ida to the corresponding struct in the map.
+			ida = *importerMap[fendL];
 		// Otherwise, try and ask the user.
 		else
 		{
