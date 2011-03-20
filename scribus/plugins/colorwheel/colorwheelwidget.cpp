4297c405e560e08d2cdc45fcd5c628899bdac777
Jean Ghali <jghali@libertysurf.fr>
2011-03-20 12:49:13 +0000

    #9856: Frequent ""Unable to find the requested color..." error in colorwheel widget
    git-svn-id: svn://scribus.net/trunk/Scribus@16464 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/colorwheel/colorwheelwidget.cpp
+++ scribus/plugins/colorwheel/colorwheelwidget.cpp
@@ -311,19 +311,19 @@ bool ColorWheel::recomputeColor(ScColor col)
 	QColor act(ScColorEngine::getRGBColor(actualColor, currentDoc));
 
 	c.getHsv(&origh, &origs, &origv);
-	for (it = colorMap.begin(); it != colorMap.end(); ++it)
+	angle = origh + angleShift;
+	if (angle > 359)
+		angle -= 360;
+	if (colorMap.contains(angle))
 	{
 		int tmph, tmps, tmpv;
-		QColor col(ScColorEngine::getRGBColor(it.value(), currentDoc));
+		QColor col(ScColorEngine::getRGBColor(colorMap[angle], currentDoc));
 		col.getHsv(&tmph, &tmps, &tmpv);
-		if (origh == tmph)
-		{
-			act.setHsv(tmph, origs, origv);
-			actualColor.fromQColor(act);
-			actualColor = ScColorEngine::convertToModel(actualColor, currentDoc, currentColorSpace);
-			baseAngle = it.key();
-			return true;
-		}
+		act.setHsv(tmph , origs, origv);
+		actualColor.fromQColor(act);
+		actualColor = ScColorEngine::convertToModel(actualColor, currentDoc, currentColorSpace);
+		baseAngle = angle;
+		return true;
 	}
 	return false;
 }
