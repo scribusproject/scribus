9606b68f3f47165314ed6e1b06c103cecdf96b2d
Jean Ghali <jghali@libertysurf.fr>
2011-07-02 22:14:02 +0000

    #9894: Undoing reversing write direction
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16702 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem.cpp
+++ scribus/pageitem.cpp
@@ -693,23 +693,25 @@ void PageItem::moveImageXYOffsetBy(const double dX, const double dY)
 {
 	if (dX==0.0 && dY==0.0)
 		return;
-	if (dX!=0.0)
-		LocalX+=dX;
-	if (dY!=0.0)
-		LocalY+=dY;
+	if (dX != 0.0)
+		LocalX += dX;
+	if (dY != 0.0)
+		LocalY += dY;
 	checkChanges();
 	emit imageOffsetScale(LocalScX, LocalScY, LocalX, LocalY);
 }
 
 void PageItem::setReversed(bool newReversed)
 {
+	if (Reverse == newReversed)
+		return;
 	if (UndoManager::undoEnabled())
 	{
 		SimpleState *ss = new SimpleState(Um::FlipH, 0, Um::IFlipH);
-		ss->set("REVERSE_TEXT", "REVERSE_TEXT");
+		ss->set("REVERSE_TEXT", newReversed);
 		undoManager->action(this, ss);
 	}
-	Reverse=newReversed;
+	Reverse = newReversed;
 }
 
 //return frame where is text end
@@ -3300,7 +3302,9 @@ void PageItem::restore(UndoState *state, bool isUndo)
 		else if (ss->contains("LINK_TEXT_FRAME"))
 			restoreLinkTextFrame(ss,isUndo);
 		else if (ss->contains("UNLINK_TEXT_FRAME"))
-			restoreUnlinkTextFrame(ss,isUndo);
+			restoreUnlinkTextFrame(ss, isUndo);
+		else if (ss->contains("REVERSE_TEXT"))
+			restoreReverseText(ss, isUndo);
 	}
 	if (!OnMasterPage.isEmpty())
 		m_Doc->setCurrentPage(oldCurrentPage);
@@ -3716,6 +3720,12 @@ void PageItem::restoreUnlinkTextFrame(UndoState *state, bool isUndo)
 	}
 }
 
+void PageItem::restoreReverseText(UndoState *state, bool /*isUndo*/)
+{
+	if (!isTextFrame())
+		return;
+	Reverse = !Reverse;
+}
 
 void PageItem::restorePoly(SimpleState *state, bool isUndo, bool isContour)
 {
