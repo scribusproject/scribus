779668b6ae67decbc3937f67e06837c1df1a3e0c
Craig Bradney <mrb@scribus.info>
2011-09-15 20:12:57 +0000

    Backport proper #9886 fix from GSoC Tables work
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16835 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/canvasmode_edit.cpp
+++ scribus/canvasmode_edit.cpp
@@ -171,10 +171,6 @@ void CanvasMode_Edit::drawControls(QPainter* p)
 
 void CanvasMode_Edit::drawTextCursor ( QPainter *p, PageItem_TextFrame* textframe )
 {
-//CB: If we have this test in we get no initial cursor placed for a new text frame
-// but if we do, we crash when we resize.. #9886
-	if(textframe->lastInFrame() < 0)
-		return;
 	if ((!m_longCursorTime && m_blinkTime.elapsed() > qApp->cursorFlashTime() / 2 ) ||
 		(m_longCursorTime && m_blinkTime.elapsed() > qApp->cursorFlashTime() )
 		)
@@ -223,7 +219,7 @@ void CanvasMode_Edit::drawTextCursor ( QPainter *p, PageItem_TextFrame* textfram
 		{
 			// Happens often when typing directly into frame.
 			// And the cursor curses nothing, vertigo.
-			textCursorPos = textframe->lastInFrame();
+			textCursorPos = qMax(0, textframe->lastInFrame());
 			QChar textCursorChar = textframe->itemText.text(textCursorPos);
 			if (textCursorChar == SpecialChars::PARSEP || textCursorChar == SpecialChars::LINEBREAK)
 			{
