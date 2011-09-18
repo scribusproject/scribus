6efafa219fe7f3655132501b90fdd7ac0e79526e
Craig Bradney <mrb@scribus.info>
2011-09-18 21:45:02 +0000

    #10263: Apply patch for master page import dialog
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@16843 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/about.cpp
+++ scribus/about.cpp
@@ -114,8 +114,8 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
 	buildID->setTextInteractionFlags(Qt::TextSelectableByMouse);
-	QString BUILD_DAY = "11";
-	QString BUILD_MONTH = CommonStrings::august;
+	QString BUILD_DAY = "18";
+	QString BUILD_MONTH = CommonStrings::september;
 	QString BUILD_YEAR = "2011";
 	QString BUILD_TIME = "";
 	QString BUILD_TZ = "";
