d076d5b70ed218d7036fba0899218652879f73b6
Jean Ghali <jghali@libertysurf.fr>
2011-12-15 13:12:09 +0000

    fix incorrect line break which sometime occurs when line ends with a break character
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@17086 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem_textframe.cpp
+++ scribus/pageitem_textframe.cpp
@@ -1750,12 +1750,17 @@ void PageItem_TextFrame::layout()
 					continue;
 				}
 				//there we have some glyphs
-				if ((current.breakIndex < 0) && !SpecialChars::isBreak(hl->ch, Cols > 1))
-				{
-					//force break
-					a--;
-					current.breakLine(itemText, style, firstLineOffset(),a);
-				}
+				if (current.breakIndex < 0)
+ 				{
+ 					//force break
+					if (!SpecialChars::isBreak(hl->ch, Cols > 1))
+					{
+						//force line end at previouse glyph
+						a--;
+						current.mustLineEnd = current.line.x;
+					}
+ 					current.breakLine(itemText, style, firstLineOffset(),a);
+ 				}
 				if (!current.addLine && !current.lastInRowLine && current.afterOverflow)
 				{
 					//we reach right edge of column
@@ -1763,9 +1768,7 @@ void PageItem_TextFrame::layout()
 					//if we have some text here - insert text WITHOUT right margin
 					//if there is no place for text - insert text WITH right margin and end line
 					current.lastInRowLine = false;
-					if (current.breakIndex <0)
-						current.mustLineEnd = current.line.x -1;
-					else if (current.line.firstItem == current.restartIndex)
+					if (current.line.firstItem == current.restartIndex)
 						current.lastInRowLine = true;
 					if (current.hasDropCap && DropLinesCount == 0 && current.restartIndex == current.restartRowIndex)
 					{
@@ -1877,7 +1880,7 @@ void PageItem_TextFrame::layout()
 								if (current.breakIndex < 0)
 								{
 									current.lastInRowLine = true;
-									current.mustLineEnd = current.line.x -1;
+									current.mustLineEnd = current.line.x;
 								}
 								else if (current.line.firstItem == current.restartIndex)
 									current.lastInRowLine = true;
