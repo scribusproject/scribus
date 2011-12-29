3cb3f3f001629835154ef342bece0bb7e37f7bee
Jean Ghali <jghali@libertysurf.fr>
2011-12-29 01:08:38 +0000

    msvc project update
    git-svn-id: svn://scribus.net/branches/Version135/Scribus@17141 11d20701-8431-0410-a711-e3c959e3b870

--- win32/vc9/win-config.h
+++ win32/vc9/win-config.h
@@ -180,7 +180,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.4.0svn"
+#define VERSION "1.4.0"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
