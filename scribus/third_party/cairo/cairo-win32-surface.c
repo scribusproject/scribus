24c83aed9cb2376f1d8887e12bd3ef5962574ffa
Jean Ghali <jghali@libertysurf.fr>
2012-01-08 18:26:02 +0000

    small win32 specific fixes for cairo private copy
    git-svn-id: svn://scribus.net/trunk/Scribus@17188 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/third_party/cairo/cairo-win32-surface.c
+++ scribus/third_party/cairo/cairo-win32-surface.c
@@ -94,14 +94,16 @@ _cairo_win32_print_gdi_error (const char *context)
 			 NULL,
 			 last_error,
 			 MAKELANGID (LANG_NEUTRAL, SUBLANG_DEFAULT),
-			 (LPSTR) &lpMsgBuf,
+			 (LPWSTR) &lpMsgBuf,
 			 0, NULL)) {
 	fprintf (stderr, "%s: Unknown GDI error", context);
     } else {
-	fwprintf (stderr, "%S: %s", context, (char *)lpMsgBuf);
+	fprintf (stderr, "%s: %S", context, (wchar_t *)lpMsgBuf);
 
 	LocalFree (lpMsgBuf);
     }
+
+	fflush (stderr);
 
     /* We should switch off of last_status, but we'd either return
      * CAIRO_STATUS_NO_MEMORY or CAIRO_STATUS_UNKNOWN_ERROR and there
