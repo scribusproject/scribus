32ae46a33dee0f8322a2f24b3f48238ddf9f0923
Jean Ghali <jghali@libertysurf.fr>
2012-09-20 20:47:05 +0000

    #9098: node palette disappears
    git-svn-id: svn://scribus.net/trunk/Scribus@17775 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribus.cpp
+++ scribus/scribus.cpp
@@ -2150,14 +2150,12 @@ void ScribusMainWindow::newActWin(QMdiSubWindow *w)
 {
 	if (w == NULL)
 	{
-		if ( mdiArea->subWindowList().count() == 0)
+		if (mdiArea->subWindowList().count() == 0)
 			ActWin = NULL;
 		return;
 	}
 	if (w->widget() == NULL)
 		return;
-	if (doc!=0 && doc->appMode == modeEditClip)
-		view->requestMode(submodeEndNodeEdit);
 	ScribusWin* scw = dynamic_cast<ScribusWin *>(w->widget());
 	if (!scw)
 		return;
@@ -2166,14 +2164,16 @@ void ScribusMainWindow::newActWin(QMdiSubWindow *w)
 		if (!scw->doc()->hasGUI())
 			return;
 	}
-	if(scw == ActWin)
+	if (scw == ActWin)
 		return;
 	ActWin = scw;
-	if (ActWin->doc()==NULL)
+	if (ActWin->doc() == NULL)
 		return;
 
 	if (doc != NULL)
 	{
+		if (doc->appMode == modeEditClip)
+			view->requestMode(submodeEndNodeEdit);
 		if ((HaveDoc) && (doc != ActWin->doc()))
 			outlinePalette->buildReopenVals();
 	}
