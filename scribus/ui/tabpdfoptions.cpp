e3c87e073747058ca83ae624fa4d8058bb36d3ae
Jean Ghali <jghali@libertysurf.fr>
2012-09-28 20:11:36 +0000

    fix profile embedding being sometime incorrectly enabled for PDF-X/1a
    git-svn-id: svn://scribus.net/trunk/Scribus@17784 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/tabpdfoptions.cpp
+++ scribus/ui/tabpdfoptions.cpp
@@ -1660,6 +1660,7 @@ void TabPDFOptions::EnablePDFX(int a)
 		X3Group->setEnabled(false);
 		setTabEnabled(indexOf(tabSecurity), true);
 		OutCombo->setEnabled(true);
+		EmbedProfs->setEnabled(true);
 		EmbedProfs2->setEnabled(true);
 		emit hasInfo();
 		if (doc != 0 && pdfExport)
@@ -1670,6 +1671,7 @@ void TabPDFOptions::EnablePDFX(int a)
 				FromEmbed->setEnabled(true);
 			ToEmbed->setEnabled(true);
 		}
+		EnablePr(OutCombo->currentIndex());
 		return;
 	}
 	// PDF/X is selected
@@ -1677,8 +1679,16 @@ void TabPDFOptions::EnablePDFX(int a)
 	OutCombo->setCurrentIndex(1);
 	OutCombo->setEnabled(false);
 	EnablePr(1);
+	if (a == 3) // X1, no profile embedding
+	{
+		EmbedProfs->setChecked(false);
+		EmbedProfs->setEnabled(false);
+		EmbedProfs2->setChecked(false);
+		EmbedProfs2->setEnabled(false);
+	}
 	if ((a == 4) || (a == 5)) // X3 or X4, enforcing color profiles on images
 	{
+		EmbedProfs->setEnabled(true);
 		EmbedProfs2->setChecked(true);
 		EmbedProfs2->setEnabled(false);
 	}
