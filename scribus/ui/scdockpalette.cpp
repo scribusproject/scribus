a95562c7ddada314665f65c88a8a59a4b180567b
Jean Ghali <jghali@libertysurf.fr>
2012-09-28 22:09:16 +0000

    fix palette issue in OS X
    git-svn-id: svn://scribus.net/trunk/Scribus@17785 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/scdockpalette.cpp
+++ scribus/ui/scdockpalette.cpp
@@ -41,7 +41,11 @@ for which a new license (GPL+exception) is in place.
 
 
 ScDockPalette::ScDockPalette( QWidget * parent, const QString& prefsContext, Qt::WFlags f)
+#ifdef Q_OS_MAC
+	: QDockWidget ( parent, f | Qt::Tool  | Qt::CustomizeWindowHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint ),
+#else
 	: QDockWidget ( parent, f | Qt::Tool  | Qt::CustomizeWindowHint ),
+#endif
 	palettePrefs(0),
 	prefsContextName(QString::null),
 	visibleOnStartup(false)
