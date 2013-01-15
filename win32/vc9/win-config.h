7a2b4ea6e193cd4b4a11fc7b8c7876834e5834a9
Jean Ghali <jghali@libertysurf.fr>
2013-01-15 08:18:37 +0000

    msvc projetc update : small fix + update build number to 1.4.3svn
    git-svn-id: svn://scribus.net/branches/Version14x/Scribus@18093 11d20701-8431-0410-a711-e3c959e3b870

--- win32/vc9/win-config.h
+++ win32/vc9/win-config.h
@@ -1,6 +1,9 @@
 #ifndef WINCONFIG_H
 #define WINCONFIG_H
 
+/* Version number of package */
+#define VERSION "1.4.3svn"
+
 /* Are we building win32 native version. */
 #define BUILD_WINDOWS_VERSION 1
 
@@ -11,7 +14,7 @@
 #undef BUILD_MAC_BUNDLE
 
 /* ColorManagementInclude */
-#define CMS_INC "lcms.h"
+#define CMS_INC "lcms2.h"
 
 /* ColorManagement (LittleCms) */
 #define HAVE_CMS 1
@@ -25,15 +28,6 @@
 /* Can we use unicode functions (Windows) */
 #define HAVE_UNICODE 1
 
-/* Define if you have the CoreAudio API */
-#undef HAVE_COREAUDIO
-
-/* Define to 1 if you have the <crt_externs.h> header file. */
-#undef HAVE_CRT_EXTERNS_H
-
-/* Defines if your system has the crypt function */
-#undef HAVE_CRYPT
-
 /* Cups */
 #undef HAVE_CUPS
 
@@ -46,9 +40,6 @@
 /* Defines if your system has the libfreetype library */
 #define HAVE_FREETYPE 1
 
-/* Define to 1 if you have the <inttypes.h> header file. */
-#undef HAVE_INTTYPES_H
-
 /* Defines if your system has the cairo library */
 /*#define HAVE_CAIRO 1*/
 
@@ -57,9 +48,6 @@
 
 /* Define if you have libpng */
 #define HAVE_LIBPNG 1
-
-/* Define if you have a working libpthread (will enable threaded code) */
-#undef HAVE_LIBPTHREAD
 
 /* Define if you have libz */
 #define HAVE_LIBZ 1
@@ -95,24 +83,6 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 // #define HAVE_STDLIB_H 1
 
-/* Define to 1 if you have the <strings.h> header file. */
-#undef HAVE_STRINGS_H
-
-/* Define to 1 if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define if you have strlcat */
-#undef HAVE_STRLCAT
-
-/* Define if you have the strlcat prototype */
-#undef HAVE_STRLCAT_PROTO
-
-/* Define if you have strlcpy */
-#undef HAVE_STRLCPY
-
-/* Define if you have the strlcpy prototype */
-#undef HAVE_STRLCPY_PROTO
-
 /* Define to 1 if you have the <sys/bitypes.h> header file. */
 #undef HAVE_SYS_BITYPES_H
 
@@ -137,50 +107,8 @@
 /* Defines if your system has the libxml2 library6 */
 #define HAVE_XML26 1
 
-/* Suffix for lib directories */
-#undef KDELIBSUFF
-
-/* Name of package */
-#undef PACKAGE
-
-/* Define to the address where bug reports for this package should be sent. */
-#undef PACKAGE_BUGREPORT
-
-/* Define to the full name of this package. */
-#undef PACKAGE_NAME
-
-/* Define to the full name and version of this package. */
-#undef PACKAGE_STRING
-
-/* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
-
-/* Define to the version of this package. */
-#undef PACKAGE_VERSION
-
-/* The size of a `char *', as computed by sizeof. */
-#define SIZEOF_CHAR_P 4
-
-/* The size of a `int', as computed by sizeof. */
-#define SIZEOF_INT 4
-
-/* The size of a `long', as computed by sizeof. */
-#define SIZEOF_LONG 4
-
-/* The size of a `short', as computed by sizeof. */
-#define SIZEOF_SHORT 2 
-
-/* The size of a `size_t', as computed by sizeof. */
-#define SIZEOF_SIZE_T 4
-
-/* The size of a `unsigned long', as computed by sizeof. */
-#define SIZEOF_UNSIGNED_LONG 4
-
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
-
-/* Version number of package */
-#define VERSION "1.4.2svn"
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
