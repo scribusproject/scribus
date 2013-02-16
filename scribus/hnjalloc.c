395138a8738adfd684bffdd4d148fc641f105f76
Jean Ghali <jghali@libertysurf.fr>
2013-02-16 00:07:06 +0000

    #11382: Problem in hyphenation
    git-svn-id: svn://scribus.net/branches/Version14x/Scribus@18145 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/hnjalloc.c
+++ scribus/hnjalloc.c
@@ -3,7 +3,7 @@
  */
 
 /* LibHnj - a library for high quality hyphenation and justification
- * Copyright (C) 1998 Raph Levien
+ * Copyright (C) 1998 Raph Levien, (C) 2001 ALTLinux, Moscow
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
