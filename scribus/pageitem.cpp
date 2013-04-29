21915070fa3e1a781a384107fececefb9adb2362
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2013-04-29 20:16:04 +0000

    Don't call checkChanges() when loading a doc.
    git-svn-id: svn://scribus.net/trunk/Scribus@18269 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem.cpp
+++ scribus/pageitem.cpp
@@ -4609,6 +4609,8 @@ void PageItem::setLayer(int newLayerID)
 
 void PageItem::checkChanges(bool force)
 {
+	if (m_Doc->view() == NULL)
+		return;
 	bool spreadChanges(false);
 	// has the item been resized
 	if (force || ((oldWidth != m_width || oldHeight != m_height) && shouldCheck()))
