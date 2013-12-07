9cb3d8606920521043c33d1cbfa156e470456b8e
Peter Linnell <mrdocs@scribus.info>
2013-12-07 07:31:44 +0000

    bump the date
    git-svn-id: svn://scribus.net/trunk/Scribus@18626 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -112,8 +112,8 @@ About::About( QWidget* parent, AboutMode diaMode ) : QDialog( parent )
 	buildID = new QLabel( tab );
 	buildID->setAlignment(Qt::AlignCenter);
 	buildID->setTextInteractionFlags(Qt::TextSelectableByMouse);
-	QString BUILD_DAY = "19";
-	QString BUILD_MONTH = CommonStrings::october;
+	QString BUILD_DAY = "08";
+	QString BUILD_MONTH = CommonStrings::december;
 	QString BUILD_YEAR = "2013";
 	QString BUILD_TIME = "";
 	QString BUILD_TZ = "";
