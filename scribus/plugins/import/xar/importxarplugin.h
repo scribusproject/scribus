ed8c0db25fe9e1ea05e75315f367985bf0d9095f
Craig Bradney <mrb@scribus.info>
2014-03-17 21:44:16 +0000

    Fix XAR plugin header guard
    git-svn-id: svn://scribus.net/trunk/Scribus@18917 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/import/xar/importxarplugin.h
+++ scribus/plugins/import/xar/importxarplugin.h
@@ -4,7 +4,7 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#ifndef IMPORTXARLUGIN_H
+#ifndef IMPORTXARPLUGIN_H
 #define IMPORTXARPLUGIN_H
 
 #include "pluginapi.h"
