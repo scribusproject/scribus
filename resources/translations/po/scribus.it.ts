25bbbee7189a21bd8dcb18e855ac5e20befe52ef
Craig Bradney <mrb@scribus.info>
2014-03-30 15:11:09 +0000

    #12174: Updated IT translation file
    git-svn-id: svn://scribus.net/branches/Version14x/Scribus@18964 11d20701-8431-0410-a711-e3c959e3b870

--- resources/translations/po/scribus.it.ts
+++ resources/translations/po/scribus.it.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="it_IT" sourcelanguage="en">
+<TS version="2.1" language="it_IT" sourcelanguage="en">
 <context>
     <name></name>
     <message>
@@ -28983,7 +28983,7 @@ Utilizzare la Verifica Preliminare per correggerli</translation>
         <location filename="../../../scribus/scribus.cpp" line="4542"/>
         <source>Your document was saved to a temporary file and could not be moved: 
 %1</source>
-        <translation>Il documento è stato salvato in un file temporaneo è non è stato possibile spostarlo:
+        <translation>Il documento è stato salvato in un file temporaneo e non è stato possibile spostarlo:
 %1</translation>
     </message>
     <message>
