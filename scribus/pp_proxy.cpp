859ac75e10c42f2a93cef642003dc31fdc33a812
Craig Bradney <mrb@scribus.info>
2014-04-22 21:44:39 +0000

    Add beginnings of future Properties Palettes proxy
    git-svn-id: svn://scribus.net/trunk/Scribus@19079 11d20701-8431-0410-a711-e3c959e3b870

---
+++ scribus/pp_proxy.cpp
@@ -0,0 +1,30 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+
+#include "pp_proxy.h"
+#include "scribus.h"
+#include "pageitem.h"
+
+PP_Proxy::PP_Proxy(QObject *parent)
+	: QObject(parent),
+	  m_pageItem(NULL),
+	  m_scribusDoc(NULL)
+{
+}
+
+void PP_Proxy::setPageItem(PageItem *item)
+{
+	m_pageItem=item;
+	if (m_scribusDoc!=m_pageItem->doc())
+		m_scribusDoc=m_pageItem->doc();
+}
+
+void PP_Proxy::setDoc(ScribusDoc *doc)
+{
+	m_scribusDoc=doc;
+}
+
---
+++ scribus/pp_proxy.h
@@ -0,0 +1,36 @@
+/*
+For general Scribus (>=1.3.2) copyright and licensing information please refer
+to the COPYING file provided with the program. Following this notice may exist
+a copyright and/or license notice that predates the release of Scribus 1.3.2
+for which a new license (GPL+exception) is in place.
+*/
+
+
+#ifndef PP_PROXY_H
+#define PP_PROXY_H
+
+#include <QObject>
+
+class PageItem;
+class ScribusDoc;
+
+
+class PP_Proxy : public QObject
+{
+		Q_OBJECT
+	public:
+		explicit PP_Proxy(QObject *parent = 0);
+
+	signals:
+
+	public slots:
+		void setPageItem(PageItem* item);
+		void setDoc(ScribusDoc* doc);
+
+	protected:
+		PageItem *m_pageItem;
+		ScribusDoc *m_scribusDoc;
+
+};
+
+#endif // PP_PROXY_H
