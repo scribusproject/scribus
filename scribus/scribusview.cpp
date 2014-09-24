251f74c054b5cefe910737732605dae4a3f572e3
Craig Bradney <mrb@scribus.info>
2014-09-24 21:29:57 +0000

    #12453: Not sure when this last was what we wanted to happen, comment out to fix this bug
    git-svn-id: svn://scribus.net/trunk/Scribus@19535 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusview.cpp
+++ scribus/scribusview.cpp
@@ -4161,11 +4161,13 @@ bool ScribusView::eventFilter(QObject *obj, QEvent *event)
 	else if (event->type() == QEvent::KeyPress)
 	{
 		QKeyEvent* m = static_cast<QKeyEvent*> (event);
-		if(m->key() == Qt::Key_Shift)
+		/* #12453... what do we use this for?
+		 if(m->key() == Qt::Key_Shift)
 		{
 			m_oldSnapToElem = Doc->SnapElement;
 			m_ScMW->SetSnapElements(false);
 		}
+		*/
 		if (m_canvasMode->handleKeyEvents())
 			m_canvasMode->keyPressEvent(m);
 		else
@@ -4175,8 +4177,10 @@ bool ScribusView::eventFilter(QObject *obj, QEvent *event)
 	else if (event->type() == QEvent::KeyRelease)
 	{
 		QKeyEvent* m = static_cast<QKeyEvent*> (event);
+		/* #12453... what do we use this for?
 		if(m->key() == Qt::Key_Shift)
 			m_ScMW->SetSnapElements(m_oldSnapToElem);
+		*/
 		if (m_canvasMode->handleKeyEvents())
 			m_canvasMode->keyReleaseEvent(m);
 		else
