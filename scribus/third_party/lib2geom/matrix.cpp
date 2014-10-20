564341e96f2522eaf5618e8e52f7a992db7d5172
Craig Bradney <mrb@scribus.info>
2014-10-20 20:31:17 +0000

    #12780: apply patch to matrix.cpp from lib2geom
    git-svn-id: svn://scribus.net/trunk/Scribus@19601 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/third_party/lib2geom/matrix.cpp
+++ scribus/third_party/lib2geom/matrix.cpp
@@ -128,7 +128,7 @@ bool Matrix::isTranslation(Coord const eps) const {
  \return A bool representing yes/no.
  */
 bool Matrix::isScale(Coord const eps) const {
-    return !are_near(_c[0], 1.0) || !are_near(_c[3], 1.0) &&  //NOTE: these are the diags, and the next line opposite diags
+    return (!are_near(_c[0], 1.0) || !are_near(_c[3], 1.0)) &&  //NOTE: these are the diags, and the next line opposite diags
            are_near(_c[1], 0.0) && are_near(_c[2], 0.0) && 
            are_near(_c[4], 0.0) && are_near(_c[5], 0.0);
 }
