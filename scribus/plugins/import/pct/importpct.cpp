2075d25b7722a643bf1ce82c339a30ba2247206f
Jean Ghali <jghali@libertysurf.fr>
2014-11-17 07:59:44 +0000

    #12803: wrong condition in Mac Pict importer plugin
    git-svn-id: svn://scribus.net/trunk/Scribus@19628 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/import/pct/importpct.cpp
+++ scribus/plugins/import/pct/importpct.cpp
@@ -1318,7 +1318,7 @@ void PctPlug::handleShape(QDataStream &ts, quint16 opCode)
 	if (opCode > 0x0044)
 		currRectType = 1;
 	finishItem(ite);
-	if ((patternMode) && ((opCode != 0x0030) || (opCode != 0x0040) || (opCode != 0x0050)))
+	if ((patternMode) && (opCode && 0x0030) && (opCode != 0x0040) && (opCode != 0x0050))
 		setFillPattern(ite);
 }
 
@@ -1403,7 +1403,7 @@ void PctPlug::handleSameShape(QDataStream &ts, quint16 opCode)
 		ite->PoLine.translate(offsetX, offsetY);
 		finishItem(ite);
 	}
-	if ((patternMode) && ((opCode != 0x0038) || (opCode != 0x0048) || (opCode != 0x0058)))
+	if ((patternMode) && (opCode != 0x0038) && (opCode != 0x0048) && (opCode != 0x0058))
 		setFillPattern(ite);
 }
 
