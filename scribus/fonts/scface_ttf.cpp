94af81d426d322f47b1c294cc437b705b8901410
Craig Bradney <mrb@scribus.info>
2015-05-13 23:19:03 +0000

    Hide font posttable qdebugs
    git-svn-id: svn://scribus.net/trunk/Scribus@20079 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/fonts/scface_ttf.cpp
+++ scribus/fonts/scface_ttf.cpp
@@ -526,10 +526,10 @@ void ScFace_ttf::load() const
 	sfnt::PostTable checkPost;
 	FT_Face face = ftFace();
 	checkPost.readFrom(face);
-	if (!checkPost.usable)
-		qDebug() << "can't use post table from " << face->family_name << face->style_name << ":" << checkPost.errorMsg;
-	else
-		qDebug() << "posttable from" << face->family_name << face->style_name << "has names for" << checkPost.numberOfGlyphs() << "glyphs and" << maxGlyph << "glyphs in charmap";
+//	if (!checkPost.usable)
+//		qDebug() << "can't use post table from " << face->family_name << face->style_name << ":" << checkPost.errorMsg;
+//	else
+//		qDebug() << "posttable from" << face->family_name << face->style_name << "has names for" << checkPost.numberOfGlyphs() << "glyphs and" << maxGlyph << "glyphs in charmap";
 	const_cast<bool&>(hasGlyphNames) = checkPost.usable && checkPost.numberOfGlyphs() >= maxGlyph;
 }
 
