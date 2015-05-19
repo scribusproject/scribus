5422591348f7b22b32683920c2ff0bc230652acf
Craig Bradney <mrb@scribus.info>
2015-05-19 22:03:55 +0000

    Open the 1.5.1 floodgates
    git-svn-id: svn://scribus.net/trunk/Scribus@20100 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.5.0"
+#cmakedefine VERSION "1.5.1.svn"
 
 #cmakedefine BUILD_WINDOWS_VERSION 1
 #cmakedefine DLL_USE_NATIVE_API 1
