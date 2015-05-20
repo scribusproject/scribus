3bd05b29b0d2d0eb7b6861a36b0376739ce18d94
Jean Ghali <jghali@libertysurf.fr>
2015-05-20 00:36:20 +0000

    remove msvc 2010 solution, msvc 2012 solution can be used to build with msvc 2010 compiler anyway
    git-svn-id: svn://scribus.net/trunk/Scribus@20105 11d20701-8431-0410-a711-e3c959e3b870

--- win32/vc10/scribus-main/scribuspch.cpp
+++
@@ -1,9 +0,0 @@
-/*
-For general Scribus (>=1.3.2) copyright and licensing information please refer
-to the COPYING file provided with the program. Following this notice may exist
-a copyright and/or license notice that predates the release of Scribus 1.3.2
-for which a new license (GPL+exception) is in place.
-*/
-
-#include "scribuspch.h"
-
