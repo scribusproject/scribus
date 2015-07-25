065a430a04452a5dad3a6b267d029bb7e22d2822
Jean Ghali <jghali@libertysurf.fr>
2015-07-25 20:51:29 +0000

    add <algorithm> header just to be sure std::swap is declared correctly
    git-svn-id: svn://scribus.net/trunk/Scribus@20277 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/appmodehelper.cpp
+++ scribus/appmodehelper.cpp
@@ -14,6 +14,8 @@ a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
 
+#include <algorithm>
+
 #include "appmodehelper.h"
 #include "appmodes.h"
 #include "canvasmode.h"
