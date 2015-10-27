4da4b30dd843e2a81abaec40ada4ee4618c76620
Jean Ghali <jghali@libertysurf.fr>
2015-10-27 12:32:39 +0000

    #10391: add keyboard shortcut Ctrl+F to Search/Replace in Story Editor <IbtisamN>
    git-svn-id: svn://scribus.net/branches/Version14x/Scribus@20500 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/storyeditor.cpp
+++ scribus/storyeditor.cpp
@@ -1675,7 +1675,7 @@ void StoryEditor::initActions()
 	seActions.insert("editCopy", new ScrAction(loadIcon("16/edit-copy.png"), QPixmap(), "", Qt::CTRL+Qt::Key_C, this));
 	seActions.insert("editPaste", new ScrAction(loadIcon("16/edit-paste.png"), QPixmap(), "", Qt::CTRL+Qt::Key_V, this));
 	seActions.insert("editClear", new ScrAction(loadIcon("16/edit-delete.png"), QPixmap(), "", Qt::Key_Delete, this));
-	seActions.insert("editSearchReplace", new ScrAction(loadIcon("16/edit-find-replace.png"), QPixmap(), "", QKeySequence(), this));
+	seActions.insert("editSearchReplace", new ScrAction(loadIcon("16/edit-find-replace.png"), QPixmap(), "", Qt::CTRL+Qt::Key_F, this));
 	//seActions.insert("editEditStyle", new ScrAction("", QKeySequence(), this));
 	seActions.insert("editFontPreview", new ScrAction("", QKeySequence(), this));
 	seActions.insert("editUpdateFrame", new ScrAction(loadIcon("compfile16.png"),loadIcon("compfile.png"), "", Qt::CTRL+Qt::Key_U, this));
