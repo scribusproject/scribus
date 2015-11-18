4a7a0bc27bb51d8b2094d739a07d7239382fa1a3
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2015-11-18 19:29:47 +0000

    Fixed Bug #12245: grouped lines appear thicker
    git-svn-id: svn://scribus.net/trunk/Scribus@20560 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scpainter.cpp
+++ scribus/scpainter.cpp
@@ -228,7 +228,7 @@ void ScPainter::rotate( double r )
 void ScPainter::scale( double x, double y )
 {
 	cairo_scale (m_cr, x, y);
-	m_zoomFactor = qMax(x, y);
+	m_zoomFactor *= qMax(x, y);
 }
 
 void ScPainter::moveTo( const double &x, const double &y )
