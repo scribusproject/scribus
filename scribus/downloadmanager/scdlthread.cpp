b967dd1ece28b0eb5595b425934ecdc68b581857
Ali Yousuf <aliyousuf2012@hotmail.com>
2015-12-07 09:47:43 +0400

    prefix private member of scdlthread class

--- scribus/downloadmanager/scdlthread.cpp
+++ scribus/downloadmanager/scdlthread.cpp
@@ -12,7 +12,7 @@
 
 
 ScDLThread::ScDLThread(QObject *parent) : QThread(parent),
-	downloadedCount(0), totalCount(0)
+	m_downloadedCount(0), m_totalCount(0)
 {
 	connect(this, SIGNAL(runSignal()), this, SLOT(runSlot()));
 }
@@ -34,8 +34,8 @@ void ScDLThread::addURL(const QUrl &url, bool overwrite, const QString& location
 	QString l(QDir::cleanPath(location));
 	if (!l.endsWith("/"))
 		l += "/";
-	downloadQueue.enqueue(qMakePair(url, l));
-	++totalCount;
+	m_downloadQueue.enqueue(qMakePair(url, l));
+	++m_totalCount;
 }
 
 void ScDLThread::addURLs(const QStringList &urlList, bool overwrite, const QString& location, const QString& destinationLocation)
@@ -50,14 +50,14 @@ void ScDLThread::addURLs(const QStringList &urlList, bool overwrite, const QStri
 		QUrl url(u);
 		if (!urlOK(u))
 			return;
-		downloadQueue.enqueue(qMakePair(url, l));
-		++totalCount;
+		m_downloadQueue.enqueue(qMakePair(url, l));
+		++m_totalCount;
 	}
 }
 
 void ScDLThread::startDownloads()
 {
-	if (downloadQueue.isEmpty())
+	if (m_downloadQueue.isEmpty())
 	{
 		//qDebug()<<"No more downloads left";
 		emit finished();
@@ -95,14 +95,14 @@ QString ScDLThread::saveFileName(const QUrl &url, const QString& location, bool 
 
 void ScDLThread::startNextDownload()
 {
-	if (downloadQueue.isEmpty())
+	if (m_downloadQueue.isEmpty())
 	{
 		//qDebug()<<downloadedCount<<"/"<<totalCount<<"files downloaded successfully";
-		downloadedCount=totalCount=0;
+		m_downloadedCount=m_totalCount=0;
 		emit finished();
 		return;
 	}
-	QPair<QUrl, QString> urlPair=downloadQueue.dequeue();
+	QPair<QUrl, QString> urlPair=m_downloadQueue.dequeue();
 
 	QString filename = saveFileName(urlPair.first, urlPair.second, true);
 	if (filename.isEmpty())
@@ -110,8 +110,8 @@ void ScDLThread::startNextDownload()
 		qDebug()<<"File name empty for url:"<<urlPair.first.toEncoded().constData();
 		return;
 	}
-	output.setFileName(filename);
-	if (!output.open(QIODevice::WriteOnly))
+	m_output.setFileName(filename);
+	if (!m_output.open(QIODevice::WriteOnly))
 	{
 		//qDebug()<<"Problem opening save file '"<<qPrintable(filename)<<"' for download '"
 		//	   <<urlPair.first.toEncoded().constData()<<"': "<<qPrintable(output.errorString());
@@ -119,22 +119,22 @@ void ScDLThread::startNextDownload()
 		startNextDownload();
 		return;
 	}
-	emit fileStarted(output.fileName());
+	emit fileStarted(m_output.fileName());
 	QNetworkRequest request(urlPair.first);
 	//QNetworkReply *nwr=manager.head(request);
 	//qDebug()<<nwr->url()<<nwr->rawHeaderList();
 	//connect(&manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
-	currentDownload = manager.get(request);
-	connect(currentDownload, SIGNAL(finished()), this, SLOT(downloadFinished()));
-	connect(currentDownload, SIGNAL(readyRead()), this, SLOT(downloadReadyRead()));
+	m_currentDownload = m_manager.get(request);
+	connect(m_currentDownload, SIGNAL(finished()), this, SLOT(downloadFinished()));
+	connect(m_currentDownload, SIGNAL(readyRead()), this, SLOT(downloadReadyRead()));
 
 	//qDebug()<<"Downloading:"<<urlPair.first.toEncoded().constData();
 }
 
 void ScDLThread::downloadFinished()
 {
-	output.close();
-	if (currentDownload->error())
+	m_output.close();
+	if (m_currentDownload->error())
 	{
 /*
 		if(currentDownload->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt() == 301 || currentDownload->rawHeaderList().contains("Location"))
@@ -147,18 +147,18 @@ void ScDLThread::downloadFinished()
 		QUrl redirectUrl = currentDownload->attribute(QNetworkRequest::RedirectionTargetAttribute).toUrl();
 		qDebug()<<redirectUrl;
 */
-		qDebug()<<"Failed: "<<qPrintable(currentDownload->errorString());
-		emit fileFailed(output.fileName());
-		if (output.exists())
-			output.remove();
+		qDebug()<<"Failed: "<<qPrintable(m_currentDownload->errorString());
+		emit fileFailed(m_output.fileName());
+		if (m_output.exists())
+			m_output.remove();
 	}
 	else
 	{
 		//qDebug()<<"Saving file:"<<qPrintable(output.fileName());
-		++downloadedCount;
-		emit fileReceived(output.fileName());
+		++m_downloadedCount;
+		emit fileReceived(m_output.fileName());
 	}
-	currentDownload->deleteLater();
+	m_currentDownload->deleteLater();
 	startNextDownload();
 }
 
@@ -194,7 +194,7 @@ void ScDLThread::replyFinished(QNetworkReply* reply)
 
 void ScDLThread::downloadReadyRead()
 {
-	output.write(currentDownload->readAll());
+	m_output.write(m_currentDownload->readAll());
 }
 
 void ScDLThread::runSlot()
