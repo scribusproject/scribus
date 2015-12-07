1fc30e2c0de6ea0f0338fa62785922455634a98f
Ali Yousuf <aliyousuf2012@hotmail.com>
2015-12-07 10:15:55 +0400

    prefix private member of contentreader class

--- scribus/plugins/gettext/sxwim/contentreader.cpp
+++ scribus/plugins/gettext/sxwim/contentreader.cpp
@@ -32,119 +32,119 @@ for which a new license (GPL+exception) is in place.
 #include "scribusstructs.h"
 #include <QByteArray>
 
-ContentReader* ContentReader::creader = NULL;
+ContentReader* ContentReader::m_creader = NULL;
 
 extern xmlSAXHandlerPtr cSAXHandler;
 
 ContentReader::ContentReader(QString documentName, StyleReader *s, gtWriter *w, bool textOnly)
 {
-	creader = this;
-	docname = documentName;
-	sreader = s;
-	writer  = w;
-	importTextOnly = textOnly;
-	defaultStyle = NULL;
-	currentStyle = NULL;
-	inList = false;
-	inNote = false;
-	inNoteBody = false;
-	inSpan = false;
-	append    = 0;
-	listIndex = 0;
-	listLevel = 0;
-	currentList = "";
-	inT = false;
-	tName = "";
+	m_creader = this;
+	m_docname = documentName;
+	m_sreader = s;
+	m_writer  = w;
+	m_importTextOnly = textOnly;
+	m_defaultStyle = NULL;
+	m_currentStyle = NULL;
+	m_inList = false;
+	m_inNote = false;
+	m_inNoteBody = false;
+	m_inSpan = false;
+	m_append    = 0;
+	m_listIndex = 0;
+	m_listLevel = 0;
+	m_currentList = "";
+	m_inT = false;
+	m_tName = "";
 }
 
 bool ContentReader::startElement(const QString&, const QString&, const QString &name, const QXmlAttributes &attrs) 
 {
 	if ((name == "text:p") || (name == "text:h"))
 	{
-		++append;
+		++m_append;
 		QString name = "";
 		for (int i = 0; i < attrs.count(); ++i)
 		{
 			if (attrs.localName(i) == "text:style-name")
 			{
 				name = attrs.value(i);
-				styleNames.push_back(attrs.value(i));
+				m_styleNames.push_back(attrs.value(i));
 			}
 		}
-		if (!inList)
+		if (!m_inList)
 		{
-			pstyle = sreader->getStyle(name);
-			currentStyle = pstyle;
+			m_pstyle = m_sreader->getStyle(name);
+			m_currentStyle = m_pstyle;
 		}
 		else
 		{
-			gtStyle *tmp = sreader->getStyle(getName());
+			gtStyle *tmp = m_sreader->getStyle(getName());
 			if ((tmp->getName()).indexOf("default-style") != -1)
 				getStyle();
 			else
-				currentStyle = tmp;
+				m_currentStyle = tmp;
 		}
 	}
 	else if (name == "text:span")
 	{
-		inSpan = true;
+		m_inSpan = true;
 		QString styleName = "";
 		for (int i = 0; i < attrs.count(); ++i)
 		{
 			if (attrs.localName(i) == "text:style-name")
 			{
-				currentStyle = sreader->getStyle(attrs.value(i));
+				m_currentStyle = m_sreader->getStyle(attrs.value(i));
 				styleName = attrs.value(i);
-				styleNames.push_back(styleName);
+				m_styleNames.push_back(styleName);
 			}
 		}
-		gtStyle *tmp = sreader->getStyle(getName());
+		gtStyle *tmp = m_sreader->getStyle(getName());
 		if ((tmp->getName()).indexOf("default-style") != -1)
 			getStyle();
 		else
-			currentStyle = tmp;
+			m_currentStyle = tmp;
 	}
 	else if ((name == "text:unordered-list") || (name == "text:ordered-list"))
 	{
-		inList = true;
-		++listLevel;
-		if (static_cast<int>(listIndex2.size()) < listLevel)
-			listIndex2.push_back(0);
+		m_inList = true;
+		++m_listLevel;
+		if (static_cast<int>(m_listIndex2.size()) < m_listLevel)
+			m_listIndex2.push_back(0);
 		for (int i = 0; i < attrs.count(); ++i)
 		{
 			if (attrs.localName(i) == "text:style-name")
-				currentList = attrs.value(i);
+				m_currentList = attrs.value(i);
 		}
-		currentStyle = sreader->getStyle(QString(currentList + "_%1").arg(listLevel));
-		styleNames.clear();
-		styleNames.push_back(QString(currentList + "_%1").arg(listLevel));
+		m_currentStyle = m_sreader->getStyle(QString(m_currentList + "_%1").arg(m_listLevel));
+		m_styleNames.clear();
+		m_styleNames.push_back(QString(m_currentList + "_%1").arg(m_listLevel));
 		if (name == "text:ordered-list")
-			isOrdered2.push_back(true);
+			m_isOrdered2.push_back(true);
 		else
-			isOrdered2.push_back(false);
+			m_isOrdered2.push_back(false);
 	}
 	else if (name == "text:list-item")
 	{
 		bool isOrdered = false;
-		int levelIndex = listLevel - 1;
-		if (levelIndex >= 0 && levelIndex < static_cast<int>(isOrdered2.size()))
-			isOrdered = isOrdered2[listLevel - 1];
+		int levelIndex = m_listLevel - 1;
+		if (levelIndex >= 0 && levelIndex < static_cast<int>(m_isOrdered2.size()))
+			isOrdered = m_isOrdered2[m_listLevel - 1];
 		if (isOrdered)
 		{
-			++listIndex;
-			++listIndex2[listLevel - 1];
-			if (listLevel == 1)
-				write(QString("%1. ").arg(listIndex2[listLevel - 1]));
+			++m_listIndex;
+			++m_listIndex2[m_listLevel - 1];
+			if (m_listLevel == 1)
+				write(QString("%1. ").arg(m_listIndex2[m_listLevel - 1]));
 			else
-				write(QString("%1. ").arg(listIndex2[listLevel - 1]));
+				write(QString("%1. ").arg(m_listIndex2[m_listLevel - 1]));
 		}
 		else
 			write("- ");
 	}
 	else if (name == "text:note")
-		inNote = true;
+		m_inNote = true;
 	else if (name == "text:note-body")
-		inNoteBody = true;
+		m_inNoteBody = true;
 	else if (name == "style:style")
 	{
 		QString sname = "";
@@ -158,11 +158,11 @@ bool ContentReader::startElement(const QString&, const QString&, const QString &
 		}
 		if (isTextStyle)
 		{
-			tName = sname;
-			inT = true;
+			m_tName = sname;
+			m_inT = true;
 		}
 	}
-	else if ((name == "style:properties") && (inT))
+	else if ((name == "style:properties") && (m_inT))
 	{
 		Properties p;
 		for (int i = 0; i < attrs.count(); ++i)
@@ -170,7 +170,7 @@ bool ContentReader::startElement(const QString&, const QString&, const QString &
 			std::pair<QString, QString> pair(attrs.localName(i), attrs.value(i));
 			p.push_back(pair);
 		}
-		tmap[tName] = p;
+		m_tmap[m_tName] = p;
 	}
 	else if (name == "text:s")
 	{
@@ -198,7 +198,7 @@ bool ContentReader::characters(const QString &ch)
 	tmp = tmp.remove(""); // Remove all OO.o hyphenation chars
 	// Unneeded as scribus now also use standard unicode non-breakable space
 	// tmp = tmp.replace(QChar(160), SpecialChars::NBSPACE); // replace OO.o nbsp with Scribus nbsp
-	if (append > 0)
+	if (m_append > 0)
 		write(tmp);
 	return true;
 }
@@ -208,71 +208,71 @@ bool ContentReader::endElement(const QString&, const QString&, const QString &na
 	if ((name == "text:p") || (name == "text:h"))
 	{
 		write("\n");
-		--append;
-		if (inList || inNote || inNoteBody)
+		--m_append;
+		if (m_inList || m_inNote || m_inNoteBody)
 		{
-			if (styleNames.size() != 0)
-				styleNames.pop_back();
+			if (m_styleNames.size() != 0)
+				m_styleNames.pop_back();
 		}
 		else
-			styleNames.clear();
+			m_styleNames.clear();
 	}
 	else if (name == "text:span")
 	{
-		inSpan = false;
-		currentStyle = pstyle;
-		if (styleNames.size() != 0)
-			styleNames.pop_back();	
-		currentStyle = sreader->getStyle(getName());
+		m_inSpan = false;
+		m_currentStyle = m_pstyle;
+		if (m_styleNames.size() != 0)
+			m_styleNames.pop_back();	
+		m_currentStyle = m_sreader->getStyle(getName());
 	}
 	else if (name == "text:note")
-		inNote = false;
+		m_inNote = false;
 	else if (name == "text:note-body")
-		inNoteBody = false;
+		m_inNoteBody = false;
 	else if (name == "text:line-break")
 		write(SpecialChars::LINEBREAK);
 	else if (name == "text:tab-stop")
 		write("\t");
 	else if ((name == "text:unordered-list") || (name == "text:ordered-list"))
 	{
-		--listLevel;
-		styleNames.clear();
-		if (listLevel == 0)
+		--m_listLevel;
+		m_styleNames.clear();
+		if (m_listLevel == 0)
 		{
-			inList = false;
-			listIndex2.clear();
+			m_inList = false;
+			m_listIndex2.clear();
 		}
 		else
 		{
-			currentStyle = sreader->getStyle(QString(currentList + "_%1").arg(listLevel));
-			styleNames.push_back(QString(currentList + "_%1").arg(listLevel));
+			m_currentStyle = m_sreader->getStyle(QString(m_currentList + "_%1").arg(m_listLevel));
+			m_styleNames.push_back(QString(m_currentList + "_%1").arg(m_listLevel));
 		}
 	}
-	else if ((name == "style:style") && (inT))
+	else if ((name == "style:style") && (m_inT))
 	{
-		inT = false;
-		tName = "";
+		m_inT = false;
+		m_tName = "";
 	}
 	return true;
 }
 
 void ContentReader::write(const QString& text)
 {
-	if (!inNote && !inNoteBody) // Disable notes import for now
+	if (!m_inNote && !m_inNoteBody) // Disable notes import for now
 	{
-		if (importTextOnly)
-			writer->appendUnstyled(text);
-		else if (inSpan)
-			writer->append(text, currentStyle, false);
+		if (m_importTextOnly)
+			m_writer->appendUnstyled(text);
+		else if (m_inSpan)
+			m_writer->append(text, m_currentStyle, false);
 		else
-			writer->append(text, currentStyle);
+			m_writer->append(text, m_currentStyle);
 	}
-	lastStyle = currentStyle;
+	m_lastStyle = m_currentStyle;
 }
 
 void ContentReader::parse(QString fileName)
 {
-	sreader->parse(fileName);
+	m_sreader->parse(fileName);
 #if defined(_WIN32)
 	QString fname = QDir::toNativeSeparators(fileName);
 	QByteArray fn = (QSysInfo::WindowsVersion & QSysInfo::WV_NT_based) ? fname.toUtf8() : fname.toLocal8Bit();
@@ -331,56 +331,56 @@ void ContentReader::startElement(void*, const xmlChar *fullname, const xmlChar *
 		for(const xmlChar** cur = atts; cur && *cur; cur += 2)
 			attrs.append(QString((char*)*cur), NULL, QString((char*)*cur), QString((char*)*(cur + 1)));
 	}
-	creader->startElement(NULL, NULL, name, attrs);
+	m_creader->startElement(NULL, NULL, name, attrs);
 }
 
 void ContentReader::characters(void*, const xmlChar *ch, int len)
 {
 	QString chars = QString::fromUtf8((const char*) ch, len);
-	creader->characters(chars);
+	m_creader->characters(chars);
 }
 
 void ContentReader::endElement(void*, const xmlChar *name)
 {
 	QString nname(QString((const char*) name).toLower());
-	creader->endElement(NULL, NULL, nname);
+	m_creader->endElement(NULL, NULL, nname);
 }
 
 QString ContentReader::getName()
 {
 	QString s = "";
-	for (uint i = 0; i < styleNames.size(); ++i)
-		s += styleNames[i];
+	for (uint i = 0; i < m_styleNames.size(); ++i)
+		s += m_styleNames[i];
 	return s;
 }
 
 void ContentReader::getStyle()
 {
 	gtStyle *style = NULL, *tmp = NULL;
-	if (styleNames.size() == 0)
-		style = sreader->getStyle("default-style");
+	if (m_styleNames.size() == 0)
+		style = m_sreader->getStyle("default-style");
 	else
-		style = sreader->getStyle(styleNames[0]);
+		style = m_sreader->getStyle(m_styleNames[0]);
 	assert (style != NULL);
 	gtParagraphStyle* par = dynamic_cast<gtParagraphStyle*>(style);
 	if (par)
 		tmp = new gtParagraphStyle(*par);
 	else
 		tmp = new gtStyle(*style);
-	for (uint i = 1; i < styleNames.size(); ++i)
+	for (uint i = 1; i < m_styleNames.size(); ++i)
 	{
-		Properties& p = tmap[styleNames[i]];
+		Properties& p = m_tmap[m_styleNames[i]];
 		for (uint j = 0; j < p.size(); ++j)
-			sreader->updateStyle(tmp, sreader->getStyle(styleNames[i - 1]), p[j].first, p[j].second);
+			m_sreader->updateStyle(tmp, m_sreader->getStyle(m_styleNames[i - 1]), p[j].first, p[j].second);
 	}
-	currentStyle = tmp;
-	sreader->setStyle(getName(), tmp);
+	m_currentStyle = tmp;
+	m_sreader->setStyle(getName(), tmp);
 }
 
 ContentReader::~ContentReader()
 {
-	creader = NULL;
-	delete defaultStyle;
+	m_creader = NULL;
+	delete m_defaultStyle;
 }
 
 #endif // HAVE_XML
