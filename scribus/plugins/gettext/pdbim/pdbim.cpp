scribusproject/scribus

commit 9170a4f26559cc4a2c55c9e8023de911f1c2541a

#Fix warnings in pdbmim.h/.cpp. Properly close opened file
git-svn-id: svn://scribus.net/trunk/Scribus@20717 11d20701-8431-0410-a711-e3c959e3b870

scribus/plugins/gettext/pdbim/pdbim.cpp
@@ -97,7 +97,12 @@ void PdbIm::loadFile(QString fname)
 							 QMessageBox::Ok, QMessageBox::NoButton);
 		return;
 	}
-	fread( &m_header, PDB_HEADER_SIZE, 1, m_pdfp );
+	size_t result = fread( &m_header, PDB_HEADER_SIZE, 1, m_pdfp );
+	if (result != PDB_HEADER_SIZE)
+	{
+		fclose(m_pdfp);
+		return;
+	}
 	if (strncmp(m_header.type, DOC_TYPE, sizeof(m_header.type) ) ||
 		strncmp( m_header.creator, DOC_CREATOR, sizeof(m_header.creator)))
 	{
@@ -113,8 +118,12 @@ void PdbIm::loadFile(QString fname)
 	fseek(m_pdfp, PDB_HEADER_SIZE, SEEK_SET);
 	GET_DWord(m_pdfp, offset);
 	fseek(m_pdfp, offset, SEEK_SET);
-	fread(&m_rec0, sizeof(m_rec0), 1, m_pdfp);
-
+	result = fread(&m_rec0, sizeof(m_rec0), 1, m_pdfp);
+	if (result != sizeof(m_rec0))
+	{
+		fclose(m_pdfp);
+		return;
+	}
 	if (swap_Word( m_rec0.version ) == 2 )
 		bCompressed = true;
 
@@ -154,6 +163,7 @@ void PdbIm::loadFile(QString fname)
 			++m_buf->position;
 		} 
 	}
+	fclose(m_pdfp);
 }
 
 void PdbIm::selectSwap()

scribus/plugins/gettext/pdbim/pdbim.h
@@ -96,8 +96,8 @@ typedef struct {
 	UT_uint32   position;
 } buffer;
 
-#define GET_Word(f,n)   { fread( &n, 2, 1, f ); n = swap_Word ( n ); }
-#define GET_DWord(f,n)  { fread( &n, 4, 1, f ); n = swap_DWord( n ); }
+#define GET_Word(f,n)   { size_t result = fread( &n, 2, 1, f ); if (result == 2) n = swap_Word ( n ); }
+#define GET_DWord(f,n)  { size_t result = fread( &n, 4, 1, f ); if (result == 4) n = swap_DWord( n ); }
 
 /*! \brief An import filter for Palm Documents (PDB files).
 PDB documents are simple non-formatted texts in binary forms used

commit 21bcacca95f082fd23339f60ff8dda368248b92d

fix resource leak in object <coverity> + bug introduced by r20717
git-svn-id: svn://scribus.net/trunk/Scribus@20750 11d20701-8431-0410-a711-e3c959e3b870

scribus/plugins/gettext/pdbim/pdbim.cpp
@@ -67,6 +67,12 @@ PdbIm::PdbIm(const QString& fname, const QString& enc, gtWriter *w)
 	loadFile(fname);
 }
 
+PdbIm::~PdbIm()
+{
+	if (m_buf)
+		delete m_buf;
+}
+
 void PdbIm::write()
 {
 	QTextCodec *codec;
@@ -98,7 +104,7 @@ void PdbIm::loadFile(QString fname)
 		return;
 	}
 	size_t result = fread( &m_header, PDB_HEADER_SIZE, 1, m_pdfp );
-	if (result != PDB_HEADER_SIZE)
+	if (result != 1)
 	{
 		fclose(m_pdfp);
 		return;
@@ -120,7 +126,7 @@ void PdbIm::loadFile(QString fname)
 	GET_DWord(m_pdfp, offset);
 	fseek(m_pdfp, offset, SEEK_SET);
 	result = fread(&m_rec0, sizeof(m_rec0), 1, m_pdfp);
-	if (result != sizeof(m_rec0))
+	if (result != 1)
 	{
 		fclose(m_pdfp);
 		return;

scribus/plugins/gettext/pdbim/pdbim.h
@@ -96,8 +96,8 @@ typedef struct {
 	UT_uint32   position;
 } buffer;
 
-#define GET_Word(f,n)   { size_t result = fread( &n, 2, 1, f ); if (result == 2) n = swap_Word ( n ); }
-#define GET_DWord(f,n)  { size_t result = fread( &n, 4, 1, f ); if (result == 4) n = swap_DWord( n ); }
+#define GET_Word(f,n)   { size_t result = fread( &n, 2, 1, f ); if (result == 1) n = swap_Word ( n ); }
+#define GET_DWord(f,n)  { size_t result = fread( &n, 4, 1, f ); if (result == 1) n = swap_DWord( n ); }
 
 /*! \brief An import filter for Palm Documents (PDB files).
 PDB documents are simple non-formatted texts in binary forms used
@@ -119,7 +119,7 @@ public:
 	\param enc user selected text encoding. See encoding attr.
 	\param w a reference to the gtWriter instance */
 	PdbIm(const QString& fname, const QString& enc, gtWriter *w);
-	~PdbIm(){};
+	~PdbIm();
 	/*! \brief Write data into Scribus text frame.
 	User should specify encoding of the imported text - it's recoded here. */
 	void write();