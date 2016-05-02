8d18fe2428849dd3df92f31d75230bde4023985e
Jean Ghali <jghali@libertysurf.fr>
2016-05-02 21:28:17 +0000

    fix Windows printing regression: curve not printed when "display curve" option is enabled for path text objects
    git-svn-id: svn://scribus.net/trunk/Scribus@21276 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scpageoutput.cpp
+++ scribus/scpageoutput.cpp
@@ -1097,6 +1097,32 @@ public:
 
 void ScPageOutput::drawItem_PathText( PageItem_PathText* item, ScPainterExBase* painter, QRect clip )
 {
+	if (item->pathTextShowFrame())
+	{
+		painter->setupPolygon(&item->PoLine, false);
+		if (item->NamedLStyle.isEmpty())
+		{
+			if (item->lineColor() != CommonStrings::None)
+				painter->strokePath();
+		}
+		else
+		{
+			multiLine ml = m_doc->MLineStyles[item->NamedLStyle];
+			for (int it = ml.size() - 1; it > -1; it--)
+			{
+				const SingleLine& sl = ml[it];
+				if ((sl.Color != CommonStrings::None) && (sl.Width != 0))
+				{
+					ScColorShade tmp(m_doc->PageColors[sl.Color], sl.Shade);
+					painter->setPen(tmp, sl.Width,  static_cast<Qt::PenStyle>(sl.Dash), 
+							 static_cast<Qt::PenCapStyle>(sl.LineEnd), 
+							 static_cast<Qt::PenJoinStyle>(sl.LineJoin));
+					painter->drawLine(FPoint(0, 0), FPoint(item->width(), 0));
+				}
+			}
+		}
+	}
+
 	ScPageOutputPainter p(item, painter, this);
 	item->textLayout.renderBackground(&p);
 	item->textLayout.render(&p);
