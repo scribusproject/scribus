03c6a670951d781d54a58bae4e702606b5e985a4
Franz Schmid <Franz.Schmid@altmuehlnet.de>
2016-05-02 20:58:32 +0000

    Fixed Bug #13204: Unusably tiny tool icons on HiDPI screen on Ubuntu Unity desktop applied patch from Khaled Hosny <khaledhosny@eglug.org>
    git-svn-id: svn://scribus.net/trunk/Scribus@21275 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/main_nix.cpp
+++ scribus/main_nix.cpp
@@ -67,7 +67,9 @@ int main(int argc, char *argv[])
 int mainApp(int argc, char **argv)
 {
 	emergencyActivated=false;
-
+#if QT_VERSION >= 0x050600
+	ScribusQApp::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
 	ScribusQApp app(argc, argv);
 	initCrashHandler();
 	app.parseCommandLine();
