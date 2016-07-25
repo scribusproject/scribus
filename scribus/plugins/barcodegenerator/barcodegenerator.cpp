dd0a38fb3823bab62c542ee1037113be72254140
Craig Bradney <mrb@scribus.info>
2016-07-25 16:21:27 +0000

    Minor update from Terry Burton for barcode plugin
    git-svn-id: svn://scribus.net/trunk/Scribus@21414 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/barcodegenerator/barcodegenerator.cpp
+++ scribus/plugins/barcodegenerator/barcodegenerator.cpp
@@ -138,7 +138,7 @@ BarcodeGenerator::BarcodeGenerator(QWidget* parent, const char* name)
 	resvers["hibcmicropdf417"]=resvers["micropdf417"];
 	resecls["hibcmicropdf417"]=resecls["micropdf417"];
 	resvers["hanxin"]="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84";
-	resecls["hanxin"]=resecls["L1,L2,L3,L4"];
+	resecls["hanxin"]="L1,L2,L3,L4";
 
 
 	// Which options checkboxes are enabled for each encoder
