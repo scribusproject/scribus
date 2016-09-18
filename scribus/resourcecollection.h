2aa5c4c02fb3b7b9b97c5b3735685c18e637df79
Craig Bradney <mrb@scribus.info>
2016-09-18 20:20:32 +0000

    Bring in some non intrusive changes from CTL
    git-svn-id: svn://scribus.net/trunk/Scribus@21450 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/resourcecollection.h
+++ scribus/resourcecollection.h
@@ -41,55 +41,60 @@ class SCFonts;
 class ResourceCollection
 {
 public:
-	void collectFont(const QString& name)      { if (!name.isEmpty()) m_fonts.insert(name,name); }
-	void collectPattern(const QString& name)   { if (!name.isEmpty()) m_patterns.insert(name,name); }
-	void collectColor(const QString& name)     { if (!name.isEmpty()) m_colors.insert(name,name); }
-	void collectGradient(const QString& name)  { if (!name.isEmpty()) m_gradients.insert(name,name); }
-	void collectStyle(const QString& name)     { if (!name.isEmpty()) m_pstyles.insert(name,name); }
-	void collectCharStyle(const QString& name) { if (!name.isEmpty()) m_cstyles.insert(name,name); }
-	void collectLineStyle(const QString& name) { if (!name.isEmpty()) m_linestyles.insert(name,name); }
-	void collectTableStyle(const QString& name) { if (!name.isEmpty()) m_tableStyles.insert(name,name); }
-	void collectCellStyle(const QString& name) { if (!name.isEmpty()) m_cellStyles.insert(name,name); }
+	void collectFont(const QString& name)         { if (!name.isEmpty()) m_fonts.insert(name,name); }
+	void collectFontfeatures(const QString& name) { if (!name.isEmpty()) m_fontfeatures.insert(name,name); }
+	void collectPattern(const QString& name)      { if (!name.isEmpty()) m_patterns.insert(name,name); }
+	void collectColor(const QString& name)        { if (!name.isEmpty()) m_colors.insert(name,name); }
+	void collectGradient(const QString& name)     { if (!name.isEmpty()) m_gradients.insert(name,name); }
+	void collectStyle(const QString& name)        { if (!name.isEmpty()) m_pstyles.insert(name,name); }
+	void collectCharStyle(const QString& name)    { if (!name.isEmpty()) m_cstyles.insert(name,name); }
+	void collectLineStyle(const QString& name)    { if (!name.isEmpty()) m_linestyles.insert(name,name); }
+	void collectTableStyle(const QString& name)   { if (!name.isEmpty()) m_tableStyles.insert(name,name); }
+	void collectCellStyle(const QString& name)    { if (!name.isEmpty()) m_cellStyles.insert(name,name); }
 	
-	void mapFont(const QString& oldname, const QString& newname)      { m_fonts.insert(oldname, newname); }
-	void mapPattern(const QString& oldname, const QString& newname)   { m_patterns.insert(oldname, newname); }
-	void mapColor(const QString& oldname, const QString& newname)     { m_colors.insert(oldname, newname); }
-	void mapGradients(const QString& oldname, const QString& newname) { m_gradients.insert(oldname, newname); }
-	void mapStyle(const QString& oldname, const QString& newname)     { m_pstyles.insert(oldname, newname); }
-	void mapCharStyle(const QString& oldname, const QString& newname) { m_cstyles.insert(oldname, newname); }
-	void mapLineStyle(const QString& oldname, const QString& newname) { m_linestyles.insert(oldname, newname); }
-	void mapTableStyle(const QString& oldname, const QString& newname) { m_tableStyles.insert(oldname, newname); }
-	void mapCellStyle(const QString& oldname, const QString& newname) { m_cellStyles.insert(oldname, newname); }
+	void mapFont(const QString& oldname, const QString& newname)         { m_fonts.insert(oldname, newname); }
+	void mapPattern(const QString& oldname, const QString& newname)      { m_patterns.insert(oldname, newname); }
+	void mapFontfeatures(const QString& oldname, const QString& newname) { m_fontfeatures.insert(oldname, newname); }
+	void mapColor(const QString& oldname, const QString& newname)        { m_colors.insert(oldname, newname); }
+	void mapGradients(const QString& oldname, const QString& newname)    { m_gradients.insert(oldname, newname); }
+	void mapStyle(const QString& oldname, const QString& newname)        { m_pstyles.insert(oldname, newname); }
+	void mapCharStyle(const QString& oldname, const QString& newname)    { m_cstyles.insert(oldname, newname); }
+	void mapLineStyle(const QString& oldname, const QString& newname)    { m_linestyles.insert(oldname, newname); }
+	void mapTableStyle(const QString& oldname, const QString& newname)   { m_tableStyles.insert(oldname, newname); }
+	void mapCellStyle(const QString& oldname, const QString& newname)    { m_cellStyles.insert(oldname, newname); }
 	
-	void mapFonts(const QMap<QString,QString>& newnames)      { m_fonts = newnames; }
-	void mapPatterns(const QMap<QString,QString>& newnames)   { m_patterns = newnames; }
-	void mapColors(const QMap<QString,QString>& newnames)     { m_colors = newnames; }
-	void mapGradients(const QMap<QString,QString>& newnames)  { m_gradients = newnames; }
-	void mapStyles(const QMap<QString,QString>& newnames)     { m_pstyles = newnames; }
-	void mapCharStyles(const QMap<QString,QString>& newnames) { m_cstyles = newnames; }
-	void mapLineStyles(const QMap<QString,QString>& newnames) { m_linestyles = newnames; }
-	void mapTableStyles(const QMap<QString,QString>& newnames) { m_tableStyles = newnames; }
-	void mapCellStyles(const QMap<QString,QString>& newnames) { m_cellStyles = newnames; }
+	void mapFonts(const QMap<QString,QString>& newnames)        { m_fonts = newnames; }
+	void mapPatterns(const QMap<QString,QString>& newnames)     { m_patterns = newnames; }
+	void mapFontfeatures(const QMap<QString,QString>& newnames) { m_fontfeatures = newnames; }
+	void mapColors(const QMap<QString,QString>& newnames)       { m_colors = newnames; }
+	void mapGradients(const QMap<QString,QString>& newnames)    { m_gradients = newnames; }
+	void mapStyles(const QMap<QString,QString>& newnames)       { m_pstyles = newnames; }
+	void mapCharStyles(const QMap<QString,QString>& newnames)   { m_cstyles = newnames; }
+	void mapLineStyles(const QMap<QString,QString>& newnames)   { m_linestyles = newnames; }
+	void mapTableStyles(const QMap<QString,QString>& newnames)  { m_tableStyles = newnames; }
+	void mapCellStyles(const QMap<QString,QString>& newnames)   { m_cellStyles = newnames; }
 	
-	const QMap<QString, QString>& fonts()      { return m_fonts; }
-	const QMap<QString, QString>& patterns()   { return m_patterns; }
-	const QMap<QString, QString>& colors()     { return m_colors; }
-	const QMap<QString, QString>& gradients()  { return m_gradients; }
-	const QMap<QString, QString>& styles()     { return m_pstyles; }
-	const QMap<QString, QString>& charStyles() { return m_cstyles; }
-	const QMap<QString, QString>& lineStyles() { return m_linestyles; }
-	const QMap<QString, QString>& tableStyles() { return m_tableStyles; }
-	const QMap<QString, QString>& cellStyles() { return m_cellStyles; }
+	const QMap<QString, QString>& fonts()        { return m_fonts; }
+	const QMap<QString, QString>& fontfeatures() { return m_fontfeatures; }
+	const QMap<QString, QString>& patterns()     { return m_patterns; }
+	const QMap<QString, QString>& colors()       { return m_colors; }
+	const QMap<QString, QString>& gradients()    { return m_gradients; }
+	const QMap<QString, QString>& styles()       { return m_pstyles; }
+	const QMap<QString, QString>& charStyles()   { return m_cstyles; }
+	const QMap<QString, QString>& lineStyles()   { return m_linestyles; }
+	const QMap<QString, QString>& tableStyles()  { return m_tableStyles; }
+	const QMap<QString, QString>& cellStyles()   { return m_cellStyles; }
 	
-	QList<QString> fontNames() const      { return m_fonts.keys(); }
-	QList<QString> patternNames() const   { return m_patterns.keys(); }
-	QList<QString> colorNames() const     { return m_colors.keys(); }
-	QList<QString> gradientNames() const  { return m_gradients.keys(); }
-	QList<QString> styleNames() const     { return m_pstyles.keys(); }
-	QList<QString> charStyleNames() const { return m_cstyles.keys(); }
-	QList<QString> lineStyleNames() const { return m_linestyles.keys(); }
-	QList<QString> tableStyleNames() const { return m_tableStyles.keys(); }
-	QList<QString> cellStyleNames() const { return m_cellStyles.keys(); }
+	QList<QString> fontNames() const         { return m_fonts.keys(); }
+	QList<QString> patternNames() const      { return m_patterns.keys(); }
+	QList<QString> fontfeaturesNames() const { return m_fontfeatures.keys(); }
+	QList<QString> colorNames() const        { return m_colors.keys(); }
+	QList<QString> gradientNames() const     { return m_gradients.keys(); }
+	QList<QString> styleNames() const        { return m_pstyles.keys(); }
+	QList<QString> charStyleNames() const    { return m_cstyles.keys(); }
+	QList<QString> lineStyleNames() const    { return m_linestyles.keys(); }
+	QList<QString> tableStyleNames() const   { return m_tableStyles.keys(); }
+	QList<QString> cellStyleNames() const    { return m_cellStyles.keys(); }
 
 	// modifies newNames so that forall x in both newNames.key() and in existingNames, newNames[x] will map to a new unique name
 	static void makeUnique(QMap<QString,QString>& newNames, const QList<QString> existingNames);
@@ -102,6 +107,7 @@ public:
 private:
 	QMap<QString,QString> m_fonts;
 	QMap<QString,QString> m_patterns;
+	QMap<QString,QString> m_fontfeatures;
 	QMap<QString,QString> m_colors;
 	QMap<QString,QString> m_gradients;
 	QMap<QString,QString> m_pstyles;
