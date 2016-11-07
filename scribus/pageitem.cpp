f5cc045e81bab47c0584d77b92ae651f9498992a
Jean Ghali <jghali@libertysurf.fr>
2016-11-07 13:27:10 +0000

    #14347: PP displays incorrect numerated list informations
    git-svn-id: svn://scribus.net/trunk/Scribus@21500 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/pageitem.cpp
+++ scribus/pageitem.cpp
@@ -1579,7 +1579,10 @@ const CharStyle& PageItem::currentCharStyle() const
 
 void PageItem::currentTextProps(ParagraphStyle& parStyle) const
 {
-	parStyle = this->currentStyle();
+	const ParagraphStyle& curStyle = this->currentStyle();
+	parStyle.setContext(curStyle.context());
+	parStyle = curStyle;
+
 	int position = itemText.cursorPosition();
 	if (itemText.lengthOfSelection() > 0)
 		position = qMin(qMax(itemText.endOfSelection() - 1, 0), qMax(position, itemText.startOfSelection()));
