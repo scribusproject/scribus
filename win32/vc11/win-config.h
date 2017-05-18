0857680033d8cd73ebdcdf92983877114f28d6fd
Craig Bradney <mrb@scribus.info>
2017-05-18 21:39:36 +0000

    Bump Changelog and versions
    git-svn-id: svn://scribus.net/trunk/Scribus@22031 11d20701-8431-0410-a711-e3c959e3b870

--- win32/vc11/win-config.h
+++ win32/vc11/win-config.h
@@ -2,7 +2,7 @@
 #define WINCONFIG_H
 
 /* Version number of package */
-#define VERSION "1.5.3.svn"
+#define VERSION "1.5.3"
 
 /* Are we building win32 native version. */
 #define BUILD_WINDOWS_VERSION 1
