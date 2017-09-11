d82b1c989bd0e79b5611521f671adbfb94996e5e
Jean Ghali <jghali@libertysurf.fr>
2017-09-11 18:06:02 +0000

    fix build issue caused by packaging of poppler on Linux
    git-svn-id: svn://scribus.net/trunk/Scribus@22155 11d20701-8431-0410-a711-e3c959e3b870

--- cmake/modules/FindPOPPLER.cmake
+++ cmake/modules/FindPOPPLER.cmake
@@ -1,15 +1,26 @@
 if (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
 	include(UsePkgConfig)
 	PKGCONFIG(poppler _libPopplerIncDir _libPopplerLinkDir _libPopplerLinkFlags _libPopplerCflags)
-    set(PKG_POPPLER_INCLUDE_DIRS ${_libPopplerIncDir})
-    set(PKG_POPPLER_LIBRARIES ${_libPopplerLinkDir})
- else (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
-    include(FindPkgConfig)
+	set(PKG_POPPLER_INCLUDE_DIRS ${_libPopplerIncDir})
+	set(PKG_POPPLER_LIBRARIES ${_libPopplerLinkDir})
+else (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
+	include(FindPkgConfig)
 	pkg_search_module(POPPLER libpoppler>=0.19.0 poppler>=0.19.0)
- endif (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
+	if (POPPLER_FOUND)
+		pkg_search_module(POPPLER_CPP REQUIRED libpoppler-cpp>=0.19.0 poppler-cpp>=0.19.0)
+	endif(POPPLER_FOUND)
+endif (${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION} LESS 2.5)
+ 
 find_path(POPPLER_INCLUDE_DIR 
   NAMES poppler-config.h
   PATHS ${PKG_POPPLER_INCLUDE_DIRS} ${POPPLER_INCLUDE_DIRS} /usr/local/include /usr/include
+  PATH_SUFFIXES poppler
+  NO_DEFAULT_PATH
+)
+
+find_path(POPPLER_CPP_INCLUDE_DIR 
+  NAMES poppler-version.h
+  PATHS ${PKG_POPPLER_CPP_INCLUDE_DIRS} ${POPPLER_CPP_INCLUDE_DIRS} /usr/local/include /usr/include
   PATH_SUFFIXES poppler
   NO_DEFAULT_PATH
 )
@@ -20,10 +31,18 @@ find_library(POPPLER_LIBRARY
   PATH_SUFFIXES poppler
   NO_DEFAULT_PATH
 )
+
+find_library(POPPLER_CPP_LIBRARY
+  NAMES libpoppler-cpp poppler-cpp
+  PATHS ${PKG_POPPLER_CPP_LIBRARIES} ${POPPLER_CPP_LIBRARY_DIRS} /usr/local/lib /usr/lib /usr/lib/${CMAKE_LIBRARY_ARCHITECTURE}
+  PATH_SUFFIXES poppler
+  NO_DEFAULT_PATH
+)
+
 if (POPPLER_LIBRARY)
-  if (POPPLER_INCLUDE_DIR)
+  if (POPPLER_INCLUDE_DIR AND POPPLER_CPP_INCLUDE_DIR)
 	set( FOUND_POPPLER 1 )
-    set( POPPLER_LIBRARIES ${POPPLER_LIBRARY} )
-    set( POPPLER_INCLUDES ${POPPLER_INCLUDE_DIR})
-  endif (POPPLER_INCLUDE_DIR)
+    set( POPPLER_LIBRARIES ${POPPLER_LIBRARY} ${POPPLER_CPP_LIBRARY} )
+    set( POPPLER_INCLUDES ${POPPLER_INCLUDE_DIR} ${POPPLER_CPP_INCLUDE_DIR} )
+  endif (POPPLER_INCLUDE_DIR AND POPPLER_CPP_INCLUDE_DIR)
 endif (POPPLER_LIBRARY)
