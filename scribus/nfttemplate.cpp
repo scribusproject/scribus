be2d8c74eefdfb5862b5610e2cacfa10ec0f4f19
Craig Bradney <mrb@scribus.info>
2017-12-29 22:59:25 +0000

    Fix Coverity 1350070
    git-svn-id: svn://scribus.net/trunk/Scribus@22303 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/nfttemplate.cpp
+++ scribus/nfttemplate.cpp
@@ -24,58 +24,62 @@ void nfttemplate::remove()
 	if (!tmplXml.exists())
 		return;
 
-	QString newTmplXml = "";
-	QString tmp;
-	bool collect = false;
-	tmplXml.open(QIODevice::ReadOnly);
-	QTextStream stream(&tmplXml);
-	stream.setCodec("UTF-8");
-	QString line = stream.readLine();
-	while (!line.isNull())
+	if (tmplXml.open(QIODevice::ReadOnly))
 	{
-		if ((line.indexOf(enCategory) != -1) || collect)
+		QString newTmplXml;
+		QString tmp;
+		bool collect = false;
+		QTextStream stream(&tmplXml);
+		stream.setCodec("UTF-8");
+		QString line = stream.readLine();
+		while (!line.isNull())
 		{
-			collect = true;
-			line += "\n";
-			tmp += line;
-			if (line.indexOf("name") != -1)
+			if ((line.indexOf(enCategory) != -1) || collect)
 			{
-				if (line.indexOf(name) == -1)
+				collect = true;
+				line += "\n";
+				tmp += line;
+				if (line.indexOf("name") != -1)
+				{
+					if (line.indexOf(name) == -1)
+					{
+						collect = false;
+						newTmplXml += tmp;
+						tmp = "";
+					}
+				}
+				else if (line.indexOf("file") != -1)
+				{
+					QString shortFile = file.right(file.length() - file.lastIndexOf("/") -1);
+					if (line.indexOf(shortFile) == -1)
+					{
+						collect = false;
+						newTmplXml += tmp;
+						tmp = "";
+					}
+				}
+				else if (line.indexOf("</template>") != -1)
 				{
 					collect = false;
-					newTmplXml += tmp;
 					tmp = "";
 				}
-			} 
-			else if (line.indexOf("file") != -1)
-			{
-				QString shortFile = file.right(file.length() - file.lastIndexOf("/") -1);
-				if (line.indexOf(shortFile) == -1)
-				{
-					collect = false;
-					newTmplXml += tmp;
-					tmp = "";
-				}
-			} 
-			else if (line.indexOf("</template>") != -1)
-			{
-				collect = false;
-				tmp = "";
 			}
+			else
+			{
+				line += "\n";
+				newTmplXml += line;
+			}
+			line = stream.readLine();
 		}
-		else
+		tmplXml.close();
+		if (tmplXml.open(QIODevice::WriteOnly))
 		{
-			line += "\n";
-			newTmplXml += line;
+			QTextStream instream(&tmplXml);
+			instream.setCodec("UTF-8");
+			instream << newTmplXml;
+			tmplXml.close();
 		}
-		line = stream.readLine();
 	}
-	tmplXml.close();
-	tmplXml.open(QIODevice::WriteOnly);
-	QTextStream instream(&tmplXml);
-	instream.setCodec("UTF-8");
-	instream << newTmplXml;
-	tmplXml.close();
 }
 
 bool nfttemplate::canWrite()
