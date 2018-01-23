bfcba1a5aba22eaf38577cb7ac9f96b629d45699
Jean Ghali <jghali@libertysurf.fr>
2018-01-23 23:13:51 +0000

    #15111: Fix some compiler warnings
    git-svn-id: svn://scribus.net/trunk/Scribus@22359 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribus.cpp
+++ scribus/scribus.cpp
@@ -6962,7 +6962,7 @@ void ScribusMainWindow::doPrintPreview()
 	QDir d(prefsManager->preferencesLocation()+"/", "sc.*", QDir::Name, QDir::Files | QDir::NoSymLinks);
 	if ((d.exists()) && (d.count() != 0))
 	{
-		for (int i = 0; i < d.count(); i++)
+		for (uint i = 0; i < d.count(); i++)
 			QFile::remove(prefsManager->preferencesLocation() +"/" + d[i]);
 	}
 }
@@ -8307,7 +8307,7 @@ void ScribusMainWindow::initHyphenator()
 	QDir transDir(transPath, "*.*", QDir::Name, QDir::Files | QDir::NoSymLinks);
 	if ((transDir.exists()) && (transDir.count() != 0))
 	{
-		for (int i = 0; i < transDir.count(); i++)
+		for (uint i = 0; i < transDir.count(); i++)
 		{
 			QFileInfo fi(transPath + transDir[i]);
 			QString ext(fi.suffix().toLower());
