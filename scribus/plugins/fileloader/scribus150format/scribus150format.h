d3dbc757ac362c9cf72b6f325e19ad60807af02a
Jean Ghali <jghali@libertysurf.fr>
2018-02-05 11:55:53 +0000

    fix wrong include guards
    git-svn-id: svn://scribus.net/trunk/Scribus@22370 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/fileloader/scribus150format/scribus150format.h
+++ scribus/plugins/fileloader/scribus150format/scribus150format.h
@@ -4,8 +4,8 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
-#ifndef SCRIBUS134FORMAT_H
-#define SCRIBUS134FORMAT_H
+#ifndef SCRIBUS150FORMAT_H
+#define SCRIBUS150FORMAT_H
 
 #include "pluginapi.h"
 #include "loadsaveplugin.h"
