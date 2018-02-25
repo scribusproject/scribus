1732edc6f352bff14d9be2cedd4f97b9f67cd000
Jean Ghali <jghali@libertysurf.fr>
2018-02-25 17:39:40 +0000

    fix gcc warning: "macro expansion producing 'defined' has undefined behavior"
    git-svn-id: svn://scribus.net/trunk/Scribus@22411 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scimagecachedir.cpp
+++ scribus/scimagecachedir.cpp
@@ -30,7 +30,9 @@ for which a new license (GPL+exception) is in place.
 #include "scimagecachemanager.h"
 #include "scpaths.h"
 
-#define SC_DEBUG_FILE defined(DEBUG_SCIMAGECACHE)
+#if defined(DEBUG_SCIMAGECACHE)
+#define SC_DEBUG_FILE 1
+#endif
 #include "scdebug.h"
 
 const QString ScImageCacheDir::accessFileName("access");
--- scribus/scimagecachefile.cpp
+++ scribus/scimagecachefile.cpp
@@ -23,7 +23,9 @@ for which a new license (GPL+exception) is in place.
 #include "scimagecachedir.h"
 #include "scimagecachefile.h"
 
-#define SC_DEBUG_FILE defined(DEBUG_SCIMAGECACHE)
+#if defined(DEBUG_SCIMAGECACHE)
+#define SC_DEBUG_FILE 1
+#endif
 #include "scdebug.h"
 
 ScImageCacheFile::ScImageCacheFile(const QString & name, ScImageCacheDir *parent)
