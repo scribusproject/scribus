428626aa3ed49600fd8a2b24e477fd412ad413f0
Craig Bradney <mrb@scribus.info>
2018-03-26 21:44:05 +0000

    Coverity 1350729
    git-svn-id: svn://scribus.net/trunk/Scribus@22448 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/canvasgesture_linemove.h
+++ scribus/canvasgesture_linemove.h
@@ -49,7 +49,7 @@ class PageItem_Line;
 class SCRIBUS_API LineMove : public CanvasGesture
 {
 public:
-	explicit LineMove (CanvasMode* parent): CanvasGesture(parent), m_haveLineItem(false), m_bounds(0,0,0,0) {}
+	explicit LineMove (CanvasMode* parent): CanvasGesture(parent), m_haveLineItem(false), m_useOriginAsEndpoint(false), m_bounds(0,0,0,0), m_line(NULL) {}
 	virtual ~LineMove() {}
 	
 	/**
