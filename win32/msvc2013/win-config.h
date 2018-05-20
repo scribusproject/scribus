1f41f64ec5c63fec425b9d564045108b7ddf6ff1
Jean Ghali <jghali@libertysurf.fr>
2018-05-20 22:02:59 +0000

    Add Visual Studio solutions for msvc 2013 and 2015 for the legacy
    git-svn-id: svn://scribus.net/branches/Version14x/Scribus@22548 11d20701-8431-0410-a711-e3c959e3b870

---
+++ win32/msvc2013/win-config.h
@@ -0,0 +1,122 @@
+#ifndef WINCONFIG_H
+#define WINCONFIG_H
+
+/* Version number of package */
+#define VERSION "1.4.7"
+
+/* Are we building win32 native version. */
+#define BUILD_WINDOWS_VERSION 1
+
+/* Are we using native api for dll management */
+#define DLL_USE_NATIVE_API 1
+
+/* Are we building an executable to be included in MacOS/X .app bundle? */
+#undef BUILD_MAC_BUNDLE
+
+/* ColorManagementInclude */
+#define CMS_INC "lcms2.h"
+
+/* ColorManagement (LittleCms) */
+#define HAVE_CMS 1
+
+/* ColorManagement (LittleCms 2.1) */
+#define HAVE_LCMS21 1
+
+/* ColorManagement (Windows) */
+#define HAVE_ICM 1
+
+/* Can we use unicode functions (Windows) */
+#define HAVE_UNICODE 1
+
+/* Cups */
+#undef HAVE_CUPS
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#undef HAVE_DLFCN_H
+
+/* Defines if your system has the libfontconfig library */
+#undef HAVE_FONTCONFIG
+
+/* Defines if your system has the libfreetype library */
+#define HAVE_FREETYPE 1
+
+/* Defines if your system has the cairo library */
+/*#define HAVE_CAIRO 1*/
+
+/* Define if you have libjpeg */
+#define HAVE_LIBJPEG 1
+
+/* Define if you have libpng */
+#define HAVE_LIBPNG 1
+
+/* Define if you have libz */
+#define HAVE_LIBZ 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define if your system needs _NSGetEnviron to set up the environment */
+#undef HAVE_NSGETENVIRON
+
+/* Define if you have the development files for python */
+#define HAVE_PYTHON 1
+
+/* Define if you have the development files for podofo */
+#define HAVE_PODOFO 1
+
+/* Python */
+#undef HAVE_PYTHON_23
+
+/* Define if you have the res_init function */
+#undef HAVE_RES_INIT
+
+/* Define to 1 if you have the `snprintf' function. */
+#define HAVE_SNPRINTF 1
+#if defined(_MSC_VER) && (_MSC_VER < 1900)
+#define snprintf(a, b, c, d) _snprintf(a, b, c, d)
+#endif
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#undef HAVE_STDINT_H
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+// #define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <sys/bitypes.h> header file. */
+#undef HAVE_SYS_BITYPES_H
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Tifflib */
+#define HAVE_TIFF 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#undef HAVE_UNISTD_H
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* Defines if your system has the libxml2 library */
+#define HAVE_XML 1
+
+/* Defines if your system has the libxml2 library6 */
+#define HAVE_XML26 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+#undef WORDS_BIGENDIAN
+
+#define HAVE_CONFIG_H 1
+#define WIN32_LEAN_AND_MEAN
+
+typedef unsigned char boolean;
+#define HAVE_BOOLEAN
+
+#endif
---
+++ win32/msvc2015/gettext-htmlim/gettext-htmlimdll.cpp
@@ -0,0 +1,15 @@
+// gettext-htmlim-1.3.0.cpp : définit le point d'entrée pour l'application DLL.
+//
+
+#define WIN32_LEAN_AND_MEAN		// Exclure les en-têtes Windows rarement utilisés
+// Fichiers d'en-tête Windows :
+#include <windows.h>
+
+BOOL APIENTRY DllMain( HANDLE hModule, 
+                       DWORD  ul_reason_for_call, 
+                       LPVOID lpReserved
+					 )
+{
+    return TRUE;
+}
+
