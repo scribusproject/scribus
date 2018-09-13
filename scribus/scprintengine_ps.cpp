4818832a787229d65c6155d31f8dbe4e73e869bc
Jean Ghali <jghali@libertysurf.fr>
2018-09-13 16:47:06 +0000

    refactor ScPrintEngine_PS print()
    git-svn-id: svn://scribus.net/trunk/Scribus@22689 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scprintengine_ps.cpp
+++ scribus/scprintengine_ps.cpp
@@ -29,61 +29,63 @@ bool ScPrintEngine_PS::print(ScribusDoc& doc, PrintOptions& options)
 
 	if (!options.toFile)
 		filename = prefsManager->preferencesLocation()+"/tmp.ps";
-	bool PSfile = dd->PS_set_file(filename);
-	filename = QDir::toNativeSeparators(filename);
-	if (PSfile)
+	bool psFile = dd->PS_set_file(filename);
+	if (!psFile)
 	{
-		// Write the PS to a file
-		int psCreationRetVal=dd->CreatePS(&doc, options);
-		if (psCreationRetVal!=0)
-		{
-			QFile::remove(filename);
-			if (psCreationRetVal==2)
-				return true;
-			m_errorMessage = dd->errorMessage();
-			return false;
-		}
-		if (options.prnEngine != PostScript3 && ScCore->haveGS())
-		{
-			// use gs to convert our PS to a lower version
-			QString tmp;
-			QStringList opts;
-			opts.append( QString("-dDEVICEWIDTHPOINTS=%1").arg(tmp.setNum(doc.pageWidth())) );
-			opts.append( QString("-dDEVICEHEIGHTPOINTS=%1").arg(tmp.setNum(doc.pageHeight())) );
-			convertPS2PS(filename, filename + ".tmp", opts, options.prnEngine);
-			moveFile(filename + ".tmp", filename);
-		}
-		if (!options.toFile)
-		{
-			// print and delete the PS file
-			QByteArray cmd;
-			if (options.useAltPrintCommand)
-			{
-				cmd += options.printerCommand;
-				cmd += " ";
-				cmd += filename;
-				system(cmd.data());
-			}
-			else
-			{
-				QByteArray cc;
-				cmd += "lpr -P '";
-				cmd += options.printer;
-				cmd += "'";
-				if (options.copies > 1)
-					cmd += " -#" + cc.setNum(options.copies);
-				cmd += options.printerOptions;
-				cmd += " "+filename;
-				system(cmd.data());
-			}
-// Disabled that for now, as kprinter won't work otherwise
-// leaving that file around doesn't harm, as it will be overwritten the next time.
-//				unlink(filename);
-		}
-		retw = true;
+		delete dd;
+		return false;
 	}
-	else
-		retw = false;
-	delete dd;
+
+	// Write the PS to a file
+	filename = QDir::toNativeSeparators(filename);
+
+	int psCreationRetVal=dd->CreatePS(&doc, options);
+	if (psCreationRetVal!=0)
+	{
+		QFile::remove(filename);
+		if (psCreationRetVal==2)
+			return true;
+		m_errorMessage = dd->errorMessage();
+		return false;
+	}
+	if (options.prnEngine != PostScript3 && ScCore->haveGS())
+	{
+		// use gs to convert our PS to a lower version
+		QString tmp;
+		QStringList opts;
+		opts.append( QString("-dDEVICEWIDTHPOINTS=%1").arg(tmp.setNum(doc.pageWidth())) );
+		opts.append( QString("-dDEVICEHEIGHTPOINTS=%1").arg(tmp.setNum(doc.pageHeight())) );
+		convertPS2PS(filename, filename + ".tmp", opts, options.prnEngine);
+		moveFile(filename + ".tmp", filename);
+	}
+	if (!options.toFile)
+	{
+		// print and delete the PS file
+		QByteArray cmd;
+		if (options.useAltPrintCommand)
+		{
+			cmd += options.printerCommand;
+			cmd += " ";
+			cmd += filename;
+			system(cmd.data());
+		}
+		else
+		{
+			QByteArray cc;
+			cmd += "lpr -P '";
+			cmd += options.printer;
+			cmd += "'";
+			if (options.copies > 1)
+				cmd += " -#" + cc.setNum(options.copies);
+			cmd += options.printerOptions;
+			cmd += " "+filename;
+			system(cmd.data());
+		}
+		// Disabled that for now, as kprinter won't work otherwise
+		// leaving that file around doesn't harm, as it will be overwritten the next time.
+		// unlink(filename);
+	}
+
+	retw = true;
 	return retw;
 }
