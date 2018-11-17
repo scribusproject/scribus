7fe91c3c918c9a096d20817bab7950630b81c911
Jean Ghali <jghali@libertysurf.fr>
2018-11-17 18:00:37 +0000

    Document scripter getObjectAttributes() and setObjectAttributes()
    git-svn-id: svn://scribus.net/trunk/Scribus@22758 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/scriptplugin/cmdsetprop.h
+++ scribus/plugins/scriptplugin/cmdsetprop.h
@@ -225,7 +225,7 @@ QT_TR_NOOP("setObjectAttributes(attributes, [\"name\"])\n\
 Sets attributes of the object \"name\".\n\
 If \"name\" is not given the currently selected item is used.\n\
 \n\
-attributes is list of dictionary. Each dictionary must have those keys:\n\
+attributes is a list of dictionary. Each dictionary must have those keys:\n\
 Name, Type, Value, Parameter, Relationship, RelationshipTo, AutoAddTo\n\
 All values must be strings.\n\
 \n\
