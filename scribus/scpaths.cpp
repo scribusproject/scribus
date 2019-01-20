e51649adf1c0d72e912b87b0cc44ececd580281d
Galileo Sartor <galileo.sartor@gmail.com>
2019-01-20 20:55:48 +0100

    Revert to Q_OS_LINUX

--- scribus/scpaths.cpp
+++ scribus/scpaths.cpp
@@ -70,7 +70,7 @@ ScPaths::ScPaths() :
 
 // Build an appimage and redirect the paths to
 // ones inside the bundle, relative to the executable.
-#ifdef BUILD_APPIMAGE_BUNDLE
+#ifdef Q_OS_LINUX
 	// Set the application name expliticly.
 	QCoreApplication::setApplicationName("scribus");
 	QString pathPtr = QCoreApplication::applicationDirPath() + QString("/..");
