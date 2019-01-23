b1ae22f135cf93ab2c9b4b6fe57efec4fcaa6f25
Jean Ghali <jghali@libertysurf.fr>
2019-01-23 03:48:14 +0000

    Refactoring of MenuManager::addMenuItemStringstoMenuBar()
    git-svn-id: svn://scribus.net/trunk/Scribus@22814 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/menumanager.cpp
+++ scribus/menumanager.cpp
@@ -140,35 +140,35 @@ void MenuManager::clear()
 
 void MenuManager::addMenuItemStringstoMenuBar(const QString &menuName, const QMap<QString, QPointer<ScrAction> > &menuActions)
 {
-	if (menuStrings.contains(menuName) && menuBarMenus.contains(menuName))
+	if (!menuStrings.contains(menuName) || !menuBarMenus.contains(menuName))
+		return;
+
+	for (int i=0; i<menuStrings[menuName].count();++i)
 	{
-		for (int i=0; i<menuStrings[menuName].count();++i)
+		//Add Separators
+		if (menuStrings[menuName].at(i) == "SEPARATOR")
 		{
-			//Add Separators
-			if (menuStrings[menuName].at(i)=="SEPARATOR")
-				menuBarMenus[menuName]->addSeparator();
-			else
+			menuBarMenus[menuName]->addSeparator();
+			continue;
+		}
+			
+		//Add Menu Items
+		if (menuActions.contains(menuStrings[menuName].at(i)))
+		{
+			menuBarMenus[menuName]->addAction(menuActions[menuStrings[menuName].at(i)]);
+			continue;
+		}
+
+		//Add Sub Menus
+		if (menuStrings.contains(menuStrings[menuName].at(i)))
+		{
+			QMenu *subMenu=menuBarMenus[menuName]->addMenu(menuStringTexts[menuStrings[menuName].at(i)]);
+			menuBarMenus.insert(menuStrings[menuName].at(i), subMenu);
+			if (rememberedMenus.contains(menuStrings[menuName].at(i)))
 			{
-				//Add Menu Items
-				if (menuActions.contains(menuStrings[menuName].at(i)))
-				{
-					menuBarMenus[menuName]->addAction(menuActions[menuStrings[menuName].at(i)]);
-				}
-				else
-				//Add Sub Menus
-				{
-					if (menuStrings.contains(menuStrings[menuName].at(i)))
-					{
-						QMenu *subMenu=menuBarMenus[menuName]->addMenu(menuStringTexts[menuStrings[menuName].at(i)]);
-						menuBarMenus.insert(menuStrings[menuName].at(i), subMenu);
-						if (rememberedMenus.contains(menuStrings[menuName].at(i)))
-						{
-							rememberedMenus.insert(menuStrings[menuName].at(i), subMenu);
-						}
-						addMenuItemStringstoMenu(menuStrings[menuName].at(i), subMenu, menuActions);
-					}
-				}
+				rememberedMenus.insert(menuStrings[menuName].at(i), subMenu);
 			}
+			addMenuItemStringstoMenu(menuStrings[menuName].at(i), subMenu, menuActions);
 		}
 	}
 }
