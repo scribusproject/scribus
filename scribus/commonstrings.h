facaa5f6515fa5f1e789a8cb7b968018ceaf9ac5
Jean Ghali <jghali@libertysurf.fr>
2019-03-11 10:50:51 +0000

    Fix license text in a few files
    git-svn-id: svn://scribus.net/trunk/Scribus@22882 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/commonstrings.h
+++ scribus/commonstrings.h
@@ -12,7 +12,7 @@ for which a new license (GPL+exception) is in place.
 
 /***************************************************************************
 *                                                                         *
-*   ScMW program is free software; you can redistribute it and/or modify  *
+*   Scribus program is free software; you can redistribute it and/or modify  *
 *   it under the terms of the GNU General Public License as published by  *
 *   the Free Software Foundation; either version 2 of the License, or     *
 *   (at your option) any later version.                                   *
