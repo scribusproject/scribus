scribusproject/scribus

commit fc8fad37eeb6f92928326aa7cae9082c99b5a011

C++11 updates and cleanups
git-svn-id: svn://scribus.net/trunk/Scribus@23017 11d20701-8431-0410-a711-e3c959e3b870

scribus/colormgmt/sccieilluminants.h
@@ -14,7 +14,7 @@ for which a new license (GPL+exception) is in place.
 class ScCIEIlluminant
 {
 public:
-	virtual ~ScCIEIlluminant() {}
+	~ScCIEIlluminant() = default;
 	
 	// Relative illuminant power for specified wavelength
 	double power(int waveLen) const { return m_pow.value(waveLen, 0.0); }
@@ -29,14 +29,14 @@ protected:
 
 class ScD50Illuminant : public ScCIEIlluminant
 {
-public:
-	ScD50Illuminant();
+	public:
+		ScD50Illuminant();
 };
 
 class ScD65Illuminant : public ScCIEIlluminant
 {
-public:
-	ScD65Illuminant();
+	public:
+		ScD65Illuminant();
 };
 
 #endif // SCCIEILLUMINANTS_H

scribus/colormgmt/sccieobservers.h
@@ -14,7 +14,7 @@ for which a new license (GPL+exception) is in place.
 class ScCIEObserver
 {
 public:
-	virtual ~ScCIEObserver() {}
+	~ScCIEObserver() = default;
 
 	// Wavelengths represented in the data set
 	QVector<int> wavelengths() const;
@@ -36,14 +36,14 @@ protected:
 
 class ScCIE1931Observer : public ScCIEObserver
 {
-public:
-	ScCIE1931Observer();
+	public:
+		ScCIE1931Observer();
 };
 
 class ScCIE1964Observer : public ScCIEObserver
 {
-public:
-	ScCIE1964Observer();
+	public:
+		ScCIE1964Observer();
 };
 
 #endif // SCCIEOBSERVERS_H

commit 9311f36e80ff42d9c8e8d85780209cfe3f8ff63d

Fix incorrect changes introduced by r23017 in sccieilluminants.h and sccieobservers.h headers
git-svn-id: svn://scribus.net/trunk/Scribus@23019 11d20701-8431-0410-a711-e3c959e3b870

scribus/colormgmt/sccieilluminants.h
@@ -14,7 +14,7 @@ for which a new license (GPL+exception) is in place.
 class ScCIEIlluminant
 {
 public:
-	~ScCIEIlluminant() = default;
+	virtual ~ScCIEIlluminant() = default;
 	
 	// Relative illuminant power for specified wavelength
 	double power(int waveLen) const { return m_pow.value(waveLen, 0.0); }
@@ -29,14 +29,14 @@ protected:
 
 class ScD50Illuminant : public ScCIEIlluminant
 {
-	public:
-		ScD50Illuminant();
+public:
+	ScD50Illuminant();
 };
 
 class ScD65Illuminant : public ScCIEIlluminant
 {
-	public:
-		ScD65Illuminant();
+public:
+	ScD65Illuminant();
 };
 
 #endif // SCCIEILLUMINANTS_H

scribus/colormgmt/sccieobservers.h
@@ -14,7 +14,7 @@ for which a new license (GPL+exception) is in place.
 class ScCIEObserver
 {
 public:
-	~ScCIEObserver() = default;
+	virtual ~ScCIEObserver() = default;
 
 	// Wavelengths represented in the data set
 	QVector<int> wavelengths() const;
@@ -36,14 +36,14 @@ protected:
 
 class ScCIE1931Observer : public ScCIEObserver
 {
-	public:
-		ScCIE1931Observer();
+public:
+	ScCIE1931Observer();
 };
 
 class ScCIE1964Observer : public ScCIEObserver
 {
-	public:
-		ScCIE1964Observer();
+public:
+	ScCIE1964Observer();
 };
 
 #endif // SCCIEOBSERVERS_H