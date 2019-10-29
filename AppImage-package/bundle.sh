69fa1643ea7c1dd9f389071e4aa998538625fdfd
Jean Ghali <jghali@libertysurf.fr>
2019-10-29 14:01:10 +0000

    One more fix for tkinter integration in AppImage
    git-svn-id: svn://scribus.net/trunk/Scribus@23304 11d20701-8431-0410-a711-e3c959e3b870

--- AppImage-package/bundle.sh
+++ AppImage-package/bundle.sh
@@ -124,5 +124,5 @@ chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 ARCH=x86_64 VERSION=$SCRIBUS_VERSION ./linuxdeployqt-continuous-x86_64.AppImage --appimage-extract-and-run appdir/usr/share/applications/scribus.desktop \
                                                -appimage -unsupported-bundle-everything \
                                                -executable=appdir/usr/bin/python3.6 \
-                                               -executable=appdir/usr/_tkinter.cpython-36m-x86_64-linux-gnu.so \
+                                               -executable=appdir/usr/lib/python3.6/lib-dynload/_tkinter.cpython-36m-x86_64-linux-gnu.so \
                                                -extra-plugins=platformthemes/libqgtk2.so,styles/libqgtk2style.so
