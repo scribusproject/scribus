887e33741ee11c7f27bace50e31bc2eef2621b51
Craig Bradney <mrb@scribus.info>
2020-04-10 21:22:57 +0000

    #15912: Process events differently on start up to catch file open events from document double click events
    git-svn-id: svn://scribus.net/trunk/Scribus@23586 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusapp.cpp
+++ scribus/scribusapp.cpp
@@ -352,8 +352,9 @@ int ScribusQApp::init()
 	if (!m_ScCore)
 		return EXIT_FAILURE;
 	ScCore=m_ScCore;
-	processEvents();
+	processEvents(QEventLoop::ExcludeUserInputEvents|QEventLoop::ExcludeSocketNotifiers,1000);
 	ScCore->init(useGUI, m_filesToLoad);
+	processEvents();
 	int retVal=EXIT_SUCCESS;
 	/* TODO:
 	 * When Scribus is truly able to run without GUI
@@ -362,7 +363,6 @@ int ScribusQApp::init()
 	 */
 	// if (useGUI)
 	retVal=ScCore->startGUI(m_showSplash, m_showFontInfo, m_showProfileInfo, m_lang);
-
 	// A hook for plugins and scripts to trigger on. Some plugins and scripts
 	// require the app to be fully set up (in particular, the main window to be
 	// built and shown) before running their setup.
@@ -675,6 +675,7 @@ bool ScribusQApp::neverSplashExists()
 
 void ScribusQApp::downloadComplete(const QString &t)
 {
+	Q_UNUSED(t)
 	//qDebug()<<"ScribusQApp: download finished:"<<t;
 }
 
