bc6355d713f687e7585ba1b44dde03ec53151d56
Jean Ghali <jghali@libertysurf.fr>
2020-10-25 19:50:11 +0000

    Fix potential crash caused by deletion of item specified in undo action
    git-svn-id: svn://scribus.net/trunk/Scribus@24106 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribusdoc.cpp
+++ scribus/scribusdoc.cpp
@@ -14969,20 +14969,25 @@ void ScribusDoc::itemSelection_UnGroupObjects(Selection* customSelection)
 	Selection* itemSelection = (customSelection != nullptr) ? customSelection : m_Selection;
 	if (itemSelection->isEmpty())
 		return;
-
+	
+	bool wasLoad = isLoading();
 	int docSelectionCount = itemSelection->count();
 	PageItem *currItem;
+
 	UndoTransaction activeTransaction;
 	if (UndoManager::undoEnabled())
 		activeTransaction = m_undoManager->beginTransaction(Um::Selection, Um::IGroup, Um::Ungroup, "", Um::IGroup);
+
 	QList<PageItem*> toDelete;
+	QRectF textInteractionRect;
 	for (int i = 0; i < docSelectionCount; ++i)
 	{
 		currItem = itemSelection->itemAt(i);
-		if (currItem->isGroup())
-			toDelete.append(currItem);
+		if (!currItem->isGroup())
+			continue;
+		toDelete.append(currItem);
 	}
-	bool wasLoad = isLoading();
+
 	// Remove group control objects
 	setLoading(true);
 	itemSelection->delaySignalsOn();
@@ -15033,7 +15038,8 @@ void ScribusDoc::itemSelection_UnGroupObjects(Selection* customSelection)
 		currItem = toDelete.at(i);
 		if (currItem->isWelded())
 			currItem->unWeld();
-		delete currItem;
+		if (!UndoManager::undoEnabled())
+			delete currItem;
 	}
 
 	if (activeTransaction)
