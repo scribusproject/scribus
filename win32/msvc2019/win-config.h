1584d8e62f7558c156029944244f8ff9ba9bc78a
Craig Bradney <mrb@scribus.info>
2020-11-07 14:49:56 +0000

    Bump versions
    git-svn-id: svn://scribus.net/trunk/Scribus@24195 11d20701-8431-0410-a711-e3c959e3b870

--- win32/msvc2019/win-config.h
+++ win32/msvc2019/win-config.h
@@ -2,7 +2,7 @@
 #define WINCONFIG_H
 
 /* Version number of package */
-#define VERSION "1.5.6.svn"
+#define VERSION "1.5.6"
 
 /* Are we building win32 native version. */
 #define BUILD_WINDOWS_VERSION 1
