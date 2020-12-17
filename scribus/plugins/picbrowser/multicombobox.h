cfaf12269b1d834d82aa4dc8f8266a431161f31c
Craig Bradney <mrb@scribus.info>
2020-12-17 22:27:55 +0000

    Add #ifdef guards to multicombobox.h
    git-svn-id: svn://scribus.net/trunk/Scribus@24330 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/picbrowser/multicombobox.h
+++ scribus/plugins/picbrowser/multicombobox.h
@@ -4,6 +4,10 @@ to the COPYING file provided with the program. Following this notice may exist
 a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
+
+#ifndef MULTICOMBOBOXMODEL_H
+#define MULTICOMBOBOXMODEL_H
+
 #include <QComboBox>
 #include <QListView>
 #include <QStandardItemModel>
@@ -55,3 +59,5 @@ class multiCombobox : public QComboBox
 	signals:
 		void checkstateChanged ( int );
 };
+
+#endif
