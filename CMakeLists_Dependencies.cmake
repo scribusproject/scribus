e633ebd08913a681d3518b9a9d672e3361e18555
Craig Bradney <mrb@scribus.info>
2022-01-29 13:23:04 +0000

    Bump versions for trunk to 1.7.0.svn
    git-svn-id: svn://scribus.net/trunk/Scribus@24830 11d20701-8431-0410-a711-e3c959e3b870

--- CMakeLists_Dependencies.cmake
+++ CMakeLists_Dependencies.cmake
@@ -89,6 +89,9 @@ else()
 	include_directories(${Qt6Network_INCLUDE_DIRS})
 	add_definitions(${Qt6Network_DEFINITIONS})
 
+	find_package(Qt6 COMPONENTS LinguistTools REQUIRED)
+	include_directories(${Qt6LinguistTools_INCLUDE_DIRS})
+	add_definitions(${Qt6LinguistTools_DEFINITIONS})
 
 	#	include_directories(${Qt5Xml_INCLUDE_DIRS})
 #	add_definitions(${Qt5Xml_DEFINITIONS})
