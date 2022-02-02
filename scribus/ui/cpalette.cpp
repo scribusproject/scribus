3c6976047f7136eb20245b78c7297a74b9c83226
Craig Bradney <mrb@scribus.info>
2022-02-02 21:00:59 +0000

    Qt6 runtime fix: Fix more signals for QComboBox
    git-svn-id: svn://scribus.net/trunk/Scribus@24882 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/cpalette.cpp
+++ scribus/ui/cpalette.cpp
@@ -164,8 +164,8 @@ void ColorPalette::connectSignals()
 	connect(gradEditStroke, SIGNAL(gradientChanged()), this, SLOT(handleStrokeGradient()));
 	connect(gradientType  , SIGNAL(activated(int)), this, SLOT(slotGradType(int)));
 	connect(gradientTypeStroke , SIGNAL(activated(int)), this, SLOT(slotGradTypeStroke(int)));
-	connect(namedGradient , SIGNAL(activated(const QString &)), this, SLOT(setNamedGradient(const QString &)));
-	connect(namedGradientStroke, SIGNAL(activated(const QString &)), this, SLOT(setNamedGradientStroke(const QString &)));
+	connect(namedGradient , SIGNAL(currentTextChanged(const QString &)), this, SLOT(setNamedGradient(const QString &)));
+	connect(namedGradientStroke, SIGNAL(currentTextChanged(const QString &)), this, SLOT(setNamedGradientStroke(const QString &)));
 	connect(overPrintCombo     , SIGNAL(activated(int)), this, SIGNAL(NewOverprint(int)));
 	connect(patternBox         , SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(selectPattern(QListWidgetItem*)));
 	connect(patternBoxStroke   , SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(selectPatternS(QListWidgetItem*)));
@@ -224,8 +224,8 @@ void ColorPalette::disconnectSignals()
 	disconnect(gradEditStroke, SIGNAL(gradientChanged()), this, SLOT(handleStrokeGradient()));
 	disconnect(gradientType  , SIGNAL(activated(int)), this, SLOT(slotGradType(int)));
 	disconnect(gradientTypeStroke , SIGNAL(activated(int)), this, SLOT(slotGradTypeStroke(int)));
-	disconnect(namedGradient , SIGNAL(textActivated(const QString &)), this, SLOT(setNamedGradient(const QString &)));
-	disconnect(namedGradientStroke, SIGNAL(textActivated(const QString &)), this, SLOT(setNamedGradientStroke(const QString &)));
+	disconnect(namedGradient , SIGNAL(currentTextChanged(const QString &)), this, SLOT(setNamedGradient(const QString &)));
+	disconnect(namedGradientStroke, SIGNAL(currentTextChanged(const QString &)), this, SLOT(setNamedGradientStroke(const QString &)));
 	disconnect(overPrintCombo     , SIGNAL(activated(int)), this, SIGNAL(NewOverprint(int)));
 	disconnect(patternBox         , SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(selectPattern(QListWidgetItem*)));
 	disconnect(patternBoxStroke   , SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(selectPatternS(QListWidgetItem*)));
