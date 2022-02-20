18847e105ab68f4ac55b215717d684c990f91a39
Jean Ghali <jghali@libertysurf.fr>
2022-02-20 18:14:06 +0000

    Restore utf-8 encoding specification incorrectly removed by Transifex update script
    git-svn-id: svn://scribus.net/trunk/Scribus@24951 11d20701-8431-0410-a711-e3c959e3b870

--- resources/translations/scribus.nl.ts
+++ resources/translations/scribus.nl.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="nl">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
 <context>
     <name>AIPlug</name>
     <message>
--- resources/translations/scribus.uk.ts
+++ resources/translations/scribus.uk.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="uk">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
 <context>
     <name>AIPlug</name>
     <message>
