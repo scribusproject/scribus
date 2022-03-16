e8216754096857c97959497062ff59f653f741b2
Craig Bradney <mrb@scribus.info>
2022-03-16 23:08:32 +0000

    Fix crash risk with duplicate iterator usage, and code clean a little
    git-svn-id: svn://scribus.net/trunk/Scribus@25035 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scribus.cpp
+++ scribus/scribus.cpp
@@ -2844,16 +2844,15 @@ void ScribusMainWindow::rebuildRecentPasteMenu()
 	{
 		QMap<QString,BibView::Elem>::Iterator it;
 		it = scrapbookPalette->tempBView->objectMap.end();
-		it--;
 		QString strippedName;
-		for (int m = 0; m < max; ++m)
+		for (int i = 0; i < max; ++i)
 		{
+			it--;
 			strippedName = it.key();
 			QPixmap pm = it.value().Preview;
-			scrRecentPasteActions.insert(strippedName, new ScrAction(ScrAction::RecentPaste, pm, QString(), QString("&%1 %2").arg(m+1).arg(strippedName), QKeySequence(), this, it.key()));
+			scrRecentPasteActions.insert(strippedName, new ScrAction(ScrAction::RecentPaste, pm, QString(), QString("&%1 %2").arg(i + 1).arg(strippedName), QKeySequence(), this, strippedName));
 			connect( scrRecentPasteActions[strippedName], SIGNAL(triggeredData(QString)), this, SLOT(pasteRecent(QString)) );
 			scrMenuMgr->addMenuItemString(strippedName, "EditPasteRecent");
-			it--;
 		}
 		scrMenuMgr->addMenuItemStringsToRememberedMenu("EditPasteRecent", scrRecentPasteActions);
 	}
