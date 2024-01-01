4bd50902f47bd49cc079a9c130986a381a3c1077
Jean Ghali <jghali@libertysurf.fr>
2024-01-01 23:41:21 +0000

    Bump version in msvc projects to 1.6.1.svn
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@25957 11d20701-8431-0410-a711-e3c959e3b870

--- win32/msvc2019/win-config.h
+++ win32/msvc2019/win-config.h
@@ -2,7 +2,7 @@
 #define WINCONFIG_H
 
 /* Version number of package */
-#define VERSION "1.6.0"
+#define VERSION "1.6.1.svn"
 
 /* Are we building an executable to be included in MacOS/X .app bundle? */
 #undef BUILD_MAC_BUNDLE
