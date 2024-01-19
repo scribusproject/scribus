d10a36721bf456f93b240f415fec8b9903bd7ac6
Jean Ghali <jghali@libertysurf.fr>
2024-01-19 21:13:17 +0000

    #17124: Useless m_item check in StyleManager
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@25999 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/stylemanager.cpp
+++ scribus/ui/stylemanager.cpp
@@ -317,14 +317,14 @@ void StyleManager::slotDelete()
 		tmp << styles[i].first;
 
 	SMReplaceDia dia(selected, tmp, this);
-	if (dia.exec() && m_item)
-	{
-		m_item->deleteStyles(dia.items());
-		applyButton->setEnabled(true);
-		resetButton->setEnabled(true);
-		reloadStyleView(false);
-		slotOk();
-	}
+	if (!dia.exec())
+		return;
+
+	m_item->deleteStyles(dia.items());
+	applyButton->setEnabled(true);
+	resetButton->setEnabled(true);
+	reloadStyleView(false);
+	slotOk();
 }
 
 
