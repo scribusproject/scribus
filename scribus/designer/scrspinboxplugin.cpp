df5758f6a752c64663826a855b72fb90aca400d6
Craig Bradney <mrb@scribus.info>
2024-05-26 20:26:10 +0000

    Add copyright/licensing information
    git-svn-id: svn://scribus.net/trunk/Scribus@26148 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/designer/scrspinboxplugin.cpp
+++ scribus/designer/scrspinboxplugin.cpp
@@ -1,9 +1,18 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 /*
-For general Scribus (>=1.3.2) copyright and licensing information please refer
-to the COPYING file provided with the program. Following this notice may exist
-a copyright and/or license notice that predates the release of Scribus 1.3.2
-for which a new license (GPL+exception) is in place.
+For general Scribus copyright and licensing information please refer
+to the COPYING file provided with the program.
 */
+
+
 #include <QtPlugin>
 #include "scrspinboxplugin.h"
 #include "../ui/scrspinbox.h"
