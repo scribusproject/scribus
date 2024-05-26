056e01788fff0774c3a8716b85966ced17b66cd4
Craig Bradney <mrb@scribus.info>
2024-05-26 20:23:51 +0000

    Add copyright/licensing information
    git-svn-id: svn://scribus.net/trunk/Scribus@26147 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/colorpicker/colorpicker_gradient.h
+++ scribus/ui/colorpicker/colorpicker_gradient.h
@@ -1,3 +1,17 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+For general Scribus copyright and licensing information please refer
+to the COPYING file provided with the program.
+*/
+
 #ifndef COLORPICKER_GRADIENT_H
 #define COLORPICKER_GRADIENT_H
 
--- scribus/ui/colorpicker/colorpicker_gradient_swatches.cpp
+++ scribus/ui/colorpicker/colorpicker_gradient_swatches.cpp
@@ -1,3 +1,17 @@
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/*
+For general Scribus copyright and licensing information please refer
+to the COPYING file provided with the program.
+*/
+
 #include "colorpicker_gradient_swatches.h"
 #include "ui/widgets/gradientlistbox.h"
 
