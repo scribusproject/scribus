a5c98b6f06a10d7fd910ac7417308461d96690b5
Jean Ghali <jghali@libertysurf.fr>
2024-06-17 20:45:24 +0000

    Bump versions to 1.6.3svn
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@26188 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.6.2"
+#cmakedefine VERSION "1.6.3.svn"
 
 #cmakedefine BUILD_MAC_BUNDLE
 #cmakedefine DLL_USE_NATIVE_API 1
