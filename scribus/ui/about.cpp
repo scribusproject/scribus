bac009799be28c129cb7d248662f9af22ca7a773
Craig Bradney <mrb@scribus.info>
2024-12-08 11:57:23 +0000

    Bump changelog and about date
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@26462 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -638,8 +638,8 @@ QString About::parseLinksFile(const QString& fileName)
 
 QString About::generateBuildInfo()
 {
-	QString BUILD_DAY = "15";
-	QString BUILD_MONTH = CommonStrings::june;
+	QString BUILD_DAY = "8";
+	QString BUILD_MONTH = CommonStrings::december;
 	QString BUILD_YEAR = "2024";
 	QString BUILD_TIME;
 	QString BUILD_TZ;
