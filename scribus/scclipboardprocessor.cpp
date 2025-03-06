b13ae04ba1044c89b178702f3a8dd96ef2625a5e
Jean Ghali <jghali@libertysurf.fr>
2025-03-06 23:14:58 +0000

    Fix incorrect access to freed memory
    git-svn-id: svn://scribus.net/trunk/Scribus@26764 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scclipboardprocessor.cpp
+++ scribus/scclipboardprocessor.cpp
@@ -144,7 +144,8 @@ bool ScClipboardProcessor::processHTML_Other()
 bool ScClipboardProcessor::html_MSFT_Process()
 {
 	// Convert to a const xmlChar*, parse with libxml2
-	const xmlChar* html_content_cstr = reinterpret_cast<const xmlChar*>(m_content.toUtf8().constData());
+	QByteArray utf8Content = m_content.toUtf8();
+	const xmlChar* html_content_cstr = reinterpret_cast<const xmlChar*>(utf8Content.constData());
 
 	// Parse the HTML content using libxml2's HTML parser
 	htmlDocPtr doc = htmlReadDoc(html_content_cstr, NULL, NULL, HTML_PARSE_RECOVER | HTML_PARSE_NOERROR | HTML_PARSE_NOWARNING);
