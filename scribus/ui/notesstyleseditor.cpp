76ac8c018960621199b0147ac12d0db989a93e0c
Jean Ghali <jghali@libertysurf.fr>
2025-03-09 17:54:43 +0000

    #17447: Notes Style Editor crashes
    git-svn-id: svn://scribus.net/trunk/Scribus@26775 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/notesstyleseditor.cpp
+++ scribus/ui/notesstyleseditor.cpp
@@ -68,7 +68,7 @@ void NotesStylesEditor::languageChange()
 	}
 	else
 	{
-		OKButton->setText("OK");
+		OKButton->setText(tr("OK"));
 		OKButton->setToolTip("");
 		ApplyButton->setText(tr("Apply"));
 	}
@@ -387,7 +387,7 @@ void NotesStylesEditor::on_NewButton_clicked()
 
 void NotesStylesEditor::on_OKButton_clicked()
 {
-	if (OKButton->text() != tr("OK"))
+	if (m_addNewNsMode)
 	{
 		//in adding new style mode go back to normal editing mode
 		OKButton->setText(tr("OK"));
@@ -397,7 +397,7 @@ void NotesStylesEditor::on_OKButton_clicked()
 		m_changesMap.remove(newName);
 		int index = NSlistBox->findText(newName);
 		NSlistBox->removeItem(index);
-		NSlistBox->setCurrentIndex(index-1);
+		NSlistBox->setCurrentIndex(index - 1);
 		on_NSlistBox_currentTextChanged(NSlistBox->currentText());
 	}
 	else
