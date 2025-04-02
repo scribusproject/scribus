42c5319d4213ac36630b673a95b285b3913b2b1d
Jean Ghali <jghali@libertysurf.fr>
2025-04-02 20:51:41 +0000

    Fix warning in slaoutput.cpp caused by [[nodiscard]] being set on ImageStream::reset() since poppler 25.02.0
    git-svn-id: svn://scribus.net/trunk/Scribus@26823 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/plugins/import/pdf/slaoutput.cpp
+++ scribus/plugins/import/pdf/slaoutput.cpp
@@ -2352,7 +2352,14 @@ void SlaOutputDev::drawImageMask(GfxState *state, Object *ref, Stream *str, int 
 {
 //	qDebug() << "Draw Image Mask";
 	auto imgStr = std::make_unique<ImageStream>(str, width, 1, 1);
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool resetDone = imgStr->reset();
+	if (!resetDone)
+		return;
+#else
 	imgStr->reset();
+#endif
+
 #ifdef WORDS_BIGENDIAN
 	QImage image(width, height, QImage::Format_Mono);
 #else
@@ -2426,7 +2433,14 @@ void SlaOutputDev::drawSoftMaskedImage(GfxState *state, Object *ref, Stream *str
 {
 //	qDebug() << "SlaOutputDev::drawSoftMaskedImage Masked Image Components" << colorMap->getNumPixelComps();
 	auto imgStr = std::make_unique<ImageStream>(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool resetDone = imgStr->reset();
+	if (!resetDone)
+		return;
+#else
 	imgStr->reset();
+#endif
+
 	unsigned int *dest = nullptr;
 	unsigned char * buffer = new unsigned char[width * height * 4];
 	for (int y = 0; y < height; y++)
@@ -2444,7 +2458,14 @@ void SlaOutputDev::drawSoftMaskedImage(GfxState *state, Object *ref, Stream *str
 	}
 
 	auto mskStr = std::make_unique<ImageStream>(maskStr, maskWidth, maskColorMap->getNumPixelComps(), maskColorMap->getBits());
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool mskResetDone = mskStr->reset();
+	if (!mskResetDone)
+		return;
+#else
 	mskStr->reset();
+#endif
+
 	unsigned char *mdest = nullptr;
 	unsigned char * mbuffer = new unsigned char[maskWidth * maskHeight];
 	memset(mbuffer, 0, maskWidth * maskHeight);
@@ -2502,7 +2523,14 @@ void SlaOutputDev::drawMaskedImage(GfxState *state, Object *ref, Stream *str,  i
 {
 //	qDebug() << "SlaOutputDev::drawMaskedImage";
 	auto imgStr = std::make_unique<ImageStream>(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool resetDone = imgStr->reset();
+	if (resetDone)
+		return;
+#else
 	imgStr->reset();
+#endif
+
 	unsigned int *dest = nullptr;
 	unsigned char * buffer = new unsigned char[width * height * 4];
 	for (int y = 0; y < height; y++)
@@ -2520,7 +2548,14 @@ void SlaOutputDev::drawMaskedImage(GfxState *state, Object *ref, Stream *str,  i
 	}
 
 	auto mskStr = std::make_unique<ImageStream>(maskStr, maskWidth, 1, 1);
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool mskResetDone = mskStr->reset();
+	if (!mskResetDone)
+		return;
+#else
 	mskStr->reset();
+#endif
+
 	unsigned char *mdest = nullptr;
 	int invert_bit = maskInvert ? 1 : 0;
 	unsigned char * mbuffer = new unsigned char[maskWidth * maskHeight];
@@ -2566,7 +2601,13 @@ void SlaOutputDev::drawMaskedImage(GfxState *state, Object *ref, Stream *str,  i
 void SlaOutputDev::drawImage(GfxState *state, Object *ref, Stream *str, int width, int height, GfxImageColorMap *colorMap, bool interpolate, const int* maskColors, bool inlineImg)
 {
 	auto imgStr = std::make_unique<ImageStream>(str, width, colorMap->getNumPixelComps(), colorMap->getBits());
+#if POPPLER_ENCODED_VERSION >= POPPLER_VERSION_ENCODE(25, 02, 0)
+	bool resetDone = imgStr->reset();
+	if (!resetDone)
+		return;
+#else
 	imgStr->reset();
+#endif
 
 	QImage image(width, height, QImage::Format_ARGB32);
 	if (image.isNull())
