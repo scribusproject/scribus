bc1910152c5ed3cc1e8fa7b3412afec416ff945c
Jean Ghali <jghali@libertysurf.fr>
2025-04-19 21:13:22 +0000

    Bump versions in win-config.h and Scribus.rc files
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@26841 11d20701-8431-0410-a711-e3c959e3b870

--- win32/msvc2015/win-config.h
+++ win32/msvc2015/win-config.h
@@ -2,7 +2,7 @@
 #define WINCONFIG_H
 
 /* Version number of package */
-#define VERSION "1.6.4.svn"
+#define VERSION "1.6.4"
 
 /* Are we building an executable to be included in MacOS/X .app bundle? */
 #undef BUILD_MAC_BUNDLE
