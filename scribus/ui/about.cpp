74a2559a554d78f6ae713723ad3e67c7597eb356
Jean Ghali <jghali@libertysurf.fr>
2025-04-22 22:41:06 +0000

    Bump version to 1.6.5.svn
    git-svn-id: svn://scribus.net/branches/Version16x/Scribus@26845 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -638,7 +638,7 @@ QString About::parseLinksFile(const QString& fileName)
 
 QString About::generateBuildInfo()
 {
-	QString BUILD_DAY = "20";
+	QString BUILD_DAY = "22";
 	QString BUILD_MONTH = CommonStrings::april;
 	QString BUILD_YEAR = "2025";
 	QString BUILD_TIME;
