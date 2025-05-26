ee0717f4670cbcc9945c5786723e7c91318904af
Craig Bradney <mrb@scribus.info>
2025-05-26 21:08:59 +0000

    Add missing include
    git-svn-id: svn://scribus.net/trunk/Scribus@26903 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/scxmlstreamreader.cpp
+++ scribus/scxmlstreamreader.cpp
@@ -5,8 +5,8 @@ a copyright and/or license notice that predates the release of Scribus 1.3.2
 for which a new license (GPL+exception) is in place.
 */
 
+#include <QStringView>
 #include "scxmlstreamreader.h"
-
 #include "scclocale.h"
 
 ScXmlStreamAttributes::ScXmlStreamAttributes()
