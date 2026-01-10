bfd6d8d11a25a720b6dc0ba396459830851d84f1
Craig Bradney <mrb@scribus.info>
2026-01-10 14:46:01 +0000

    Bump versions, Changelog and updated releases list
    git-svn-id: svn://scribus.net/trunk/Scribus@27309 11d20701-8431-0410-a711-e3c959e3b870

--- config.h.cmake
+++ config.h.cmake
@@ -9,7 +9,7 @@
 #error "Include <scconfig.h> rather than using <config.h> directly"
 #endif
 
-#cmakedefine VERSION "1.7.2"
+#cmakedefine VERSION "1.7.3.svn"
 
 #cmakedefine BUILD_MAC_BUNDLE
 #cmakedefine DLL_USE_NATIVE_API 1
--- scribus/ui/about.cpp
+++ scribus/ui/about.cpp
@@ -644,7 +644,7 @@ QString About::parseLinksFile(const QString& fileName)
 
 QString About::generateBuildInfo()
 {
-	QString BUILD_DAY = "09";
+	QString BUILD_DAY = "10";
 	QString BUILD_MONTH = CommonStrings::january;
 	QString BUILD_YEAR = "2026";
 	QString BUILD_TIME;
