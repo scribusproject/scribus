e9c54d1d5237572f135a0f3a7bc0c1a447a2cd11
Craig Bradney <mrb@scribus.info>
2026-02-20 14:33:51 +0000

    Fix licence to standard
    git-svn-id: svn://scribus.net/trunk/Scribus@27415 11d20701-8431-0410-a711-e3c959e3b870

--- scribus/imagedataloaders/scimgdataloader_jpegxl.h
+++ scribus/imagedataloaders/scimgdataloader_jpegxl.h
@@ -1,9 +1,12 @@
-/*
-For general Scribus (>=1.3.2) copyright and licensing information please refer
-to the COPYING file provided with the program. Following this notice may exist
-a copyright and/or license notice that predates the release of Scribus 1.3.2
-for which a new license (GPL+exception) is in place.
-*/
+/***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************/
+
 #ifndef SCIMGDATALOADER_JPEGXL_H
 #define SCIMGDATALOADER_JPEGXL_H
 
@@ -20,10 +23,10 @@ public:
 
 	struct JXLImageInfo
 	{
-			size_t width;
-			size_t height;
-			int channels;
-			int bits_per_sample;
+		size_t width;
+		size_t height;
+		int channels;
+		int bits_per_sample;
 	};
 
 	ScImgDataLoader_JPEGXL();
